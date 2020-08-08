22f4741ccf3db2758fddf5f9cc7670bbd883c603
Adam Soffer <adam@livepeer.org>
2020-08-08 14:25:32 -0400

    fix docs on mobile

--- packages/www/components/DocsLayout/sidebar.tsx
+++ packages/www/components/DocsLayout/sidebar.tsx
@@ -7,7 +7,7 @@ import { useRouter } from "next/router";
 const listItems = [
   {
     label: "Using Livepeer for live streaming",
-    href: "/docs",
+    href: "/docs"
   },
   {
     label: "How to live stream with livepeer.com",
@@ -15,29 +15,29 @@ const listItems = [
     children: [
       {
         label: "How to create a stream",
-        href: "/docs/livepeer-dot-com/create-a-stream",
+        href: "/docs/livepeer-dot-com/create-a-stream"
       },
       {
         href: "/docs/livepeer-dot-com/stream-page-specifications",
-        label: "Understanding the stream page specifications",
+        label: "Understanding the stream page specifications"
       },
       {
         href: "/docs/livepeer-dot-com/broadcast-a-stream-session",
-        label: "How to broadcast a stream session",
+        label: "How to broadcast a stream session"
       },
       {
         href: "/docs/livepeer-dot-com/stream-rendition-properties",
-        label: "How to set stream rendition properties",
+        label: "How to set stream rendition properties"
       },
       {
         href: "/docs/livepeer-dot-com/playback-a-stream",
-        label: "How to playback a stream",
+        label: "How to playback a stream"
       },
       {
         href: "/docs/livepeer-dot-com/delete-a-stream",
-        label: "How to delete a stream",
-      },
-    ],
+        label: "How to delete a stream"
+      }
+    ]
   },
   {
     label: "How to live stream with Livepeer API",
@@ -45,57 +45,57 @@ const listItems = [
     children: [
       {
         label: "How to create an API key",
-        href: "/docs/livepeer-api/create-api-key",
+        href: "/docs/livepeer-api/create-api-key"
       },
       {
         label: "How to get base urls",
-        href: "/docs/livepeer-api/base-urls",
+        href: "/docs/livepeer-api/base-urls"
       },
       {
         label: "How to create a stream",
-        href: "/docs/livepeer-api/create-a-stream",
+        href: "/docs/livepeer-api/create-a-stream"
       },
       {
         label: "How to broadcast a live stream",
-        href: "/docs/livepeer-api/broadcast-a-live-stream",
+        href: "/docs/livepeer-api/broadcast-a-live-stream"
       },
       {
         label: "How to playback a live stream",
-        href: "/docs/livepeer-api/playback-a-live-stream",
+        href: "/docs/livepeer-api/playback-a-live-stream"
       },
       {
         label: "How to list all streams",
-        href: "/docs/livepeer-api/list-all-streams",
+        href: "/docs/livepeer-api/list-all-streams"
       },
       {
         label: "How to delete a stream",
-        href: "/docs/livepeer-api/delete-a-stream",
-      },
-    ],
+        href: "/docs/livepeer-api/delete-a-stream"
+      }
+    ]
   },
   {
     label: "How do you manage API keys",
     children: [
       {
         href: "/docs/api-keys/when-do-you-need-an-API-key",
-        label: "When do you need an API key?",
+        label: "When do you need an API key?"
       },
       {
         href: "/docs/api-keys/create-an-api-key",
-        label: "How to create an API key",
+        label: "How to create an API key"
       },
       {
         href: "/docs/api-keys/delete-an-api-key",
-        label: "How to delete an API key",
-      },
-    ],
-  },
+        label: "How to delete an API key"
+      }
+    ]
+  }
 ];
 
-export default () => {
+export default ({ ...props }) => {
   const { asPath } = useRouter();
   return (
-    <Box sx={{ a: { textDecoration: "none" } }}>
+    <Box sx={{ a: { textDecoration: "none" } }} {...props}>
       <Styled.h5 as="h1" sx={{ mb: 3 }}>
         Documentation
       </Styled.h5>
@@ -103,14 +103,13 @@ export default () => {
         if (listItem?.children) {
           return (
             <Category
-              active={listItem.children.some((l) => l.href === asPath)}
+              active={listItem.children.some(l => l.href === asPath)}
               label={listItem.label}
               open={
-                listItem?.open ||
-                listItem.children.some((l) => l.href === asPath)
+                listItem?.open || listItem.children.some(l => l.href === asPath)
               }
             >
-              {listItem.children.map((listItem) => {
+              {listItem.children.map(listItem => {
                 return (
                   <ListItem
                     key={listItem.href}
@@ -149,7 +148,7 @@ function Category({ label, open = false, active, children }) {
           sx={{
             mr: 3,
             ml: "1px",
-            transform: isOpen ? "rotate(90deg)" : "rotate(0deg)",
+            transform: isOpen ? "rotate(90deg)" : "rotate(0deg)"
           }}
           width="6"
           height="10"
@@ -187,15 +186,15 @@ function ListItem({ href, label, active }) {
           height: 4,
           mr: 3,
           borderRadius: "50%",
-          background: "rgb(102, 102, 102)",
-        },
+          background: "rgb(102, 102, 102)"
+        }
       }}
     >
       <Link href={href} as={href} passHref>
         <a
           sx={{
             fontWeight: active ? "heading" : "body",
-            color: active ? "black" : "#444444",
+            color: active ? "black" : "#444444"
           }}
         >
           {label}
