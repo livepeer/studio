7b6fb2a3166e8239a2b27580ac69597a1c150e6c
Adam Soffer <adam@livepeer.org>
2020-08-26 09:25:18 -0400

    disable cache on blog for now (#57)

--- packages/www/pages/blog/category/[slug].tsx
+++ packages/www/pages/blog/category/[slug].tsx
@@ -4,22 +4,8 @@ import allCategories from "../../../queries/allCategories.gql";
 import allPosts from "../../../queries/allPosts.gql";
 import BlogIndex from "../index";
 
-export async function getStaticPaths() {
-  const { allCategory } = await request(
-    "https://dp4k3mpw.api.sanity.io/v1/graphql/production/default",
-    print(allCategories)
-  );
-  let paths = [];
-  for (const category of allCategory) {
-    paths.push({ params: { slug: category.slug.current } });
-  }
-  return {
-    fallback: true,
-    paths,
-  };
-}
-
-export async function getStaticProps({ params }) {
+export async function getServerSideProps({ params }) {
+  const { slug } = params;
   const { allCategory: categories } = await request(
     "https://dp4k3mpw.api.sanity.io/v1/graphql/production/default",
     print(allCategories)
@@ -38,8 +24,45 @@ export async function getStaticProps({ params }) {
       categories: categories.reverse(),
       posts,
     },
-    revalidate: 1,
   };
 }
+
+// export async function getStaticPaths() {
+//   const { allCategory } = await request(
+//     "https://dp4k3mpw.api.sanity.io/v1/graphql/production/default",
+//     print(allCategories)
+//   );
+//   let paths = [];
+//   for (const category of allCategory) {
+//     paths.push({ params: { slug: category.slug.current } });
+//   }
+//   return {
+//     fallback: true,
+//     paths,
+//   };
+// }
+
+// export async function getStaticProps({ params }) {
+//   const { allCategory: categories } = await request(
+//     "https://dp4k3mpw.api.sanity.io/v1/graphql/production/default",
+//     print(allCategories)
+//   );
+//   categories.push({ title: "All", slug: { current: "" } });
+//   const {
+//     allPost: posts,
+//   } = await request(
+//     "https://dp4k3mpw.api.sanity.io/v1/graphql/production/default",
+//     print(allPosts),
+//     { where: { category: { slug: { current: { eq: params.slug } } } } }
+//   );
+
+//   return {
+//     props: {
+//       categories: categories.reverse(),
+//       posts,
+//     },
+//     revalidate: 1,
+//   };
+// }
 
 export default BlogIndex;
