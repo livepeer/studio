3dd4bf6f49520fd50aad420815e7ae6f0d46bb86
Julián Benegas <julianbenegas99@gmail.com>
2020-09-03 12:42:00 -0300

    fix some zIndex details

--- packages/www/components/Navigation/mobile/docs-submenu.tsx
+++ packages/www/components/Navigation/mobile/docs-submenu.tsx
@@ -25,73 +25,76 @@ const DocsMobileSubMenu = ({
   };
 
   return (
-    <Box
-      sx={{
-        position: "sticky",
-        top: 64,
-        bg: "white",
-        boxShadow: isOpen
-          ? "none"
-          : "rgba(0, 0, 0, 0.02) 0px 30px 30px, rgba(0, 0, 0, 0.03) 0px 0px 8px, rgba(0, 0, 0, 0.05) 0px 1px 0px",
-        zIndex: 21,
-        opacity: mobileSubmenuVisible || isOpen ? 1 : 0,
-        visibility: mobileSubmenuVisible || isOpen ? "visible" : "hidden",
-        transition: "opacity .2s"
-      }}
-    >
-      <Container>
-        <Flex
-          onClick={() => setIsOpen(!isOpen)}
-          sx={{
-            display: ["flex", "flex", "none"],
-            cursor: "pointer",
-            pb: 3,
-            pt: 3,
-            alignItems: "center",
-            justifyContent: "space-between",
-            fontSize: "24px",
-            lineHeight: "29px",
-            letterSpacing: "-0.04em",
-            fontWeight: 600
-          }}
-        >
-          {pathname.includes("/docs/reference") ? (
-            <Box>API Reference</Box>
-          ) : (
-            <Box>Guides</Box>
-          )}
-          <IconButton sx={{ fontSize: 6 }}>
-            <FiChevronDown size="24px" />
-          </IconButton>
-        </Flex>
-      </Container>
+    <>
+      <div sx={{ position: "sticky", top: 64, height: 64 }} />
       <Box
         sx={{
           position: "fixed",
-          top: "128px",
-          height: isOpen ? "calc(100vh - 128px)" : 0,
+          top: 64,
           width: "100%",
-          bg: "rgba(0,0,0,.35)",
-          overflow: "hidden",
-          transition: "height .2s"
+          bg: "white",
+          boxShadow: isOpen
+            ? "none"
+            : "rgba(0, 0, 0, 0.02) 0px 30px 30px, rgba(0, 0, 0, 0.03) 0px 0px 8px, rgba(0, 0, 0, 0.05) 0px 1px 0px",
+          opacity: mobileSubmenuVisible || isOpen ? 1 : 0,
+          visibility: mobileSubmenuVisible || isOpen ? "visible" : "hidden",
+          transition: "opacity .2s"
         }}
-        onClick={handleClick}
       >
-        <Container
-          sx={{
-            bg: "background",
-            height: "calc(100vh - 228px)",
-            overflow: "auto"
-          }}
-        >
-          <TableOfContents
-            onClose={() => setIsOpen(false)}
-            tree={tree}
-            ignoreList={ignoreList}
-          />
+        <Container>
+          <Flex
+            onClick={() => setIsOpen(!isOpen)}
+            sx={{
+              display: ["flex", "flex", "none"],
+              cursor: "pointer",
+              pb: 3,
+              pt: 3,
+              alignItems: "center",
+              justifyContent: "space-between",
+              fontSize: "24px",
+              lineHeight: "29px",
+              letterSpacing: "-0.04em",
+              fontWeight: 600
+            }}
+          >
+            {pathname.includes("/docs/reference") ? (
+              <Box>API Reference</Box>
+            ) : (
+              <Box>Guides</Box>
+            )}
+            <IconButton sx={{ fontSize: 6 }}>
+              <FiChevronDown size="24px" />
+            </IconButton>
+          </Flex>
         </Container>
+        <Box
+          sx={{
+            position: "fixed",
+            top: "128px",
+            height: isOpen ? "calc(100vh - 128px)" : 0,
+            width: "100%",
+            bg: "rgba(0,0,0,.35)",
+            overflow: "hidden",
+            transition: "height .2s"
+          }}
+          onClick={handleClick}
+        >
+          <Container
+            sx={{
+              bg: "background",
+              height: "calc(100vh - 228px)",
+              overflow: "auto"
+            }}
+          >
+            <TableOfContents
+              onClose={() => setIsOpen(false)}
+              tree={tree}
+              ignoreList={ignoreList}
+            />
+          </Container>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
