0c40c7b235b148c5aafe0bfa148bce4b3b401d2f
Julián Benegas <julianbenegas99@gmail.com>
2020-09-04 13:25:28 -0300

    remove commented code

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -28,19 +28,3 @@ module.exports = withPlugins([
   ],
   withTM
 ]);
-
-// module.exports = withMDX({
-//   pageExtensions: ["js", "jsx", "mdx", "ts", "tsx", "svg"],
-//   webpack(config, _options) {
-//     config.module.rules.push({
-//       test: /\.(graphql|gql)$/,
-//       exclude: /node_modules/,
-//       loader: "graphql-tag/loader"
-//     });
-//     config.module.rules.push({
-//       test: /\.md$/,
-//       use: "raw-loader"
-//     });
-//     return config;
-//   }
-// });
