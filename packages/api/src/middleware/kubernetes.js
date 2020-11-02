431389aca0772067e0fe8917b69f57275a0f4fff
Eli Mallon <eli@livepeer.org>
2020-11-02 14:01:17 -0800

    api: cache k8s control plane responses

--- packages/api/src/middleware/kubernetes.js
+++ packages/api/src/middleware/kubernetes.js
@@ -13,14 +13,39 @@ export default function kubernetesMiddleware({
   kubeBroadcasterTemplate,
   kubeOrchestratorTemplate,
 }) {
+  const cache = {}
   const kc = new k8s.KubeConfig()
   kc.loadFromDefault()
 
   const kubeApi = kc.makeApiClient(k8s.CoreV1Api)
 
+  // Try to read namespaced endpoints, return most recent data if we can't
+  const cachedReadNamespacedEndpoints = async (kubeService, kubeNamespace) => {
+    const cacheKey = `${kubeService}%${kubeNamespace}`
+    try {
+      const endpoints = await timeout(5000, () =>
+        kubeApi.readNamespacedEndpoints(kubeService, kubeNamespace),
+      )
+      cache[cacheKey] = endpoints
+      return endpoints
+    } catch (e) {
+      if (cache[cacheKey]) {
+        console.log(
+          'WARNING: kubernetes control plane unavailable, returning cached data',
+        )
+        return cache[cacheKey]
+      }
+      console.log(
+        'ERROR: kubernetes control plane unavailable, cached data unavailable, cannot provide orchestrator list',
+      )
+      throw e
+    }
+  }
+
   const getBroadcasters = async () => {
-    const endpoints = await timeout(5000, () =>
-      kubeApi.readNamespacedEndpoints(kubeBroadcasterService, kubeNamespace),
+    const endpoints = await cachedReadNamespacedEndpoints(
+      kubeBroadcasterService,
+      kubeNamespace,
     )
     const ret = []
     if (endpoints.body && endpoints.body.subsets) {
@@ -43,8 +68,9 @@ export default function kubernetesMiddleware({
   }
 
   const getOrchestrators = async () => {
-    const endpoints = await timeout(5000, () =>
-      kubeApi.readNamespacedEndpoints(kubeOrchestratorService, kubeNamespace),
+    const endpoints = await cachedReadNamespacedEndpoints(
+      kubeOrchestratorService,
+      kubeNamespace,
     )
     const ret = []
     if (endpoints.body && endpoints.body.subsets) {
