81c45c24b78e12250e918dc661592d61cbd501fa
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-12-04 02:07:30 +0200

    /admin/active-clean endpoint Checks if there is streams marked 'active' but wasn't seen recently and mark them as not active

--- packages/api/src/controllers/index.js
+++ packages/api/src/controllers/index.js
@@ -9,6 +9,7 @@ import geolocate from './geolocate'
 import webhook from './webhook'
 import stripe from './stripe'
 import version from './version'
+import admin from './admin'
 
 // Annoying but necessary to get the routing correct
 export default {
@@ -23,4 +24,5 @@ export default {
   webhook,
   stripe,
   version,
+  admin,
 }
