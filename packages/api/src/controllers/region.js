1a23ffd4dea38546692830c581f73cd344f6b33f
M Yahya <ya7yaz@gmail.com>
2020-12-08 13:06:57 +0100

    put and delete endpoints

--- packages/api/src/controllers/region.js
+++ packages/api/src/controllers/region.js
@@ -16,4 +16,34 @@ app.get('/:region', authMiddleware({ admin: true }), async (req, res, next) => {
 
 app.get('/orchestrator', getOrchestrators)
 
+app.put('/:region', authMiddleware({ admin: true }), async (req, res, next) => {
+  if (!req.body || !req.body.region) {
+    res.status(406)
+    return res.json({ error: 'region field is required' })
+  }
+
+  if (!req.body.orchestrators || !req.body.orchestrators.length) {
+    console.log(`WARNING req.body.orchestrators is undefined`)
+  }
+
+  let region = {
+    region: req.body.region,
+    orchestrators: req.body.orchestrators || [],
+  }
+
+  console.log(`req.body ${JSON.stringify(req.body)}`)
+  const resp = await req.store.put(`region/${req.params.region}`, region)
+
+  return res.json(region)
+})
+
+app.delete(
+  '/:region',
+  authMiddleware({ admin: true }),
+  async (req, res, next) => {
+    const resp = await req.store.delete(`region/${req.params.region}`)
+    return res.json({ ok: 1 })
+  },
+)
+
 export default app
