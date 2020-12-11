7f5c9c22c385ac208ebe1f49f238fb0be2f0e29d
Ivan Tivonenko <darkdarkdragon@gmail.com>
2020-12-11 06:14:04 +0200

    prevent potential race when cleaning up timed out active streams

--- packages/api/src/controllers/admin.js
+++ packages/api/src/controllers/admin.js
@@ -3,43 +3,61 @@ import Router from 'express/lib/router'
 import { db } from '../store'
 import sql from 'sql-template-strings'
 
-const ACTIVE_TIMEOUT = 24 * 3600 * 1000 // 24h
+const ACTIVE_TIMEOUT = 1 * 3600 * 1000 // 1h
 
 const app = Router()
 
-app.post('/active-clean', authMiddleware({ anyAdmin: true }), async (req, res) => {
-  // check if there is streams marked 'active' but wasn't seen recently
+app.post(
+  '/active-clean',
+  authMiddleware({ anyAdmin: true }),
+  async (req, res) => {
+    // check if there is streams marked 'active' but wasn't seen recently
 
-  const query = []
-  query.push(sql`data->>'isActive' = 'true'`)
+    const query = []
+    query.push(sql`data->>'isActive' = 'true'`)
 
-  let cleaned = 0
-  let docs, cursor
-  const now = Date.now()
-  const toClean = []
-  do {
-    [docs, cursor] = await db.stream.find(query, { cursor, limit: 100 })
-    console.log(`got ${docs.length} active streams, cursor: ${cursor}`)
-    for (const stream of docs) {
-        const lastSeen = +new Date(stream.lastSeen)
-        const needClean = isNaN(lastSeen) ||(now - lastSeen) > ACTIVE_TIMEOUT
-        console.log(`id ${stream.id} seen ${new Date(stream.lastSeen)} need clean ${needClean} name ${stream.name}`)
+    let docs, cursor
+    const now = Date.now()
+    const toClean = []
+    res.writeHead(200)
+    res.flushHeaders()
+    do {
+      ;[docs, cursor] = await db.stream.find(query, { cursor, limit: 100 })
+      // sending progress should prevent request timing out
+      res.write('.')
+      for (const stream of docs) {
+        const lastSeen = new Date(stream.lastSeen).getTime()
+        const needClean = isNaN(lastSeen) || now - lastSeen > ACTIVE_TIMEOUT
         if (needClean) {
-            toClean.push(stream)
+          toClean.push(stream)
         }
+      }
+    } while (cursor && docs.length === 100)
+    res.write('\n')
+    let i = 0
+    let cleaned = 0
+    for (const stream of toClean) {
+      const upRes = await db.stream.update(
+        stream.id,
+        {
+          isActive: false,
+        },
+        ` (data->>'lastSeen')::bigint = ${stream.lastSeen}`,
+        true,
+      )
+      if (upRes.rowCount) {
+        console.log(
+          `cleaned timed out stream id=${stream.id} lastSeen=${new Date(
+            stream.lastSeen,
+          )} name=${stream.name}`,
+        )
+      }
+      cleaned += upRes.rowCount
+      res.write(`index=${i} cleaned=${cleaned} total=${toClean.length}\n`)
+      i++
     }
-  } while (cursor && docs.length === 100)
-  for (const stream of toClean) {
-    await db.stream.update(stream.id, {
-        isActive: false,
-    })
-    console.log(`stream ${stream.id} (${stream.name}) active set to false`)
-    cleaned++
-  }
-  const msg = `cleaned ${cleaned} streams`
-
-  res.status(200)
-  res.json({ msg })
-})
+    res.end(`cleaned ${cleaned} streams\n`)
+  },
+)
 
 export default app
