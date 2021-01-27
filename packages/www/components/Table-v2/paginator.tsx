ec1d326e620a7deedf6aa586861823c7b490c1bd
Julián Benegas <julianbenegas99@gmail.com>
2021-01-27 09:51:15 -0300

    some fixes

--- packages/www/components/Table-v2/paginator.tsx
+++ packages/www/components/Table-v2/paginator.tsx
@@ -1,19 +1,14 @@
 import Button from "components/Button";
 
 interface Props {
-  pageFirstRowIndex: number;
-  pageLastRowIndex: number;
-  numberOfRows: number;
   canPreviousPage: boolean;
   canNextPage: boolean;
   onPreviousPage: () => void;
   onNextPage: () => void;
 }
 
+// TODO fix this tailwind
 const Paginator = ({
-  pageFirstRowIndex,
-  pageLastRowIndex,
-  numberOfRows,
   canPreviousPage,
   canNextPage,
   onPreviousPage,
@@ -22,23 +17,15 @@ const Paginator = ({
   <nav
     className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
     aria-label="Pagination">
-    <div className="hidden sm:block">
-      <p className="text-sm text-gray-700">
-        Mostrando <span className="font-medium">{pageFirstRowIndex}</span> a{" "}
-        <span className="font-medium">{pageLastRowIndex}</span> de{" "}
-        <span className="font-medium">{numberOfRows}</span>{" "}
-        {numberOfRows === 1 ? "fila" : "filas"}
-      </p>
-    </div>
     <div className="flex-1 flex justify-between sm:justify-end space-x-3">
       <Button
         variant="white"
         onClick={onPreviousPage}
         disabled={!canPreviousPage}>
-        Anterior
+        Previous
       </Button>
       <Button variant="white" onClick={onNextPage} disabled={!canNextPage}>
-        Próxima
+        Next
       </Button>
     </div>
   </nav>
