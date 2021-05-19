3765b24e4c2fbc5e4337571b12811f68fd245f8d
Ivan Tivonenko <darkdarkdragon@gmail.com>
2021-05-20 00:07:15 +0300

    Show 'Download mp4' button instead of dropdown

--- packages/www/components/StreamSessionsTable/index.tsx
+++ packages/www/components/StreamSessionsTable/index.tsx
@@ -21,33 +21,33 @@ function makeMP4Url(hlsUrl: string, profileName: string): string {
   return pp.join("/") + "/" + profileName + ".mp4";
 }
 
+type Profile = { name: string; width: number; height: number };
 export type RecordingUrlCellProps = {
   children?: React.ReactNode;
   tooltipChildren?: React.ReactNode;
   href?: string;
   id?: string;
-  profiles?: Array<{ name: string }>;
+  profiles?: Array<Profile>;
   showMP4: boolean;
 };
+
+function getHighestMP4Url(hlsUrl: string, profiles: Array<Profile>): string {
+  const [profileName, _] = profiles.reduce<[string, number]>(
+    (pv, cv) => {
+      if (cv.width * cv.height > pv[1]) {
+        return [cv.name, cv.width * cv.height];
+      }
+      return pv;
+    },
+    ["", 0]
+  );
+  return makeMP4Url(hlsUrl, profileName);
+}
 
 const RecordingUrlCell = <D extends TableData>({
   cell,
 }: CellComponentProps<D, RecordingUrlCellProps>) => {
   const id = cell.value.id;
-
-  const [isOpen, setIsOpen] = useState(false);
-
-  function handleClick(e: any) {
-    const isInside = e?.target?.closest(`#mp4-dropdown-${id}`) !== null;
-    if (isInside) return;
-    setIsOpen(false);
-    document.removeEventListener("click", handleClick);
-  }
-
-  useEffect(() => {
-    if (isOpen) document.addEventListener("click", handleClick);
-    return () => document.removeEventListener("click", handleClick);
-  }, [isOpen]);
 
   return (
     <div id={`mp4-link-dropdown-${id}`} sx={{ position: "relative" }}>
@@ -56,65 +56,15 @@ const RecordingUrlCell = <D extends TableData>({
           <Link href={cell.value.href}>
             <a>{cell.value.children}</a>
           </Link>
-          {isOpen ? (
-            <div
-              id={`mp4-dropdown-${id}`}
-              sx={{
-                position: "absolute",
-                right: "-15px",
-                top: "32px",
-                width: "max-content",
-                bg: "background",
-                boxShadow:
-                  "0px 24px 40px rgba(0, 0, 0, 0.24), 0px 30px 30px rgba(0, 0, 0, 0.02)",
-                borderRadius: 8,
-                p: 3,
-                border: "1px solid #EAEAEA",
-              }}>
-              <div
-                sx={{
-                  position: "absolute",
-                  right: "15px",
-                  top: "-8px",
-                }}>
-                <svg
-                  width="20"
-                  height="10"
-                  viewBox="0 0 20 10"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="M17.2839 7.5H18.5C19.0523 7.5 19.5 7.94772 19.5 8.5C19.5 9.05229 19.0523 9.5 18.5 9.5H1.5C0.947715 9.5 0.5 9.05229 0.5 8.5C0.5 7.94772 0.947715 7.5 1.5 7.5H2.76293C3.69243 7.5 4.57781 7.10358 5.19688 6.41023L9.06779 2.07481C9.68073 1.38832 10.7616 1.41058 11.3458 2.12172L14.7987 6.32528C15.4096 7.06898 16.3215 7.5 17.2839 7.5Z"
-                    fill="white"
-                    stroke="#EAEAEA"
-                  />
-                  <rect y="8" width="20" height="2" fill="white" />
-                </svg>
-              </div>
-              {cell.value.profiles.map((pr) => (
-                <div>
-                  <A
-                    variant="download"
-                    target="_blank"
-                    href={makeMP4Url(cell.value.href, pr.name)}
-                    sx={{ p: 1 }}>
-                    {pr.name}.mp4
-                  </A>
-                </div>
-              ))}
-            </div>
-          ) : null}
           {cell.value.showMP4 && cell.value.profiles?.length ? (
             <Box>
-              <Button
-                variant="outline"
-                sx={{ p: 1 }}
-                onClick={(e) => {
-                  e.preventDefault();
-                  setIsOpen(!isOpen);
-                }}>
-                Download
-              </Button>
+              <A
+                variant="downloadOutline"
+                target="_blank"
+                href={makeMP4Url(cell.value.href, "source")}
+                sx={{ p: 1 }}>
+                Download&nbsp;mp4
+              </A>
             </Box>
           ) : null}
         </Flex>
