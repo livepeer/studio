95038a04626cc201741762a0d16b0f95360c2789
adamsoffer <adam@livepeer.org>
2021-05-24 23:11:26 -0400

    wip: home

--- packages/www/pages/dashboard/streams/index.tsx
+++ packages/www/pages/dashboard/streams/index.tsx
@@ -1,6 +1,7 @@
 import Layout from "../../../layouts/dashboard";
 import { Box, Flex, Heading } from "@livepeer.com/design-system";
 import { useApi, useLoggedIn } from "hooks";
+import StreamsTable from "@components/Dashboard/StreamsTable";
 
 const Streams = () => {
   useLoggedIn();
@@ -9,34 +10,11 @@ const Streams = () => {
   if (!user || user.emailValid === false) {
     return <Layout />;
   }
+
   return (
-    <Layout>
+    <Layout id="streams" breadcrumbs={[{ title: "Streams" }]}>
       <Box css={{ p: "$6" }}>
-        <Box css={{ mb: "$8" }}>
-          <Flex
-            justify="between"
-            align="end"
-            css={{
-              borderBottom: "1px solid",
-              borderColor: "$slate500",
-              pb: "$4",
-              mb: "$5",
-              width: "100%",
-            }}>
-            <Heading size="2">
-              <Flex>
-                <Box
-                  css={{
-                    mr: "$3",
-                    fontWeight: 600,
-                    letterSpacing: "0",
-                  }}>
-                  Streams
-                </Box>
-              </Flex>
-            </Heading>
-          </Flex>
-        </Box>
+        <StreamsTable title="Streams" userId={user.id} />
       </Box>
     </Layout>
   );
