e48fb31c0a58e33d99c902368e9a194e7e9af5f8
adamsoffer <adam@livepeer.org>
2021-05-24 23:11:26 -0400

    wip: home

--- packages/www/pages/dashboard/index.tsx
+++ packages/www/pages/dashboard/index.tsx
@@ -4,16 +4,14 @@ import {
   Box,
   Flex,
   Button,
-  Promo,
   Text,
   IconButton,
+  Promo,
 } from "@livepeer.com/design-system";
 import GettingStarted from "@components/Dashboard/GettingStarted";
 import UsageSummary from "@components/Dashboard/UsageSummary";
 import StreamsTable from "@components/Dashboard/StreamsTable";
-import SessionsTable from "@components/Dashboard/SessionsTable";
 import { useLoggedIn, useApi } from "hooks";
-import { Stream } from "@livepeer.com/api";
 import { Cross1Icon } from "@radix-ui/react-icons";
 import InfoIcon from "../../public/img/icons/info.svg";
 
@@ -35,13 +33,15 @@ const Dashboard = () => {
   }
 
   return (
-    <Layout>
+    <Layout id="home" breadcrumbs={[{ title: "Home" }]}>
       <Box css={{ p: "$6" }}>
         <Promo size="2" css={{ mb: "$7" }}>
           <Flex>
             <StyledInfoIcon />
             <Box>
-              <Text size="2" css={{ mb: "$1", fontWeight: 500 }}>
+              <Text
+                size="2"
+                css={{ fontSize: "14px", mb: "$1", fontWeight: 500 }}>
                 Upgrade to Pro
               </Text>
               <Text size="2" css={{ lineHeight: 1.4, color: "$slate900" }}>
@@ -64,14 +64,11 @@ const Dashboard = () => {
         <Box css={{ mb: "$8" }}>
           <GettingStarted />
         </Box>
-        <Box css={{ mb: "$9" }}>
+        <Box css={{ mb: "$8" }}>
           <UsageSummary />
         </Box>
         <Box css={{ mb: "$8" }}>
-          <StreamsTable id="Streams Table" userId={user.id} />
-        </Box>
-        <Box css={{ mb: "$8" }}>
-          {/* <SessionsTable streamId={stream.id} /> */}
+          <StreamsTable title="Active Streams" userId={user.id} />
         </Box>
       </Box>
     </Layout>
