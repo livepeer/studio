cdeee45358c52819c44916b79942f216f1ba7c06
adamsoffer <adam@livepeer.org>
2021-06-10 15:35:08 -0400

    add design system

---
+++ packages/design-system/components/Button.tsx
@@ -0,0 +1,424 @@
+import { styled } from "@modulz/design-system";
+
+const DEFAULT_TAG = "button";
+
+export const Button = styled(DEFAULT_TAG, {
+  // Reset
+  all: "unset",
+  alignItems: "center",
+  boxSizing: "border-box",
+  userSelect: "none",
+  "&::before": {
+    boxSizing: "border-box",
+  },
+  "&::after": {
+    boxSizing: "border-box",
+  },
+
+  // Custom reset?
+  display: "inline-flex",
+  flexShrink: 0,
+  justifyContent: "center",
+  lineHeight: "1",
+  WebkitTapHighlightColor: "rgba(0,0,0,0)",
+
+  // Custom
+  height: "$5",
+  px: "$2",
+  fontFamily: "$untitled",
+  fontSize: "$2",
+  fontWeight: 500,
+  fontVariantNumeric: "tabular-nums",
+
+  "&:disabled": {
+    backgroundColor: "$slate100",
+    boxShadow: "inset 0 0 0 1px $colors$slate600",
+    color: "$slate700",
+    pointerEvents: "none",
+  },
+
+  variants: {
+    size: {
+      "1": {
+        borderRadius: "$1",
+        height: "$5",
+        px: "$2",
+        fontSize: "$1",
+        lineHeight: "$sizes$5",
+      },
+      "2": {
+        borderRadius: "$2",
+        height: "$6",
+        px: "$3",
+        fontSize: "$3",
+        lineHeight: "$sizes$6",
+      },
+      "3": {
+        borderRadius: "$2",
+        height: "$7",
+        px: "$4",
+        fontSize: "$4",
+        lineHeight: "$sizes$7",
+      },
+    },
+    variant: {
+      gray: {
+        backgroundColor: "$loContrast",
+        boxShadow: "inset 0 0 0 1px $colors$slate600",
+        color: "$hiContrast",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$slate700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$slate100",
+          boxShadow: "inset 0 0 0 1px $colors$slate700",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$slate700, 0 0 0 1px $colors$slate700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$slate300",
+          boxShadow: "inset 0 0 0 1px $colors$slate700",
+        },
+      },
+      blue: {
+        backgroundColor: "$blue100",
+        boxShadow: "inset 0 0 0 1px $colors$blue600",
+        color: "$blue900",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$blue700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$blue200",
+          boxShadow: "inset 0 0 0 1px $colors$blue700",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$blue700, 0 0 0 1px $colors$blue700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$blue300",
+          boxShadow: "inset 0 0 0 1px $colors$blue700",
+        },
+      },
+      indigo: {
+        backgroundColor: "$indigo100",
+        boxShadow: "inset 0 0 0 1px $colors$indigo600",
+        color: "$indigo900",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$indigo700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$indigo200",
+          boxShadow: "inset 0 0 0 1px $colors$indigo700",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$indigo700, 0 0 0 1px $colors$indigo700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$indigo300",
+          boxShadow: "inset 0 0 0 1px $colors$indigo700",
+        },
+      },
+      violet: {
+        backgroundColor: "$violet100",
+        boxShadow: "inset 0 0 0 1px $colors$violet600",
+        color: "$violet900",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$violet700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$violet200",
+          boxShadow: "inset 0 0 0 1px $colors$violet700",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$violet700, 0 0 0 1px $colors$violet700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$violet300",
+          boxShadow: "inset 0 0 0 1px $colors$violet700",
+        },
+      },
+      green: {
+        backgroundColor: "$green100",
+        boxShadow: "inset 0 0 0 1px $colors$green600",
+        color: "$green900",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$green700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$green200",
+          boxShadow: "inset 0 0 0 1px $colors$green700",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$green700, 0 0 0 1px $colors$green700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$green300",
+          boxShadow: "inset 0 0 0 1px $colors$green700",
+        },
+      },
+      red: {
+        backgroundColor: "$loContrast",
+        boxShadow: "inset 0 0 0 1px $colors$slate600",
+        color: "$red900",
+        "@hover": {
+          "&:hover": {
+            boxShadow: "inset 0 0 0 1px $colors$slate700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$red200",
+          boxShadow: "inset 0 0 0 1px $colors$red700",
+        },
+        "&:focus": {
+          boxShadow: "inset 0 0 0 1px $colors$red700, 0 0 0 1px $colors$red700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$red300",
+          boxShadow: "inset 0 0 0 1px $colors$red700",
+        },
+      },
+      transparentWhite: {
+        backgroundColor: "hsla(0,100%,100%,.2)",
+        color: "white",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "hsla(0,100%,100%,.25)",
+          },
+        },
+        "&:active": {
+          backgroundColor: "hsla(0,100%,100%,.3)",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px hsla(0,100%,100%,.35), 0 0 0 1px hsla(0,100%,100%,.35)",
+        },
+      },
+      transparentBlack: {
+        backgroundColor: "hsla(0,0%,0%,.2)",
+        color: "black",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "hsla(0,0%,0%,.25)",
+          },
+        },
+        "&:active": {
+          backgroundColor: "hsla(0,0%,0%,.3)",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px hsla(0,0%,0%,.35), 0 0 0 1px hsla(0,0%,0%,.35)",
+        },
+      },
+    },
+    state: {
+      active: {
+        backgroundColor: "$slate300",
+        boxShadow: "inset 0 0 0 1px $colors$slate700",
+        color: "$slate900",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$slate400",
+            boxShadow: "inset 0 0 0 1px $colors$slate700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$slate400",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$slate700, 0 0 0 1px $colors$slate700",
+        },
+      },
+      waiting: {
+        backgroundColor: "$slate300",
+        boxShadow: "inset 0 0 0 1px $colors$slate700",
+        color: "transparent",
+        pointerEvents: "none",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$slate400",
+            boxShadow: "inset 0 0 0 1px $colors$slate700",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$slate400",
+        },
+        "&:focus": {
+          boxShadow: "inset 0 0 0 1px $colors$slate700",
+        },
+      },
+    },
+    ghost: {
+      true: {
+        backgroundColor: "transparent",
+        boxShadow: "none",
+      },
+    },
+  },
+  compoundVariants: [
+    {
+      variant: "gray",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        color: "$hiContrast",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$slate200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$slate300",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$slate700, 0 0 0 1px $colors$slate700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$slate300",
+          boxShadow: "none",
+        },
+      },
+    },
+    {
+      variant: "blue",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$blue200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$blue300",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$blue700, 0 0 0 1px $colors$blue700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$blue300",
+          boxShadow: "none",
+        },
+      },
+    },
+    {
+      variant: "indigo",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$indigo200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$indigo300",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$indigo700, 0 0 0 1px $colors$indigo700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$indigo300",
+          boxShadow: "none",
+        },
+      },
+    },
+    {
+      variant: "violet",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$violet200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$violet300",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$violet700, 0 0 0 1px $colors$violet700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$violet300",
+          boxShadow: "none",
+        },
+      },
+    },
+    {
+      variant: "green",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$green200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$green300",
+        },
+        "&:focus": {
+          boxShadow:
+            "inset 0 0 0 1px $colors$green700, 0 0 0 1px $colors$green700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$green300",
+          boxShadow: "none",
+        },
+      },
+    },
+    {
+      variant: "red",
+      ghost: "true",
+      css: {
+        backgroundColor: "transparent",
+        "@hover": {
+          "&:hover": {
+            backgroundColor: "$red200",
+            boxShadow: "none",
+          },
+        },
+        "&:active": {
+          backgroundColor: "$red300",
+        },
+        "&:focus": {
+          boxShadow: "inset 0 0 0 1px $colors$red700, 0 0 0 1px $colors$red700",
+        },
+        '&[data-radix-popover-trigger][data-state="open"], &[data-radix-dropdown-menu-trigger][data-state="open"]': {
+          backgroundColor: "$red300",
+          boxShadow: "none",
+        },
+      },
+    },
+  ],
+  defaultVariants: {
+    size: "1",
+    variant: "gray",
+  },
+});
---
+++ packages/design-system/components/Link.tsx
@@ -0,0 +1,77 @@
+import { styled } from "@modulz/design-system";
+
+const DEFAULT_TAG = "a";
+
+export const Link = styled(DEFAULT_TAG, {
+  alignItems: "center",
+  gap: "$1",
+  flexShrink: 0,
+  outline: "none",
+  textDecorationLine: "none",
+  textUnderlineOffset: "3px",
+  textDecorationColor: "$slate300",
+  WebkitTapHighlightColor: "rgba(0,0,0,0)",
+  lineHeight: "inherit",
+  "@hover": {
+    "&:hover": {
+      textDecorationLine: "underline",
+    },
+  },
+  "&:focus": {
+    outlineWidth: "2px",
+    outlineStyle: "solid",
+    outlineOffset: "2px",
+    textDecorationLine: "none",
+  },
+  [`& ${Text}`]: {
+    color: "inherit",
+  },
+  variants: {
+    variant: {
+      blue: {
+        color: "$blue900",
+        textDecorationColor: "$blue300",
+        "&:focus": {
+          outlineColor: "$blue700",
+        },
+      },
+      indigo: {
+        color: "$indigo900",
+        textDecorationColor: "$indigo300",
+        "&:focus": {
+          outlineColor: "$indigo700",
+        },
+      },
+      violet: {
+        color: "$violet900",
+        textDecorationColor: "$violet300",
+        "&:focus": {
+          outlineColor: "$violet700",
+        },
+      },
+      subtle: {
+        color: "$slate900",
+        textDecorationColor: "$slate300",
+        "&:focus": {
+          outlineColor: "$slate700",
+        },
+      },
+      contrast: {
+        color: "$hiContrast",
+        textDecoration: "underline",
+        textDecorationColor: "$slate300",
+        "@hover": {
+          "&:hover": {
+            textDecorationColor: "$slate600",
+          },
+        },
+        "&:focus": {
+          outlineColor: "$slate700",
+        },
+      },
+    },
+  },
+  defaultVariants: {
+    variant: "contrast",
+  },
+});
