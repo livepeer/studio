cf83e719b180b93ce0c849d85b92ac4f74aeb793
adamsoffer <adam@livepeer.org>
2021-06-12 20:19:02 -0400

    add delete functionality to streams table

--- packages/www/components/Dashboard/StreamsTable/DeleteStreams.tsx
+++ packages/www/components/Dashboard/StreamsTable/DeleteStreams.tsx
@@ -1,5 +1,4 @@
 import {
-  Box,
   Button,
   Flex,
   AlertDialog,
@@ -8,43 +7,53 @@ import {
   AlertDialogDescription,
   AlertDialogCancel,
   AlertDialogAction,
+  useSnackbar,
   Heading,
   Text,
-  DropdownMenuItem,
+  Box,
 } from "@livepeer.com/design-system";
 import { useState } from "react";
-import { useApi } from "../../../hooks";
 import Spinner from "@components/Dashboard/Spinner";
-import Router from "next/router";
+import { Cross1Icon } from "@radix-ui/react-icons";
 
-const Delete = ({ stream, setStream, ...props }) => {
-  const { deleteStream } = useApi();
+const DeleteStreams = ({ total, onUnselect, onDelete }) => {
   const [saving, setSaving] = useState(false);
   const [open, setOpen] = useState(false);
+  const [openSnackbar] = useSnackbar();
 
   return (
-    <AlertDialog open={open} {...props}>
-      <Box
-        as={DropdownMenuItem}
-        onSelect={(e) => {
-          e.preventDefault();
-          setOpen(true);
-        }}
-        color="red">
-        Delete
-      </Box>
+    <AlertDialog open={open}>
+      <Flex css={{ ai: "center" }}>
+        <Flex css={{ ai: "center", mr: "$3" }}>
+          <Box css={{ fontSize: "$2", color: "$mauve9" }}>{total} selected</Box>
+          <Box css={{ height: 18, width: "1px", bc: "$mauve7", mx: "$3" }} />
+          <Box
+            css={{ cursor: "pointer", fontSize: "$2", color: "$violet11" }}
+            onClick={onUnselect}>
+            Deselect
+          </Box>
+        </Flex>
+        <Button
+          size="2"
+          onClick={() => {
+            setOpen(true);
+          }}
+          css={{ display: "flex", alignItems: "center" }}>
+          <Cross1Icon /> <Box css={{ ml: "$2" }}>Delete</Box>
+        </Button>
+      </Flex>
 
       <AlertDialogContent css={{ maxWidth: 450, px: "$5", pt: "$4", pb: "$4" }}>
         <AlertDialogTitle as={Heading} size="1">
-          Delete stream?
+          Delete {total} stream{total > 1 && "s"}?
         </AlertDialogTitle>
         <AlertDialogDescription
           as={Text}
-          size="2"
+          size="3"
           variant="gray"
-          css={{ mt: "$2", lineHeight: "17px" }}>
-          Are you sure you want to delete stream {stream.name}? Deleting a
-          stream cannot be undone.
+          css={{ mt: "$2", lineHeight: "22px" }}>
+          This will permanently remove the stream{total > 1 && "s"}. This action
+          cannot be undone.
         </AlertDialogDescription>
 
         <Flex css={{ jc: "flex-end", gap: "$3", mt: "$5" }}>
@@ -62,9 +71,8 @@ const Delete = ({ stream, setStream, ...props }) => {
             onClick={async () => {
               try {
                 setSaving(true);
-                await deleteStream(stream.id);
-                Router.replace("/dashboard");
-                setStream({ ...stream });
+                await onDelete();
+                openSnackbar(`${total} stream${total > 1 ? "s" : ""} deleted.`);
                 setSaving(false);
                 setOpen(false);
               } catch (e) {
@@ -90,4 +98,4 @@ const Delete = ({ stream, setStream, ...props }) => {
   );
 };
 
-export default Delete;
+export default DeleteStreams;
