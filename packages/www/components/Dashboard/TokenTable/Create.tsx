ed4f808d0d9bb28ca11304ee61bf5ee4d46fc776
adamsoffer <adam@livepeer.org>
2021-06-16 11:44:55 -0400

    finish api keys view

--- packages/www/components/Dashboard/TokenTable/Create.tsx
+++ packages/www/components/Dashboard/TokenTable/Create.tsx
@@ -14,10 +14,12 @@ import {
   Heading,
   styled,
 } from "@livepeer.com/design-system";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useApi } from "../../../hooks";
 import { PlusIcon } from "@radix-ui/react-icons";
 import Spinner from "@components/Dashboard/Spinner";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { CopyIcon as Copy } from "@radix-ui/react-icons";
 
 const StyledPlusIcon = styled(PlusIcon, {
   mr: "$1",
@@ -26,81 +28,134 @@ const StyledPlusIcon = styled(PlusIcon, {
 const Create = ({ newToken, setNewToken }) => {
   const [creating, setCreating] = useState(false);
   const [tokenName, setTokenName] = useState("");
-  const { getApiTokens, createApiToken } = useApi();
+  const [open, setOpen] = useState(false);
+  const { createApiToken } = useApi();
+  const [isCopied, setCopied] = useState(0);
+
+  useEffect(() => {
+    if (isCopied) {
+      const interval = setTimeout(() => {
+        setCopied(0);
+      }, isCopied);
+      return () => clearTimeout(interval);
+    }
+  }, [isCopied]);
 
   return (
-    <AlertDialog>
-      <AlertDialogTrigger
-        as={Button}
+    <AlertDialog open={open}>
+      <Button
+        onClick={() => setOpen(true)}
         variant="violet"
         size="2"
         css={{ display: "flex", alignItems: "center" }}>
         <StyledPlusIcon /> Create key
-      </AlertDialogTrigger>
+      </Button>
       <AlertDialogContent css={{ maxWidth: 450, px: "$5", pt: "$4", pb: "$4" }}>
-        <AlertDialogTitle as={Heading} size="1">
-          Create key
-        </AlertDialogTitle>
-        <Box
-          as="form"
-          onSubmit={(e) => {
-            e.preventDefault();
-            if (creating) {
-              return;
-            }
-            setCreating(true);
-            createApiToken({ name: tokenName })
-              .then((newToken) => {
-                setNewToken(newToken);
-                setCreating(false);
-              })
-              .catch(() => {
-                setCreating(false);
-              });
-          }}>
-          <AlertDialogDescription
-            as={Text}
-            size="3"
-            variant="gray"
-            css={{ mt: "$2", lineHeight: "22px", mb: "$2" }}>
-            Enter a name for your key to differentiate it from other keys.
-          </AlertDialogDescription>
+        {!newToken && (
+          <>
+            <AlertDialogTitle as={Heading} size="1">
+              Create key
+            </AlertDialogTitle>
+            <Box
+              as="form"
+              onSubmit={async (e) => {
+                e.preventDefault();
+                if (creating) {
+                  return;
+                }
+                setCreating(true);
+                try {
+                  const newToken = await createApiToken({ name: tokenName });
+                  setNewToken(newToken);
+                  setCreating(false);
+                } catch (e) {
+                  setCreating(false);
+                }
+              }}>
+              <AlertDialogDescription
+                as={Text}
+                size="3"
+                variant="gray"
+                css={{ mt: "$2", lineHeight: "22px", mb: "$2" }}>
+                Enter a name for your key to differentiate it from other keys.
+              </AlertDialogDescription>
 
-          <Flex direction="column" gap="2">
-            <TextField
-              size="2"
-              type="text"
-              id="tokenName"
-              autoFocus={true}
-              value={tokenName}
-              onChange={(e) => setTokenName(e.target.value)}
-              placeholder="e.g. New key"
-            />
-          </Flex>
-        </Box>
-        <Flex css={{ jc: "flex-end", gap: "$3", mt: "$4" }}>
-          <AlertDialogCancel size="2" as={Button} ghost>
-            Cancel
-          </AlertDialogCancel>
-          <AlertDialogAction
-            as={Button}
-            size="2"
-            disabled={creating}
-            type="submit"
-            variant="violet">
-            {creating && (
-              <Spinner
-                css={{
-                  color: "$hiContrast",
-                  width: 16,
-                  height: 16,
-                  mr: "$2",
+              <Flex direction="column" gap="2">
+                <TextField
+                  size="2"
+                  type="text"
+                  required
+                  id="tokenName"
+                  autoFocus={true}
+                  value={tokenName}
+                  onChange={(e) => setTokenName(e.target.value)}
+                  placeholder="e.g. New key"
+                />
+              </Flex>
+
+              <Flex css={{ jc: "flex-end", gap: "$3", mt: "$4" }}>
+                <AlertDialogCancel size="2" as={Button} ghost>
+                  Cancel
+                </AlertDialogCancel>
+                <Button
+                  size="2"
+                  disabled={creating}
+                  type="submit"
+                  variant="violet">
+                  {creating && (
+                    <Spinner
+                      css={{
+                        color: "$hiContrast",
+                        width: 16,
+                        height: 16,
+                        mr: "$2",
+                      }}
+                    />
+                  )}
+                  Create
+                </Button>
+              </Flex>
+            </Box>
+          </>
+        )}
+        {newToken && (
+          <Box>
+            <AlertDialogTitle as={Heading} size="1">
+              Token Created
+            </AlertDialogTitle>
+            <AlertDialogDescription
+              as={Text}
+              size="3"
+              variant="gray"
+              css={{ mt: "$2", lineHeight: "22px", mb: "$2" }}>
+              <Box>
+                <Box css={{ mb: "$2" }}>Here's your new API key:</Box>
+                <CopyToClipboard
+                  text={newToken.id}
+                  onCopy={() => setCopied(2000)}>
+                  <Button
+                    ghost
+                    size="2"
+                    variant="transparentBlack"
+                    css={{ cursor: "pointer" }}>
+                    {newToken.id}
+                  </Button>
+                </CopyToClipboard>
+              </Box>
+            </AlertDialogDescription>
+            <Flex css={{ jc: "flex-end", gap: "$3", mt: "$4" }}>
+              <Button
+                onClick={() => {
+                  setNewToken(null);
+                  setTokenName("");
+                  setOpen(false);
                 }}
-              />
-            )}
-            Create
-          </AlertDialogAction>
-        </Flex>
+                size="2">
+                Close
+              </Button>
+            </Flex>
+          </Box>
+        )}
       </AlertDialogContent>
     </AlertDialog>
   );
