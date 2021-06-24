912f31f6f6d2c2824b97ed83603038a954c6f538
Julian Benegas <julianbenegas99@gmail.com>
2021-06-24 15:52:34 -0300

    bring back token table

--- packages/www/components/Dashboard/TokenTable/index.tsx
+++ packages/www/components/Dashboard/TokenTable/index.tsx
@@ -4,10 +4,18 @@ import {
   Button,
   Box,
   Flex,
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+  useSnackbar,
+  Text,
 } from "@livepeer.com/design-system";
-import { useCallback, useEffect, useMemo, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { useApi } from "../../../hooks";
-import Table from "components/Dashboard/Table";
+import Table, { Fetcher, useTableState } from "components/Dashboard/Table";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import TextCell, { TextCellProps } from "components/Dashboard/Table/cells/text";
 import { Column, Row } from "react-table";
@@ -15,10 +23,13 @@ import DateCell, { DateCellProps } from "components/Dashboard/Table/cells/date";
 import { dateSort, stringSort } from "components/Dashboard/Table/sorts";
 import { SortTypeArgs } from "components/Dashboard/Table/types";
 import { CopyIcon } from "@radix-ui/react-icons";
-import Create from "./Create";
-import Delete from "./Delete";
+import CreateTokenDialog from "./CreateTokenDialog";
+import { Cross1Icon, PlusIcon } from "@radix-ui/react-icons";
+import Spinner from "@components/Dashboard/Spinner";
+import { useToggleState } from "hooks/use-toggle-state";
 
 type TokenTableData = {
+  id: string;
   name: TextCellProps;
   token: TextCellProps;
   lastUsed: DateCellProps;
@@ -38,24 +49,12 @@ const TokenTable = ({
   title?: string;
   userId: string;
 }) => {
-  const [tokens, setTokens] = useState([]);
-  const [newToken, setNewToken] = useState(null);
-  const [selectedTokens, setSelectedTokens] = useState([]);
-  const [onUnselect, setOnUnselect] = useState();
   const { getApiTokens, deleteApiToken } = useApi();
-
-  useEffect(() => {
-    getApiTokens(userId)
-      .then((result) => {
-        const [tokens, nextCursor, resp] = result;
-        if (resp.ok && Array.isArray(tokens)) {
-          setTokens(tokens);
-        } else {
-          console.error(result);
-        }
-      })
-      .catch((err) => console.error(err)); // todo: surface this
-  }, [userId, newToken]);
+  const tableProps = useTableState({ pageSize: 30 });
+  const deleteDialogState = useToggleState();
+  const createDialogState = useToggleState();
+  const [savingDeleteDialog, setSavingDeleteDialog] = useState(false);
+  const [openSnackbar] = useSnackbar();
 
   const columns: Column<TokenTableData>[] = useMemo(
     () => [
@@ -130,77 +129,153 @@ const TokenTable = ({
     );
   };
 
-  const data: TokenTableData[] = useMemo(() => {
-    return tokens.map((token) => {
-      return {
-        token: {
-          children: <Key token={token} />,
-        },
-        name: {
-          children: token.name,
-        },
-        createdAt: {
-          date: new Date(token.createdAt),
-          fallback: <Box css={{ color: "$mauve8" }}>—</Box>,
-        },
-        lastUsed: {
-          date: new Date(token.lastSeen),
-          fallback: <i>unused</i>,
-        },
-      };
-    });
-  }, [tokens]);
+  const fetcher: Fetcher<TokenTableData> = useCallback(async () => {
+    const [tokens, nextCursor, resp] = await getApiTokens(userId);
+    if (!resp.ok || !Array.isArray(tokens)) {
+      throw resp;
+    }
 
-  const handleRowSelectionChange = useCallback(
-    (rows: Row<TokenTableData>[]) => {
-      setSelectedTokens(
-        rows.map((r: any) => tokens.find((t) => t.id === r.original.id))
-      );
-    },
-    [tokens]
-  );
+    return {
+      nextCursor,
+      rows: tokens.map((token) => {
+        return {
+          id: token.id,
+          token: {
+            children: <Key token={token} />,
+          },
+          name: {
+            children: token.name,
+          },
+          createdAt: {
+            date: new Date(token.createdAt),
+            fallback: <Box css={{ color: "$mauve8" }}>—</Box>,
+          },
+          lastUsed: {
+            date: new Date(token.lastSeen),
+            fallback: <i>unused</i>,
+          },
+        };
+      }),
+    };
+  }, [userId]);
 
   return (
-    <Box>
-      <Flex
-        align="end"
-        justify="between"
-        css={{
-          mb: "$5",
-        }}>
-        <Heading size="2" css={{ fontWeight: 600 }}>
-          {title}
-        </Heading>
+    <>
+      {/* TODO fix this before merging! @julianbenegas */}
+      <Table
+        {...tableProps}
+        tableId="api-tokens"
+        header={
+          <>
+            <Heading size="2" css={{ fontWeight: 600 }}>
+              {title}
+            </Heading>
+          </>
+        }
+        columns={columns}
+        fetcher={fetcher}
+        rowSelection="all"
+        selectAction={{
+          onClick: deleteDialogState.onOn,
+          children: (
+            <>
+              <Cross1Icon /> <Box css={{ ml: "$2" }}>Delete</Box>
+            </>
+          ),
+          css: { display: "flex", alignItems: "center" },
+          // @ts-ignore
+          size: "2",
+        }}
+        createAction={{
+          onClick: createDialogState.onOn,
+          variant: "violet",
+          css: { display: "flex", alignItems: "center" },
+          children: (
+            <>
+              <PlusIcon />{" "}
+              <Box as="span" css={{ ml: "$2" }}>
+                Create key
+              </Box>
+            </>
+          ),
+        }}
+      />
 
-        <Flex css={{ alignItems: "center" }}>
-          {!!selectedTokens.length && (
-            <Delete
-              onUnselect={onUnselect}
-              total={selectedTokens.length}
-              onDelete={async () => {
-                for (const selectedToken of selectedTokens) {
-                  await deleteApiToken(selectedToken.id);
+      {/* Delete dialog */}
+      <AlertDialog open={deleteDialogState.on}>
+        <AlertDialogContent
+          css={{ maxWidth: 450, px: "$5", pt: "$4", pb: "$4" }}>
+          <AlertDialogTitle as={Heading} size="1">
+            Delete {tableProps.state.selectedRows.length} API token
+            {tableProps.state.selectedRows.length > 1 && "s"}?
+          </AlertDialogTitle>
+          <AlertDialogDescription
+            as={Text}
+            size="3"
+            variant="gray"
+            css={{ mt: "$2", lineHeight: "22px" }}>
+            This will permanently remove the API token
+            {tableProps.state.selectedRows.length > 1 && "s"}. This action
+            cannot be undone.
+          </AlertDialogDescription>
+
+          <Flex css={{ jc: "flex-end", gap: "$3", mt: "$5" }}>
+            <AlertDialogCancel
+              size="2"
+              onClick={deleteDialogState.onOff}
+              as={Button}
+              ghost>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              as={Button}
+              size="2"
+              disabled={savingDeleteDialog}
+              onClick={async () => {
+                try {
+                  setSavingDeleteDialog(true);
+                  const promises = tableProps.state.selectedRows.map(
+                    async (row) => {
+                      return deleteApiToken(row.original.id as string);
+                    }
+                  );
+                  await Promise.all(promises);
+                  await tableProps.state.swrState?.revalidate();
+                  openSnackbar(
+                    `${tableProps.state.selectedRows.length} stream${
+                      tableProps.state.selectedRows.length > 1 ? "s" : ""
+                    } deleted.`
+                  );
+                  deleteDialogState.onOff();
+                } finally {
+                  setSavingDeleteDialog(false);
                 }
               }}
-            />
-          )}
-          {!selectedTokens.length && (
-            <Create newToken={newToken} setNewToken={setNewToken} />
-          )}
-        </Flex>
-      </Flex>
+              variant="red">
+              {savingDeleteDialog && (
+                <Spinner
+                  css={{
+                    color: "$hiContrast",
+                    width: 16,
+                    height: 16,
+                    mr: "$2",
+                  }}
+                />
+              )}
+              Delete
+            </AlertDialogAction>
+          </Flex>
+        </AlertDialogContent>
+      </AlertDialog>
 
-      <Box css={{ mb: "$5" }}>
-        {/* TODO fix this before merging! @julianbenegas */}
-        {/* <Table
-          setOnUnselect={setOnUnselect}
-          columns={columns}
-          data={data}
-          rowSelection="all"
-          onRowSelectionChange={handleRowSelectionChange}
-        /> */}
-      </Box>
-    </Box>
+      {/* Create dialog */}
+      <CreateTokenDialog
+        isOpen={createDialogState.on}
+        onClose={createDialogState.onOff}
+        onOpenChange={createDialogState.onToggle}
+        onCreateSuccess={tableProps.state.swrState?.revalidate}
+      />
+    </>
   );
 };
 
