8847139918e772a345427d430028e0fbbb08c7eb
adamsoffer <adam@livepeer.org>
2021-06-26 13:27:48 -0400

    [wip] stream detail view

--- packages/www/components/Dashboard/StreamDetails/Delete.tsx
+++ packages/www/components/Dashboard/StreamDetails/Delete.tsx
@@ -4,32 +4,31 @@ import {
   Flex,
   Dialog,
   DialogContent,
-  DialogClose,
   Text,
-  Switch,
+  DropdownMenuItem,
 } from "@livepeer.com/design-system";
 import { useState } from "react";
 import { useApi } from "../../../hooks";
 import Spinner from "@components/Dashboard/Spinner";
-import { Stream } from "@livepeer.com/api";
+import Router from "next/router";
 
 const Delete = ({ stream, setStream, ...props }) => {
-  const initialMessage = `Are you sure you want to terminate (stop running live) stream
-      ${stream.name}? Terminating a stream will break RTMPconnection.`;
-
-  const { terminateStream } = useApi();
+  const { deleteStream } = useApi();
   const [saving, setSaving] = useState(false);
   const [open, setOpen] = useState(false);
-  const [message, setMessage] = useState(initialMessage);
 
   return (
     <Dialog open={open} {...props}>
       <Box
-        css={{ color: "$red9" }}
+        as={DropdownMenuItem}
+        onSelect={(e) => {
+          e.preventDefault();
+        }}
+        color="red"
         onClick={() => {
           setOpen(true);
         }}>
-        Terminate
+        <Box>Delete</Box>
       </Box>
 
       <DialogContent css={{ p: 0 }}>
@@ -47,10 +46,11 @@ const Delete = ({ stream, setStream, ...props }) => {
               lineHeight: "20px",
               mb: "$3",
             }}>
-            Are you you want to terminate this stream?
+            Are you you want to delete this stream?
           </Text>
           <Text size="2" variant="gray" css={{ lineHeight: "17px" }}>
-            {message}
+            Are you sure you want to delete stream {stream.name}? Deleting a
+            stream cannot be undone.`
           </Text>
           <Flex
             css={{
@@ -61,7 +61,6 @@ const Delete = ({ stream, setStream, ...props }) => {
             <Button
               disabled={saving}
               onClick={() => {
-                setMessage(initialMessage);
                 setOpen(false);
               }}
               size="2"
@@ -75,13 +74,12 @@ const Delete = ({ stream, setStream, ...props }) => {
               onClick={async () => {
                 try {
                   setSaving(true);
-                  const res = await terminateStream(stream.id);
+                  await deleteStream(stream.id);
+                  Router.replace("/dashboard");
                   setStream({ ...stream });
                   setSaving(false);
-                  setMessage(initialMessage);
                   setOpen(false);
                 } catch (e) {
-                  setMessage(e.toString());
                   setSaving(false);
                 }
               }}
@@ -96,7 +94,7 @@ const Delete = ({ stream, setStream, ...props }) => {
                   }}
                 />
               )}
-              Terminate
+              Delete
             </Button>
           </Flex>
         </Box>
