2146dd921c0ec8e0085ac3c220c85fa0f128aa77
adamsoffer <adam@livepeer.org>
2021-06-26 01:49:57 -0400

    add webhooks table

---
+++ packages/www/components/Dashboard/WebhooksTable/CreateWebhookDialog.tsx
@@ -0,0 +1,133 @@
+import {
+  Box,
+  Button,
+  Flex,
+  Dialog,
+  DialogContent,
+  DialogClose,
+  TextField,
+  Text,
+  Label,
+  styled,
+} from "@livepeer.com/design-system";
+import { useState } from "react";
+import Spinner from "@components/Dashboard/Spinner";
+
+const CreateWebhookDialog = ({
+  isOpen,
+  onOpenChange,
+  onCreate,
+}: {
+  isOpen: boolean;
+  onOpenChange: (isOpen: boolean) => void;
+  onCreate: ({ event: string, name: string, url: string }) => Promise<void>;
+}) => {
+  const [creating, setCreating] = useState(false);
+  const [webhookName, setWebhookName] = useState("");
+  const [webhookUrl, setWebhookUrl] = useState("");
+  console.log(webhookName, webhookUrl);
+  return (
+    <Dialog open={isOpen} onOpenChange={onOpenChange}>
+      <DialogContent css={{ p: 0 }}>
+        <Box
+          css={{
+            p: "$4",
+            fontWeight: 500,
+            borderBottom: "1px solid",
+            borderColor: "$slate6",
+          }}>
+          <Text size="4" as="h6">
+            Create a new webhook
+          </Text>
+        </Box>
+
+        <Box
+          as="form"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            if (creating) {
+              return;
+            }
+            setCreating(true);
+            try {
+              await onCreate({
+                event: "stream.started",
+                name: webhookName,
+                url: webhookUrl,
+              });
+            } catch (error) {
+              console.error(error);
+            } finally {
+              setCreating(false);
+            }
+          }}>
+          <Box
+            css={{
+              px: "$4",
+              pt: "$5",
+              pb: "$5",
+              borderBottom: "1px solid",
+              borderColor: "$slate6",
+            }}>
+            <Flex direction="column" gap="2">
+              <Label htmlFor="name">Name</Label>
+              <TextField
+                required
+                size="2"
+                type="text"
+                id="name"
+                autoFocus={true}
+                value={webhookName}
+                onChange={(e) => setWebhookName(e.target.value)}
+                placeholder="My Webhook"
+              />
+              <Text size="1" css={{ fontWeight: 500, color: "$gray9" }}>
+                A-Z, a-z, 0-9, -, _, ~ only
+              </Text>
+              <Label htmlFor="url">URL</Label>
+              <TextField
+                required
+                size="2"
+                type="url"
+                id="url"
+                value={webhookUrl}
+                onChange={(e) => setWebhookUrl(e.target.value)}
+              />
+            </Flex>
+          </Box>
+          <Box css={{ py: "$3", px: "$4" }}>
+            <Flex css={{ ai: "center", jc: "flex-end" }}>
+              <DialogClose
+                disabled={creating}
+                as={Button}
+                size="2"
+                css={{ mr: "$2" }}>
+                Cancel
+              </DialogClose>
+              <Button
+                css={{ display: "flex", ai: "center" }}
+                type="submit"
+                size="2"
+                disabled={creating}
+                variant="violet">
+                {creating && (
+                  <Spinner
+                    css={{
+                      color: "$hiContrast",
+                      width: 16,
+                      height: 16,
+                      mr: "$2",
+                    }}
+                  />
+                )}
+                Create
+              </Button>
+            </Flex>
+          </Box>
+        </Box>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CreateWebhookDialog;
---
+++ packages/www/components/Dashboard/WebhooksTable/index.tsx
@@ -0,0 +1,266 @@
+import { useCallback, useMemo } from "react";
+import { useApi } from "../../../hooks";
+import Table, { Fetcher, useTableState } from "components/Dashboard/Table";
+import TextCell, { TextCellProps } from "components/Dashboard/Table/cells/text";
+import DateCell, { DateCellProps } from "components/Dashboard/Table/cells/date";
+import {
+  dateSort,
+  numberSort,
+  stringSort,
+} from "components/Dashboard/Table/sorts";
+import { SortTypeArgs } from "components/Dashboard/Table/types";
+import { Column } from "react-table";
+import {
+  Box,
+  Flex,
+  Heading,
+  Link as A,
+  AlertDialog,
+  AlertDialogTitle,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogCancel,
+  AlertDialogAction,
+  Button,
+  Text,
+  useSnackbar,
+} from "@livepeer.com/design-system";
+import { useToggleState } from "hooks/use-toggle-state";
+import { Cross1Icon, PlusIcon } from "@radix-ui/react-icons";
+import Spinner from "components/Dashboard/Spinner";
+import CreateWebhookDialog from "components/Dashboard/WebhooksTable/CreateWebhookDialog";
+import { useRouter } from "next/router";
+
+type WebhooksTableData = {
+  name: TextCellProps;
+  url: TextCellProps;
+  created: DateCellProps;
+};
+
+const WebhooksTable = ({ title = "Webhooks" }: { title?: string }) => {
+  const router = useRouter();
+  const { user, getWebhooks, deleteWebhook, createWebhook } = useApi();
+  const tableProps = useTableState({ pageSize: 50 });
+  const deleteDialogState = useToggleState();
+  const savingState = useToggleState();
+  const [openSnackbar] = useSnackbar();
+  const createDialogState = useToggleState();
+  const { state, stateSetter } = useTableState<WebhooksTableData>({
+    pageSize: 20,
+  });
+
+  const columns: Column<WebhooksTableData>[] = useMemo(
+    () => [
+      {
+        Header: "URL",
+        accessor: "url",
+        Cell: TextCell,
+        sortType: (...params: SortTypeArgs) =>
+          numberSort("original.url.children", ...params),
+      },
+      {
+        Header: "Name",
+        accessor: "name",
+        Cell: TextCell,
+        sortType: (...params: SortTypeArgs) =>
+          stringSort("original.name.children", ...params),
+      },
+      {
+        Header: "Created at",
+        accessor: "created",
+        Cell: DateCell,
+        sortType: (...params: SortTypeArgs) =>
+          dateSort("original.created.date", ...params),
+      },
+    ],
+    []
+  );
+
+  const fetcher: Fetcher<WebhooksTableData> = useCallback(
+    async (state) => {
+      const [webhooks, nextCursor] = await getWebhooks(
+        false,
+        true,
+        state.order,
+        null,
+        state.pageSize,
+        state.cursor
+      );
+      return {
+        nextCursor,
+        rows: webhooks.map((webhook: any) => {
+          return {
+            name: {
+              children: webhook.name,
+              href: `/dashboard/developers/webhooks/${webhook.id}`,
+              css: {
+                cursor: "pointer",
+              },
+            },
+            url: {
+              children: webhook.url,
+              href: `/dashboard/developers/webhooks/${webhook.id}`,
+              css: {
+                cursor: "pointer",
+              },
+            },
+            created: {
+              date: new Date(webhook.createdAt),
+              fallback: <i>unseen</i>,
+              href: `/dashboard/developers/webhooks/${webhook.id}`,
+              css: {
+                cursor: "pointer",
+              },
+            },
+          };
+        }),
+      };
+    },
+    [getWebhooks, user.id]
+  );
+
+  // const onDeleteStreams = useCallback(async () => {
+  //   if (tableProps.state.selectedRows.length === 1) {
+  //     await deleteStream(tableProps.state.selectedRows[0].id);
+  //     await tableProps.state.swrState?.revalidate();
+  //     deleteDialogState.onOff();
+  //   } else if (tableProps.state.selectedRows.length > 1) {
+  //     await deleteStreams(tableProps.state.selectedRows.map((s) => s.id));
+  //     await tableProps.state.swrState?.revalidate();
+  //     deleteDialogState.onOff();
+  //   }
+  // }, [
+  //   deleteStream,
+  //   deleteStreams,
+  //   deleteDialogState.onOff,
+  //   tableProps.state.selectedRows.length,
+  //   tableProps.state.swrState?.revalidate,
+  // ]);
+
+  return (
+    <>
+      <Table
+        {...tableProps}
+        tableId="webhooks"
+        columns={columns}
+        fetcher={fetcher}
+        state={state}
+        stateSetter={stateSetter}
+        rowSelection="all"
+        header={
+          <>
+            <Heading size="2" css={{ fontWeight: 600 }}>
+              {title}
+            </Heading>
+          </>
+        }
+        selectAction={{
+          onClick: deleteDialogState.onOn,
+          children: (
+            <>
+              <Cross1Icon />{" "}
+              <Box css={{ ml: "$2" }} as="span">
+                Delete
+              </Box>
+            </>
+          ),
+        }}
+        createAction={{
+          onClick: createDialogState.onOn,
+          variant: "violet",
+          css: { display: "flex", alignItems: "center" },
+          children: (
+            <>
+              <PlusIcon />{" "}
+              <Box as="span" css={{ ml: "$2" }}>
+                Create webhook
+              </Box>
+            </>
+          ),
+        }}
+      />
+
+      <AlertDialog open={deleteDialogState.on}>
+        <AlertDialogContent
+          css={{ maxWidth: 450, px: "$5", pt: "$4", pb: "$4" }}>
+          <AlertDialogTitle as={Heading} size="1">
+            Delete {tableProps.state.selectedRows.length} webhook
+            {tableProps.state.selectedRows.length > 1 && "s"}?
+          </AlertDialogTitle>
+          <AlertDialogDescription
+            as={Text}
+            size="3"
+            variant="gray"
+            css={{ mt: "$2", lineHeight: "22px" }}>
+            This will permanently remove the webhook
+            {tableProps.state.selectedRows.length > 1 && "s"}. This action
+            cannot be undone.
+          </AlertDialogDescription>
+
+          <Flex css={{ jc: "flex-end", gap: "$3", mt: "$5" }}>
+            <AlertDialogCancel
+              size="2"
+              onClick={deleteDialogState.onOff}
+              as={Button}
+              ghost>
+              Cancel
+            </AlertDialogCancel>
+            <AlertDialogAction
+              as={Button}
+              size="2"
+              disabled={savingState.on}
+              onClick={async () => {
+                try {
+                  savingState.onOn();
+                  //await onDeleteStreams();
+                  openSnackbar(
+                    `${tableProps.state.selectedRows.length} webhooks${
+                      tableProps.state.selectedRows.length > 1 ? "s" : ""
+                    } deleted.`
+                  );
+                  savingState.onOff();
+                  deleteDialogState.onOff();
+                } catch (e) {
+                  savingState.onOff();
+                }
+              }}
+              variant="red">
+              {savingState.on && (
+                <Spinner
+                  css={{
+                    color: "$hiContrast",
+                    width: 16,
+                    height: 16,
+                    mr: "$2",
+                  }}
+                />
+              )}
+              Delete
+            </AlertDialogAction>
+          </Flex>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      {/* Create stream dialog */}
+      <CreateWebhookDialog
+        isOpen={createDialogState.on}
+        onOpenChange={createDialogState.onToggle}
+        onCreate={async ({ event, name, url }) => {
+          const newWebhook = await createWebhook({
+            event,
+            name,
+            url,
+          });
+          await state.swrState.revalidate();
+          const query = router.query.admin === "true" ? { admin: true } : {};
+          await router.push({
+            pathname: `/dashboard/developers/webhooks/${newWebhook.id}`,
+            query,
+          });
+        }}
+      />
+    </>
+  );
+};
+
+export default WebhooksTable;
