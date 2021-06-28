c433819d3958ac355da3261ba5f0dfc03b95b36c
adamsoffer <adam@livepeer.org>
2021-06-28 16:53:28 -0400

    remove mailchimp dependency

--- packages/www/hooks/index.tsx
+++ packages/www/hooks/index.tsx
@@ -2,3 +2,4 @@ export { default as useLoggedIn } from "./use-logged-in";
 export { default as useApi } from "./use-api";
 export { default as usePageVisibility } from "./use-page-visibility";
 export { default as useDebounce } from "./use-debounce";
+export { default as useMailchimp } from "./use-mailchimp";
