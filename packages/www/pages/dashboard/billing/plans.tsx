e6e52a4c7d87f5e1876867e6f00dbff890e2e98b
adamsoffer <adam@livepeer.org>
2021-07-02 10:33:58 -0400

    add billing and plans views

--- packages/www/pages/dashboard/billing/plans.tsx
+++ packages/www/pages/dashboard/billing/plans.tsx
@@ -1,8 +1,9 @@
 import Layout from "../../../layouts/dashboard";
 import { Box, Flex, Heading } from "@livepeer.com/design-system";
 import { useApi, useLoggedIn } from "hooks";
+import Plans from "components/Dashboard/Plans";
 
-const Plans = () => {
+const PlansPage = () => {
   useLoggedIn();
   const { user } = useApi();
 
@@ -17,7 +18,7 @@ const Plans = () => {
         { title: "Plans" },
       ]}>
       <Box css={{ p: "$6" }}>
-        <Box css={{ mb: "$8" }}>
+        <Box css={{ mb: "$6" }}>
           <Flex
             justify="between"
             align="end"
@@ -42,9 +43,15 @@ const Plans = () => {
             </Heading>
           </Flex>
         </Box>
+        <Plans
+          dashboard={true}
+          stripeProductId={
+            user?.stripeProductId ? user.stripeProductId : "prod_0"
+          }
+        />
       </Box>
     </Layout>
   );
 };
 
-export default Plans;
+export default PlansPage;
