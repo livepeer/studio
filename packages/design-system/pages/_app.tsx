de0e8d88c61efcad17f523336d22578781eddf08
adamsoffer <adam@livepeer.org>
2021-07-06 18:13:39 -0400

    add design system

---
+++ packages/design-system/pages/_app.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Head from "next/head";
+import "../styles.css";
+import { DesignSystemProvider } from "@modulz/design-system";
+
+function App({ Component, pageProps }) {
+  return (
+    <DesignSystemProvider>
+      <div>
+        <Head>
+          <title>Design System</title>
+          <link
+            rel="stylesheet"
+            href="https://develop.modulz.app/fonts/fonts.css"
+          />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </Head>
+
+        <Component {...pageProps} />
+      </div>
+    </DesignSystemProvider>
+  );
+}
+
+export default App;
