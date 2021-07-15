7dfbd1a1d9fc2c2c69e1ba43eaee1ec6dae6193f
adamsoffer <adam@livepeer.org>
2021-07-15 01:22:28 -0400

    add fallback

--- packages/www/pages/use-cases/[slug].tsx
+++ packages/www/pages/use-cases/[slug].tsx
@@ -6,8 +6,27 @@ import Prefooter from "components/Redesign/Prefooter";
 import { GraphQLClient, request } from "graphql-request";
 import { print } from "graphql/language/printer";
 import allUseCases from "../../queries/allUseCases.gql";
+import { useRouter } from "next/router";
+import { Box } from "@livepeer.com/design-system";
 
 const UseCase = ({ hero, why, caseStudy, preview }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return (
+      <Layout>
+        <Box
+          sx={{
+            py: 5,
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}>
+          Loading
+        </Box>
+      </Layout>
+    );
+  }
+
   return (
     <Layout preview={preview}>
       <Hero
