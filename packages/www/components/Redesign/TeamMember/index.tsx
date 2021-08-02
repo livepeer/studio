7bff45841d3edba05408cee3d31a65bcad147964
adamsoffer <adam@livepeer.org>
2021-08-01 22:51:23 -0400

    style team page

--- packages/www/components/Redesign/TeamMember/index.tsx
+++ packages/www/components/Redesign/TeamMember/index.tsx
@@ -1,9 +1,14 @@
-/** @jsx jsx */
-import { jsx } from "theme-ui";
-import { Box, Flex, Image, Heading, Link } from "@theme-ui/components";
+import {
+  Box,
+  Flex,
+  Heading,
+  Text,
+  Link as A,
+} from "@livepeer.com/design-system";
 import imageUrlBuilder from "@sanity/image-url";
-import client from "../../lib/client";
+import client from "lib/client";
 import { FaTwitter, FaMedium, FaGithub, FaLinkedin } from "react-icons/fa";
+import Image from "next/image";
 
 const TeamMember = ({
   fullname,
@@ -13,18 +18,19 @@ const TeamMember = ({
   github,
   linkedin,
   medium,
+  css = {},
   ...props
 }) => {
   const builder = imageUrlBuilder(client as any);
 
   return (
     <Box
-      sx={{
+      css={{
         textAlign: "center",
         borderRadius: 24,
-        border: "1px solid #F5F5F5",
+        border: "1px solid $colors$mauve5",
         p: 40,
-        bg: "background",
+        bc: "$mauve2",
         display: "flex",
         flexDirection: "column",
         justifyContent: "space-between",
@@ -33,102 +39,102 @@ const TeamMember = ({
           boxShadow:
             "0px 2px 1px rgba(0, 0, 0, 0.04), 0px 16px 40px rgba(0, 0, 0, 0.04)",
         },
+        ...css,
       }}
       {...props}>
-      <div>
-        <Image
-          sx={{
+      <Box>
+        <Box
+          as={Image}
+          css={{
             borderRadius: 1000,
-            width: 130,
-            height: 130,
             objectFit: "cover",
-            m: "0 auto 8px",
           }}
           width={130}
           height={130}
-          className="lazyload"
-          data-src={builder.image(image).url()}
+          src={builder.image(image).url()}
         />
-        <Box sx={{ mb: 3 }}>
+        <Box css={{ mb: "$4" }}>
           <Heading
             as="h3"
-            sx={{
-              mb: 2,
-              mt: 3,
+            css={{
+              mb: "$1",
+              mt: "$3",
               fontWeight: 500,
               fontSize: "20px",
             }}>
             {fullname}
           </Heading>
-          <Box sx={{ fontSize: 2, color: "gray" }}>{role}</Box>
+          <Text size="3" variant="gray" css={{ color: "$mauve9" }}>
+            {role}
+          </Text>
         </Box>
-      </div>
+      </Box>
       <Flex
-        sx={{
+        css={{
           maxWidth: 120,
-          margin: "0 auto",
+          mx: "auto",
           justifyContent: "center",
           alignItems: "center",
         }}>
         {twitter && (
-          <Link
-            sx={{
-              "&:not(:last-of-type)": { mr: 3 },
-              color: "black",
-              fontSize: 3,
+          <A
+            css={{
+              "&:not(:last-of-type)": { mr: "$3" },
+              color: "$hiContrast",
+              fontSize: "$3",
               transition: "color .15s",
-              "&:hover": { color: "primary" },
+              "&:hover": { color: "$violet9" },
             }}
             href={twitter}
             target="_blank"
             rel="noopener noreferrer">
             <FaTwitter />
-          </Link>
+          </A>
         )}
         {linkedin && (
-          <Link
-            sx={{
-              "&:not(:last-of-type)": { mr: 3 },
-              color: "black",
-              fontSize: 3,
+          <A
+            css={{
+              "&:not(:last-of-type)": { mr: "$3" },
+              color: "$hiContrast",
+              fontSize: "$3",
               transition: "color .15s",
-              "&:hover": { color: "primary" },
+              "&:hover": { color: "$violet9" },
             }}
             href={linkedin}
             target="_blank"
             rel="noopener noreferrer">
             <FaLinkedin />
-          </Link>
+          </A>
         )}
         {medium && (
-          <Link
-            sx={{
-              "&:not(:last-of-type)": { mr: 3 },
-              color: "black",
-              fontSize: 3,
+          <A
+            css={{
+              "&:not(:last-of-type)": { mr: "$3" },
+              color: "$hiContrast",
+              fontSize: "$3",
               transition: "color .15s",
-              "&:hover": { color: "primary" },
+              "&:hover": { color: "$violet9" },
             }}
             href={medium}
             target="_blank"
             rel="noopener noreferrer">
             <FaMedium />
-          </Link>
+          </A>
         )}
         {github && (
-          <Link
-            sx={{
-              "&:not(:last-of-type)": { mr: 3 },
-              color: "black",
-              fontSize: 3,
+          <A
+            css={{
+              "&:not(:last-of-type)": { mr: "$3" },
+              color: "$hiContrast",
+              fontSize: "$3",
               transition: "color .15s",
-              "&:hover": { color: "primary" },
+              "&:hover": { color: "$violet9" },
             }}
             href={github}
             target="_blank"
             rel="noopener noreferrer">
             <FaGithub />
-          </Link>
+          </A>
         )}
       </Flex>
     </Box>
---
+++ packages/www/components/Redesign/TeamSection/index.tsx
@@ -0,0 +1,48 @@
+import { Container, Grid } from "@livepeer.com/design-system";
+import TeamMember from "components/Redesign/TeamMember";
+
+const TeamSection = ({ teamMembers }) => {
+  return (
+    <Grid
+      gap={3}
+      css={{
+        gridTemplateColumns: "repeat(1,1fr)",
+        "@bp1": {
+          gridTemplateColumns: "repeat(2,1fr)",
+        },
+        "@bp2": {
+          gridTemplateColumns: "repeat(3,1fr)",
+        },
+        "@bp3": {
+          gridTemplateColumns: "repeat(4,1fr)",
+        },
+      }}>
+      {teamMembers.map((t, i) => (
+        <TeamMember
+          key={i}
+          fullname={t.fullname}
+          role={t.role}
+          image={t.image}
+          twitter={t.twitter}
+          linkedin={t.linkedin}
+          github={t.github}
+          medium={t.medium}
+          css={{
+            flex: "1 1 100%",
+            "@bp1": {
+              flex: "1 1 50%",
+            },
+            "@bp2": {
+              flex: "1 1 33%",
+            },
+            "@bp3": {
+              flex: "1 1 25%",
+            },
+          }}
+        />
+      ))}
+    </Grid>
+  );
+};
+
+export default TeamSection;
