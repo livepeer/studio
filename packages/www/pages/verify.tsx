d920e3c7541204d70c78ef560e68d239cb0ca675
adamsoffer <adam@livepeer.org>
2021-08-04 17:50:08 -0400

    fix verify page layout

--- packages/www/pages/verify.tsx
+++ packages/www/pages/verify.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import Layout from "layouts/admin";
+import Layout from "layouts/main";
 import { useLoggedIn, useApi } from "hooks";
 import {
   Container,
