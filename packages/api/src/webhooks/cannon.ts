021512570eb3749788e8f892c6273289a854b0b8
M Yahya <ya7yaz@gmail.com>
2021-08-05 15:42:11 +0200

    moving recording ready logic and removing onTrigger func

--- packages/api/src/webhooks/cannon.ts
+++ packages/api/src/webhooks/cannon.ts
@@ -71,6 +71,25 @@ export default class WebhookCannon {
     }
     let event = message;
 
+    const since = Date.now() - event.createdAt;
+
+    if (event.event === "recording.ready") {
+      if (!event.payload.sessionId) {
+        return;
+      }
+      const session = await this.db.stream.get(event.payload.sessionId, {
+        useReplica: false,
+      });
+      if (!session) {
+        return;
+      }
+      if (session.partialSession) {
+        // new session was started, so recording is not ready yet
+        return;
+      }
+      delete event.payload.sessionId;
+    }
+
     try {
       const { data: webhooksList } = await this.db.webhook.listSubscribed(
         event.userId,
@@ -102,7 +121,7 @@ export default class WebhookCannon {
       const responses = await Promise.all(
         webhooksList.map(async (webhook, key) => {
           try {
-            this.queue.publish("webhooks.triggers", {
+            await this.queue.publish("webhooks.triggers", {
               id: uuid(),
               event: event,
               stream: sanitized,
@@ -310,81 +329,5 @@ export default class WebhookCannon {
       statusCode: resp.status,
       response: resp,
     });
-  }
-
-  async onTrigger(event: WebhookMessage) {
-    console.log("ON TRIGGER triggered", event);
-    if (!event) {
-      // throw new Error('onTrigger requires a Queue event!')
-      // this is fine because pop could return a null if some other client raced and picked the task
-      // faster when it got the NOTIFY call.
-      return;
-    }
-    const since = Date.now() - event.createdAt;
-
-    if (event.event === "recording.ready") {
-      if (!event.payload.sessionId) {
-        return;
-      }
-      const session = await this.db.stream.get(event.payload.sessionId, {
-        useReplica: false,
-      });
-      if (!session) {
-        return;
-      }
-      if (session.partialSession) {
-        // new session was started, so recording is not ready yet
-        return;
-      }
-      delete event.payload.sessionId;
-    }
-
-    const { data: webhooksList } = await this.db.webhook.listSubscribed(
-      event.userId,
-      event.event
-    );
-
-    console.log("webhooks : ", webhooksList);
-    let stream = await this.db.stream.get(event.streamId);
-    if (!stream) {
-      // if stream isn't found. don't fire the webhook, log an error
-      return console.error(
-        `webhook Cannon: onTrigger: Stream Not found , streamId: ${event.streamId}`
-      );
-    }
-    // basic sanitization.
-    let sanitized = this.db.stream.addDefaultFields(
-      this.db.stream.removePrivateFields({ ...stream })
-    );
-    delete sanitized.streamKey;
-
-    let user = await this.db.user.get(event.userId);
-    if (!user) {
-      // if user isn't found. don't fire the webhook, log an error
-      return console.error(
-        `webhook Cannon: onTrigger: User Not found , userId: ${event.userId}`
-      );
-    }
-
-    try {
-      const responses = await Promise.all(
-        webhooksList.map(async (webhook, key) => {
-          try {
-            await this._fireHook(event, webhook, sanitized, user, false);
-          } catch (error) {
-            console.log("_fireHook Error", error);
-          }
-        })
-      );
-      // this version doesn't have blocking webhooks
-      // if (responses.some((o) => !o)) {
-      //   // at least one of responses is false, blocking this stream
-      //   res.status(403);
-      //   return res.end();
-      // }
-    } catch (e) {
-      console.error("webhook loop error", e);
-      throw e;
-    }
   }
 }
