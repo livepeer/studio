f8403604b43cd9c4f810870997dab665240bfb7e
adamsoffer <adam@livepeer.org>
2021-08-06 16:49:06 -0400

    fix 404 template styles

--- packages/www/components/Marketing/DefaultError/index.tsx
+++ packages/www/components/Marketing/DefaultError/index.tsx
@@ -1,47 +1,51 @@
 import Layout from "layouts/main";
-import Button from "@components/Marketing/Button";
+import Button from "components/Marketing/Button";
 import { Container, Heading, Box, Text } from "@livepeer.com/design-system";
 import Link from "next/link";
+import Image from "next/image";
+import Guides from "components/Marketing/Guides";
 
 const DefaultError = () => {
   return (
     <Layout>
-      <Box
-        as="img"
-        src="img/404.png"
-        alt=""
-        css={{
-          position: "absolute",
-          width: "100%",
-          left: "50%",
-          top: "50%",
-          transform: "translate(-50%,-150%)",
-          zIndex: -1,
-          "@bp2": {
-            transform: "translate(-50%, -50%)",
-          },
-        }}
-      />
-      <Container css={{ py: "250px", my: "auto" }}>
-        <Box
+      <Box css={{ position: "relative" }}>
+        <Guides />
+        <Image
+          src="/img/404.png"
+          alt="404"
+          layout="fill"
+          objectFit="contain"
           css={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            textAlign: "center",
-          }}>
-          <Heading as="h1" size="4" css={{ fontWeight: 700 }}>
-            Something went wrong.
-          </Heading>
-          <Text size="5">The page you requested could not be found.</Text>
-          <Link href="/" passHref>
-            <Button arrow css={{ my: "30px" }}>
-              Go to the homepage
-            </Button>
-          </Link>
-        </Box>
-      </Container>
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        />
+        <Container
+          size="3"
+          css={{ position: "relative", px: "$4", py: "250px", my: "auto" }}>
+          <Box
+            css={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              textAlign: "center",
+              position: "relative",
+            }}>
+            <Heading as="h1" size="4" css={{ fontWeight: 700, mb: "$4" }}>
+              Something went wrong.
+            </Heading>
+            <Text size="5">The page you requested could not be found.</Text>
+            <Link href="/" passHref>
+              <Button arrow css={{ my: "30px" }}>
+                Go to the homepage
+              </Button>
+            </Link>
+          </Box>
+        </Container>
+      </Box>
     </Layout>
   );
 };
