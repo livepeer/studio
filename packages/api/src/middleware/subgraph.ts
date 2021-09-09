84f26176172396bb148237d0f30eeb02e8f6f021
Yondon Fu <yondon.fu@gmail.com>
2021-09-08 22:40:14 -0400

    api: Remove temp block list for updating to v0.5.18

--- packages/api/src/middleware/subgraph.ts
+++ packages/api/src/middleware/subgraph.ts
@@ -18,12 +18,6 @@ export default function subgraphMiddleware({
     "0x3e2b450c0c499d8301146367680e067cd009db93": true,
     "0x23ca66656701ea524c92c5040cf022bd1bf8a4f8": true,
     "0x2559ae126336207c93060ed626f8bdefd998b66f": true,
-    // TODO: Remove these addresses after some time for upgrading to >= v0.5.18
-    "0xbaec9a6508b8270f4ff61049502d19cfe6ba428b": true,
-    "0xd84781e1a9b74d71ea76cda8bb9f30893bfd00d1": true,
-    "0xfb9849b0b53f66b747bfa47396964a3fa22400a0": true,
-    "0xc08dbaf4fe0cbb1d04a14b13edef38526976f2fb": true,
-    "0x2a15f9e60968451780bd2d09ca4e8c4498f168c3": true,
   };
 
   let cachedResp: Array<OrchestratorNodeAddress> = [];
