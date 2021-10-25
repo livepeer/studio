082288ecff84f61a7ffa82eb3d40c43d79827fb2
FullStackTiger <freelancetiger@hotmail.com>
2021-10-25 22:42:40 +0200

    add metadata fields to the blog post

--- packages/admin/schemas/documents/post.js
+++ packages/admin/schemas/documents/post.js
@@ -4,6 +4,12 @@ export default {
   name: "post",
   type: "document",
   title: "Blog Post",
+  fieldsets: [
+    {
+      title: "SEO & metadata",
+      name: "metadata",
+    },
+  ],
   fields: [
     {
       name: "title",
@@ -73,6 +79,34 @@ export default {
           type: "mux.video",
         },
       ],
+    },
+    {
+      name: "metaTitle",
+      type: "string",
+      title: "Title",
+      description: "This title populates meta-tags on the webpage",
+      fieldset: "metadata",
+    },
+    {
+      name: "metaDescription",
+      type: "text",
+      title: "Description",
+      description: "This description populates meta-tags on the webpage",
+      fieldset: "metadata",
+    },
+    {
+      name: "metaUrl",
+      type: "url",
+      title: "URL",
+      description: "This url populates meta-tags on the webpage",
+      fieldset: "metadata",
+    },
+    {
+      name: "openGraphImage",
+      type: "image",
+      title: "Open Graph Image",
+      description: "Image for sharing previews on Facebook, Twitter etc.",
+      fieldset: "metadata",
     },
   ],
 
