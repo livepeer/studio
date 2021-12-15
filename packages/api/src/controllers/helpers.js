f4c2c448614e548b89f6478c74b361ccce17aeeb
Victor Elias <victorgelias@gmail.com>
2021-12-14 21:09:18 -0300

    api: Fix if/else condition formatting

--- packages/api/src/controllers/helpers.js
+++ packages/api/src/controllers/helpers.js
@@ -293,41 +293,37 @@ export function parseFilters(fieldsMap, val) {
               } `
             )
           );
-        } else {
+        } else if (isObject(filter.value)) {
           // if value is a dictionary
-          if (isObject(filter.value)) {
-            Object.keys(filter.value).map(function (key, _index) {
-              let comparison = "";
-              switch (key) {
-                case "gt":
-                  comparison = ">";
-                  break;
-                case "gte":
-                  comparison = ">=";
-                  break;
-                case "lt":
-                  comparison = "<";
-                  break;
-                case "lte":
-                  comparison = "<=";
-                  break;
-                default:
-                  comparison = "=";
-              }
-              q.push(
-                sql``
-                  .append(fv.val)
-                  .append(comparison)
-                  .append(sql` ${filter.value[key]}`)
-              );
-            });
-          } else {
+          Object.keys(filter.value).forEach(function (key, _index) {
+            let comparison = "";
+            switch (key) {
+              case "gt":
+                comparison = ">";
+                break;
+              case "gte":
+                comparison = ">=";
+                break;
+              case "lt":
+                comparison = "<";
+                break;
+              case "lte":
+                comparison = "<=";
+                break;
+              default:
+                comparison = "=";
+            }
             q.push(
               sql``
                 .append(fv.val)
-                .append(sql` LIKE ${"%" + filter.value + "%"}`)
+                .append(comparison)
+                .append(sql` ${filter.value[key]}`)
             );
-          }
+          });
+        } else {
+          q.push(
+            sql``.append(fv.val).append(sql` LIKE ${"%" + filter.value + "%"}`)
+          );
         }
       }
     }
