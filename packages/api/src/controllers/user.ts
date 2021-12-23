f60bd86449fcff67ed17b4c11d479084bc0a4972
Victor Elias <victorgelias@gmail.com>
2021-12-22 21:57:45 -0300

    Revert "fix (#808)"
    This reverts commit b4c1c033b077602a4da2d0561fe8d26584be951d.

--- packages/api/src/controllers/user.ts
+++ packages/api/src/controllers/user.ts
@@ -439,7 +439,6 @@ app.post(
       await db.passwordResetToken.get(id)
     );
     if (newToken) {
-      delete newToken.resetToken;
       res.status(201);
       res.json(newToken);
     } else {
