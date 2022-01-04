35dabcfbaa3b991bd801fdb5d65440fdd9a5c26c
FullStackTiger <35409129+FullStackTiger@users.noreply.github.com>
2022-01-04 16:10:16 +0100

    set redirects for job details page (#828)

--- packages/www/next.config.js
+++ packages/www/next.config.js
@@ -199,6 +199,101 @@ const config = {
         destination: "/docs/guides/start-live-streaming/verify",
         permanent: false,
       },
+      {
+        source: "/jobs/technical-writer",
+        destination: "/jobs/1496366",
+        permanent: false,
+      },
+      {
+        source: "/jobs/full-stack-video-engineer",
+        destination: "/jobs/1412799",
+        permanent: false,
+      },
+      {
+        source: "/jobs/operations-manager",
+        destination: "/jobs/1466566",
+        permanent: false,
+      },
+      {
+        source: "/jobs/video-developer-community-manager",
+        destination: "/jobs/1476601",
+        permanent: false,
+      },
+      {
+        source: "/jobs/web3-developer-evangelist",
+        destination: "/jobs/1491881",
+        permanent: false,
+      },
+      {
+        source: "/jobs/chief-operating-officer",
+        destination: "/jobs/1466562",
+        permanent: false,
+      },
+      {
+        source: "/jobs/senior-video-infrastructure-engineer",
+        destination: "/jobs/1414584",
+        permanent: false,
+      },
+      {
+        source: "/jobs/video-developer-success-manager",
+        destination: "/jobs/1476607",
+        permanent: false,
+      },
+      {
+        source: "/jobs/senior-software-engineer-video-transcoding",
+        destination: "/jobs/1412803",
+        permanent: false,
+      },
+      {
+        source: "/jobs/analytics-engineer",
+        destination: "/jobs/1496262",
+        permanent: false,
+      },
+      {
+        source: "/jobs/protocol-engineer",
+        destination: "/jobs/1412804",
+        permanent: false,
+      },
+      {
+        source: "/jobs/investor-relations-manager",
+        destination: "/jobs/1454503",
+        permanent: false,
+      },
+      {
+        source: "/jobs/senior-product-marketing-manager",
+        destination: "/jobs/1454194",
+        permanent: false,
+      },
+      {
+        source: "/jobs/senior-lead-product-manager",
+        destination: "/jobs/1454194",
+        permanent: false,
+      },
+      {
+        source: "/jobs/content-marketing",
+        destination: "/jobs/1476609",
+        permanent: false,
+      },
+      {
+        source: "/jobs/marketing-manager",
+        destination: "/jobs/1412808",
+        permanent: false,
+      },
+      {
+        source: "/jobs/events-manager",
+        destination: "/jobs/1454453",
+        permanent: false,
+      },
+      {
+        source: "/jobs/engineering-manager-livepeer-core-software",
+        destination: "/jobs/1478605",
+        permanent: false,
+      },
+      {
+        source: "/jobs/technical-product-manager-orchestrator-experience",
+        destination: "/jobs/1496214",
+        permanent: false,
+      },
     ];
   },
 };
