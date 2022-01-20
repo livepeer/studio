0e9fcbba960ad379dfed2f724c3ec08c3935f2ba
Eli Mallon <eli@livepeer.org>
2022-01-19 21:38:20 -0800

    www: fix --version import

--- packages/www/server.js
+++ packages/www/server.js
@@ -1,20 +1,47 @@
-// Programmatic entrypoint into the Next.js server for `pkg` purposes
+// Programmatic entrypoint into the Next.js server for `pkg` purposes. Kind of
+// hacky. Really this should just get compiled to static JS and included that
+// way. But this was expedient for livepeer-in-a-box.
+//
+// On the off chance you're here to add more command line parameters in this
+// file, you should figure out how to reuse the logic in
+// ../api/src/yargs-to-mist.ts.
 
-// Unsure why this doesn't get properly included in pkg otherwise...
-
+const yargs = require("yargs");
 const nextConfig = require("./next.config");
-// server.js
 const { createServer } = require("http");
 const next = require("next");
+const pkg = require("./package.json");
 
 const app = next({ dir: __dirname, conf: nextConfig, dev: false });
 const handle = app.getRequestHandler();
+const cli = yargs.options({
+  port: {
+    type: "number",
+    default: 3012,
+  },
+}).argv;
 
-const port = process.env.PORT || 3040;
+if (cli.j) {
+  const description = {
+    name: "livepeer-www",
+    friendly: "Livepeer Dashboard",
+    desc: "Livepeer Next.js Dashboard Front-end Server",
+    optional: {
+      port: {
+        option: "--port",
+        default: 3012,
+        help: "Port to run on",
+      },
+    },
+    version: pkg.version,
+  };
+  console.log(JSON.stringify(description));
+  process.exit(0);
+}
 
 app.prepare().then(() => {
-  createServer(handle).listen(port, (err) => {
+  createServer(handle).listen(cli.port, (err) => {
     if (err) throw err;
-    console.log(`> Ready on http://localhost:${port}`);
+    console.log(`> Ready on http://localhost:${cli.port}`);
   });
 });
