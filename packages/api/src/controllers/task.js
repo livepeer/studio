91c2cee0984d4055f6d3783de685b0014b0aef96
gioelecerati <gioele@cerati.org>
2022-01-25 12:59:31 +0100

    draft task ctrl

---
+++ packages/api/src/controllers/task.js
@@ -0,0 +1,180 @@
+import { URL } from "url";
+import { authMiddleware } from "../middleware";
+import { validatePost } from "../middleware";
+import Router from "express/lib/router";
+import logger from "../logger";
+import uuid from "uuid/v4";
+import { makeNextHREF, parseFilters, parseOrder } from "./helpers";
+import { db } from "../store";
+import sql from "sql-template-strings";
+import { UnprocessableEntityError } from "../store/errors";
+
+const app = Router();
+
+function validateTaskPayload(id, userId, createdAt, payload) {
+  return {
+    id,
+    userId,
+    createdAt,
+    name: payload.name,
+    type: payload.type,
+  };
+}
+
+const fieldsMap = {
+  id: `task.ID`,
+  name: { val: `task.data->>'name'`, type: "full-text" },
+  createdAt: `task.data->'createdAt'`,
+  userId: `task.data->>'userId'`,
+  "user.email": { val: `users.data->>'email'`, type: "full-text" },
+  type: `task.data->>'type'`,
+};
+
+app.get("/", authMiddleware({}), async (req, res) => {
+  let { limit, cursor, all, event, allUsers, order, filters, count } =
+    req.query;
+  if (isNaN(parseInt(limit))) {
+    limit = undefined;
+  }
+
+  if (req.user.admin && allUsers && allUsers !== "false") {
+    const query = parseFilters(fieldsMap, filters);
+    if (!all || all === "false") {
+      query.push(sql`task.data->>'deleted' IS NULL`);
+    }
+
+    let fields =
+      " task.id as id, task.data as data, users.id as usersId, users.data as usersdata";
+    if (count) {
+      fields = fields + ", count(*) OVER() AS count";
+    }
+    const from = `task left join users on task.data->>'userId' = users.id`;
+    const [output, newCursor] = await db.task.find(query, {
+      limit,
+      cursor,
+      fields,
+      from,
+      order: parseOrder(fieldsMap, order),
+      process: ({ data, usersdata, count: c }) => {
+        if (count) {
+          res.set("X-Total-Count", c);
+        }
+        return { ...data, user: db.user.cleanWriteOnlyResponse(usersdata) };
+      },
+    });
+
+    res.status(200);
+
+    if (output.length > 0 && newCursor) {
+      res.links({ next: makeNextHREF(req, newCursor) });
+    }
+    return res.json(output);
+  }
+
+  const query = parseFilters(fieldsMap, filters);
+  query.push(sql`task.data->>'userId' = ${req.user.id}`);
+
+  if (!all || all === "false") {
+    query.push(sql`task.data->>'deleted' IS NULL`);
+  }
+
+  let fields = " task.id as id, task.data as data";
+  if (count) {
+    fields = fields + ", count(*) OVER() AS count";
+  }
+  const from = `task`;
+  const [output, newCursor] = await db.task.find(query, {
+    limit,
+    cursor,
+    fields,
+    from,
+    order: parseOrder(fieldsMap, order),
+    process: ({ data, count: c }) => {
+      if (count) {
+        res.set("X-Total-Count", c);
+      }
+      return { ...data };
+    },
+  });
+
+  res.status(200);
+
+  if (output.length > 0) {
+    res.links({ next: makeNextHREF(req, newCursor) });
+  }
+
+  return res.json(output);
+});
+
+app.get("/:id", authMiddleware({}), async (req, res) => {
+  const os = await req.store.get(`task/${req.params.id}`);
+  if (!os) {
+    res.status(404);
+    return res.json({
+      errors: ["not found"],
+    });
+  }
+
+  if (req.user.admin !== true && req.user.id !== os.userId) {
+    res.status(403);
+    return res.json({
+      errors: ["user can only request information on their own tasks"],
+    });
+  }
+
+  res.json(os);
+});
+
+app.post("/", authMiddleware({}), validatePost("task"), async (req, res) => {
+  const id = uuid();
+  const doc = validateTaskPayload(id, req.user.id, Date.now(), req.body);
+  try {
+    await db.task.create(doc);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+  res.status(201);
+  res.json(doc);
+});
+
+app.delete("/:id", authMiddleware({}), async (req, res) => {
+  const { id } = req.params;
+  const task = await db.task.get(id);
+  if (!task) {
+    res.status(404);
+    return res.json({ errors: ["not found"] });
+  }
+  if (!req.user.admin && req.user.id !== task.userId) {
+    res.status(403);
+    return res.json({
+      errors: ["users may only delete their own tasks"],
+    });
+  }
+  await db.task.delete(id);
+  res.status(204);
+  res.end();
+});
+
+app.put("/:id", authMiddleware({}), validatePost("task"), async (req, res) => {
+  // update a specific task
+  const task = await req.store.get(`task/${req.body.id}`);
+  if ((task.userId !== req.user.id || task.deleted) && !req.user.admin) {
+    // do not reveal that task exists
+    res.status(404);
+    return res.json({ errors: ["not found"] });
+  }
+
+  const { id, userId, createdAt } = task;
+  const doc = req.body;
+  try {
+    await db.task.replace(doc);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+  res.status(200);
+  res.json({ id: req.body.id });
+});
+
+export default app;
