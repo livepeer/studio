5edbe2927bc661a440acc50ac19895abaa469833
FullStackTiger <35409129+FullStackTiger@users.noreply.github.com>
2022-02-07 17:46:52 +0100

    prettier

--- packages/www/components/Dashboard/MediaServerTable/index.tsx
+++ packages/www/components/Dashboard/MediaServerTable/index.tsx
@@ -26,8 +26,8 @@ const MediaServerTable = ({ title = "Media Server" }: { title?: string }) => {
           Configurable, deployable, and open-source
         </Heading>
         <Text variant="gray" css={{ lineHeight: 1.5, mb: "$3" }}>
-          Livepeer Media Server (MistServer) allows you to take any media from any
-          location, using any method, and deliver it seamlessly to anyone,
+          Livepeer Media Server (MistServer) allows you to take any media from
+          any location, using any method, and deliver it seamlessly to anyone,
           anywhere, in any format.
         </Text>
 
