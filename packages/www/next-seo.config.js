67a9abe890a52f9f541b19f217a2de1d05a3c6eb
Alexander Tibbets <alexander@livepeer.org>
2022-02-11 00:31:23 -0800

    Update next-seo.config.js

--- packages/www/next-seo.config.js
+++ packages/www/next-seo.config.js
@@ -1,14 +1,14 @@
 export default {
-  title: "Livepeer.com",
+  title: "Livepeer Video Services",
   description:
-    "Build and scale next generation streaming platforms and services at an industry shattering price through quick and reliable API access to Livepeer, the world's open video infrastructure.",
+    "Accessible APIs and infrastructure to build video streaming applications — big and small. Powered by Livepeer, the leading video layer for Web3.",
   openGraph: {
     type: "website",
     locale: "en_IE",
     url: "https://livepeer.com",
-    title: "Livepeer.com",
+    title: "Livepeer Video Services",
     description:
-      "Build and scale next generation streaming platforms and services at an industry shattering price through quick and reliable API access to Livepeer, the world's open video infrastructure.",
+      "Accessible APIs and infrastructure to build video streaming applications — big and small. Powered by Livepeer, the leading video layer for Web3.",
     defaultImageWidth: 1200,
     defaultImageHeight: 642,
     images: [
@@ -16,10 +16,10 @@ export default {
         url: "https://livepeer.com/img/OG.png",
         width: 1200,
         height: 642,
-        alt: "Livepeer.com",
+        alt: "Livepeer Video Services",
       },
     ],
-    site_name: "Livepeer.com",
+    site_name: "Livepeer Video Services",
   },
   twitter: {
     handle: "@livepeerdotcom",
