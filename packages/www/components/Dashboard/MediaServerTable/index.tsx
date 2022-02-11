37c7ea1e414a6e29bce11d380e71ff859c956c9b
FullStackTiger <35409129+FullStackTiger@users.noreply.github.com>
2022-02-11 22:25:23 +0100

    Add page to Dashboard for "Media Server" under Developers (#886)
    * Add page to Dashboard for "Media Server" under Developers
    
    * updated Livepeer Media Server (Mist) to Livepeer Media Server (MistServer)
    
    * prettier

---
+++ packages/www/components/Dashboard/MediaServerTable/index.tsx
@@ -0,0 +1,48 @@
+import Link from "next/link";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import {
+  Flex,
+  Heading,
+  Link as A,
+  Text,
+  Box,
+} from "@livepeer.com/design-system";
+
+const MediaServerTable = ({ title = "Media Server" }: { title?: string }) => {
+  return (
+    <>
+      <Heading size="2" css={{ fontWeight: 600 }}>
+        {title}
+      </Heading>
+      <Flex
+        direction="column"
+        justify="center"
+        css={{
+          margin: "0 auto",
+          height: "calc(100vh - 400px)",
+          maxWidth: 450,
+        }}>
+        <Heading css={{ fontWeight: 500, mb: "$3" }}>
+          Configurable, deployable, and open-source
+        </Heading>
+        <Text variant="gray" css={{ lineHeight: 1.5, mb: "$3" }}>
+          Livepeer Media Server (MistServer) allows you to take any media from
+          any location, using any method, and deliver it seamlessly to anyone,
+          anywhere, in any format.
+        </Text>
+
+        <Link href="https://mistserver.org/" passHref>
+          <A
+            target="_blank"
+            variant="violet"
+            css={{ display: "flex", ai: "center", mb: "$5" }}>
+            <Box>Learn more</Box>
+            <ArrowRightIcon />
+          </A>
+        </Link>
+      </Flex>
+    </>
+  );
+};
+
+export default MediaServerTable;
