a841f873252945a4cd3adeea4ad2630adf4ab4f0
Victor Elias <victorgelias@gmail.com>
2022-02-23 12:59:55 -0700

    Output ID of minted token

--- packages/www/pages/transact/eth/index.tsx
+++ packages/www/pages/transact/eth/index.tsx
@@ -2,11 +2,15 @@ import { Box } from "@livepeer.com/design-system";
 import { useMetaMask } from "metamask-react";
 import { useEffect, useMemo, useState } from "react";
 import Web3 from "web3";
-import minterAbi from "./minter-abi.json";
+import { VideoNft } from "./types/video-nft";
+import videoNftAbi from "./types/video-nft.json";
 
-const livepeerNftMinterAddress = "0x11b145EBc404011EdC17d957e204051DBe04b971"; // TODO: Real address here
+const livepeerNftMinterAddress = "0x69C53E7b8c41bF436EF5a2D81DB759Dc8bD83b5F"; // TODO: Real address here
 
 const parseTransaction = (value: string) => {
+  if (!value) {
+    return null;
+  }
   try {
     value = decodeURIComponent(value);
     value = Buffer.from(value).toString("base64");
@@ -35,40 +39,72 @@ const TransactEth = () => {
         recipient: searchParams.get("recipient") || account,
       };
     }, [typeof window !== "undefined" && window?.location?.search, account]);
-  const [logs, setLogs] = useState<string[]>([]);
+  const [logs, setLogs] = useState<JSX.Element[]>([]);
+  const addLog = (log: JSX.Element | string) =>
+    setLogs((prev) => [...prev, typeof log === "string" ? <>{log}</> : log]);
 
+  const videoNft = useMemo(
+    () => new web3.eth.Contract(videoNftAbi as any, contractAddress),
+    [web3, contractAddress]
+  ) as unknown as VideoNft;
   const transaction = useMemo(() => {
     if (inputTransaction) {
       return inputTransaction;
+    } else if (!tokenUri || !recipient) {
+      return null;
     }
-    if (tokenUri && recipient) {
-      const nftContract = new web3.eth.Contract(
-        minterAbi as any,
-        contractAddress
-      );
-      return {
-        to: contractAddress,
-        gas: 500000,
-        maxPriorityFeePerGas: 39999999987,
-        data: nftContract.methods.mint(recipient, tokenUri).encodeABI(),
-      };
-    }
-    return null;
+    return {
+      to: contractAddress,
+      gas: 500000,
+      maxPriorityFeePerGas: 39999999987,
+      data: videoNft.methods.mint(recipient, tokenUri).encodeABI(),
+    };
   }, [inputTransaction, contractAddress, recipient, tokenUri]);
 
   useEffect(() => {
-    if (status !== "connected" || !transaction) return;
+    if (status !== "connected" || !transaction) {
+      return;
+    }
     Promise.resolve().then(async () => {
       const nonce = await web3.eth.getTransactionCount(account, "latest");
-      setLogs([...logs, `Got nonce: ${nonce}`]);
-
       const tx = {
         ...transaction,
         from: account,
         nonce,
       };
+      addLog("Minting...");
       const receipt = await web3.eth.sendTransaction(tx);
-      setLogs([...logs, `Transaction receipt: ${JSON.stringify(receipt)}`]);
+
+      const events = await videoNft.getPastEvents("Mint", {
+        filter: { sender: account },
+        fromBlock: receipt.blockNumber,
+        toBlock: receipt.blockNumber,
+      });
+      const event = events.find(
+        (ev) => ev.transactionHash === receipt.transactionHash
+      );
+      if (!event) {
+        addLog(
+          `NFT minted but failed to find event. Transacton receipt:\n${JSON.stringify(
+            receipt,
+            null,
+            2
+          )}`
+        );
+        return;
+      }
+      const { tokenId } = event.returnValues;
+      addLog(
+        <>
+          Successfully minted token with ID {tokenId}! Check it on{" "}
+          <a
+            href={`https://opensea.io/assets/matic/${contractAddress}/${tokenId}`}
+            target="_blank">
+            OpenSea
+          </a>
+          !
+        </>
+      );
     });
   }, [status, transaction]);
 
@@ -104,8 +140,8 @@ const TransactEth = () => {
             <div>
               Connected account {account} on chain ID {chainId}
             </div>
-            {logs.map((log) => (
-              <div>{log}</div>
+            {logs.map((log, idx) => (
+              <div key={`log-${idx}`}>{log}</div>
             ))}
           </Box>
         </>
