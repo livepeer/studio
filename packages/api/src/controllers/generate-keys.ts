53c65e55ded3d0f04ee0f4216961f608aae3b43d
Victor Elias <victorgelias@gmail.com>
2022-05-11 18:43:06 -0300

    api: Make generateStreamKey a little better

--- packages/api/src/controllers/generate-keys.ts
+++ packages/api/src/controllers/generate-keys.ts
@@ -8,32 +8,34 @@ const SEGMENT_COUNT = 4;
 const SEGMENT_LENGTH = 4;
 const hexToBase36 = anyBase(anyBase.HEX, BASE_36);
 
+function randomBytesAsync(size: number) {
+  return new Promise<Buffer>((resolve, reject) => {
+    randomBytes(size, (err, buf) => {
+      err ? reject(err) : resolve(buf);
+    });
+  });
+}
+
 /**
  * Securely generate a stream key of a given length. Goals for stream keys: be reasonably secure
  * but also easy to type if necessary. Base36 facilitates this.
  *
  * Returns stream keys of the form XXXX-XXXX-XXXX in Base36. 62-ish bits of entropy.
  */
-export function generateStreamKey() {
-  return new Promise<string>((resolve, reject) => {
-    randomBytes(128, (err, buf) => {
-      if (err) {
-        return reject(err);
-      }
-      const raw = hexToBase36(buf.toString("hex"));
-      let result = "";
-      const TOTAL_LENGTH = SEGMENT_COUNT * SEGMENT_LENGTH;
-      for (let i = 0; i < TOTAL_LENGTH; i += 1) {
-        // Pull from the end of the raw string, the start has least siginificant bits
-        // and isn't likely to be fully random.
-        result += raw[raw.length - 1 - i];
-        if ((i + 1) % SEGMENT_LENGTH === 0 && i < TOTAL_LENGTH - 1) {
-          result += "-";
-        }
-      }
-      resolve(result);
-    });
-  });
+export async function generateStreamKey() {
+  const buf = await randomBytesAsync(128);
+  const raw = hexToBase36(buf.toString("hex"));
+  let result = "";
+  const TOTAL_LENGTH = SEGMENT_COUNT * SEGMENT_LENGTH;
+  for (let i = 0; i < TOTAL_LENGTH; i += 1) {
+    // Pull from the end of the raw string, the start has least siginificant bits
+    // and isn't likely to be fully random.
+    result += raw[raw.length - 1 - i];
+    if ((i + 1) % SEGMENT_LENGTH === 0 && i < TOTAL_LENGTH - 1) {
+      result += "-";
+    }
+  }
+  return result;
 }
 
 function formatPlaybackId(key: string): string {
