livepeer/studio

commit 6c61ca7a21aea2cc4d306d19a9e4bb025c1fca2f

Use latest design-system library (#1032)

packages/www/components/Admin/UserTable/index.tsx
@@ -1,12 +1,20 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import { useState, useMemo } from "react";
 import { useApi } from "hooks";
-import { Button, Flex, Container, Select } from "@theme-ui/components";
 import Modal from "../Modal";
 import { products } from "@livepeer.com/api/src/config";
 import CommonAdminTable from "@components/Admin/CommonAdminTable";
-import { Box, Checkbox, Label, Tooltip } from "@livepeer.com/design-system";
+import {
+  Button,
+  Flex,
+  Container,
+  Select,
+  Box,
+  Checkbox,
+  Label,
+  Tooltip,
+} from "@livepeer/design-system";
 
 type UserTableProps = {
   userId: string;
@@ -161,8 +169,8 @@ const UserTable = ({ userId, id }: UserTableProps) => {
             <Checkbox
               id="isCopyrightInfringiment"
               checked={isCopyrightInfringiment}
-              onCheckedChange={(e) =>
-                setIsCopyrightInfringiment(e.target.checked)
+              onCheckedChange={(checked: boolean) =>
+                setIsCopyrightInfringiment(checked)
               }
             />
             <Tooltip

packages/design-system/components/Snackbar/index.tsx
@@ -1,2 +1,0 @@
-export { SnackbarProvider } from "./SnackbarProvider";
-export { useSnackbar } from "./useSnackbar";

packages/www/components/Admin/AdminObjectStoreTable/index.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import { useState, useMemo } from "react";
 import { useApi } from "hooks";

packages/www/components/Admin/AdminStreamsTable/index.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import { useEffect, useState, useMemo } from "react";
 import Link from "next/link";

packages/www/components/Admin/AdminTokenTable/index.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import { useEffect, useState, useMemo } from "react";
 import { useApi } from "hooks";

packages/www/components/Admin/AdminTools/index.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import Link from "next/link";
 import { useEffect, useState } from "react";

packages/www/components/Admin/AdminUsageTable/index.tsx
@@ -1,4 +1,4 @@
-/** @jsx jsx */
+/** @jsxImportSource @emotion/react */
 import { jsx } from "theme-ui";
 import { useEffect, useState } from "react";
 import { useApi } from "hooks";
@@ -13,7 +13,7 @@ function dur2str(dur?: number) {
   return `${dur} sec (${min} min)`;
 }
 
-const Index = ({ id }: { id: string }) => {
+const Index = ({ id }: { id: string }, children) => {
   const [message, setMessage] = useState("");
   const [selectedUser, setSelectedUser] = useState(null);
   const [fromTime, setFromTime] = useState("");
@@ -94,20 +94,26 @@ const Index = ({ id }: { id: string }) => {
         </Button>
       </Box>
       <Table sx={{ gridTemplateColumns: "auto auto auto auto auto" }}>
-        <TableRow variant={TableRowVariant.Header} key="usage header">
-          <Box></Box>
-          <Box>Source seconds</Box>
-          <Box>Transcoded seconds</Box>
-          <Box>Source segments</Box>
-          <Box>Transcoded segments</Box>
-        </TableRow>
-        <TableRow key="just one row for now" variant={TableRowVariant.Normal}>
-          <Box></Box>
-          <Box>{dur2str(usage && usage.sourceSegmentsDuration)}</Box>
-          <Box>{dur2str(usage && usage.transcodedSegmentsDuration)}</Box>
-          <Box>{usage && usage.sourceSegments}</Box>
-          <Box>{usage && usage.transcodedSegments}</Box>
-        </TableRow>
+        <>
+          <TableRow variant={TableRowVariant.Header} key="usage header">
+            <>
+              <Box></Box>
+              <Box>Source seconds</Box>
+              <Box>Transcoded seconds</Box>
+              <Box>Source segments</Box>
+              <Box>Transcoded segments</Box>
+            </>
+          </TableRow>
+          <TableRow key="just one row for now" variant={TableRowVariant.Normal}>
+            <>
+              <Box></Box>
+              <Box>{dur2str(usage && usage.sourceSegmentsDuration)}</Box>
+              <Box>{dur2str(usage && usage.transcodedSegmentsDuration)}</Box>
+              <Box>{usage && usage.sourceSegments}</Box>
+              <Box>{usage && usage.transcodedSegments}</Box>
+            </>
+          </TableRow>
+        </>
       </Table>
     </Container>
   );

commit 58357483898b7fea46961ee1adf4c244a2076d27

www: Fix component imports in admin users table
This fixes a regression caused by: https://github.com/livepeer/livepeer-com/pull/1032

Turns out we are mixing @theme-ui/components with the
@livepeer/design-system in that page.

We should probably migrate everything to design system
altogether, but while we don't do that let's keep it
working as before.

packages/www/components/Admin/UserTable/index.tsx
@@ -2,19 +2,11 @@
 import { jsx } from "theme-ui";
 import { useState, useMemo } from "react";
 import { useApi } from "hooks";
+import { Button, Flex, Container, Select } from "@theme-ui/components";
 import Modal from "../Modal";
 import { products } from "@livepeer.com/api/src/config";
 import CommonAdminTable from "@components/Admin/CommonAdminTable";
-import {
-  Button,
-  Flex,
-  Container,
-  Select,
-  Box,
-  Checkbox,
-  Label,
-  Tooltip,
-} from "@livepeer/design-system";
+import { Box, Checkbox, Label, Tooltip } from "@livepeer/design-system";
 
 type UserTableProps = {
   userId: string;