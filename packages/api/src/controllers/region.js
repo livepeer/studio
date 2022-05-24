76f26c549ed19ecc5f740e1c44d42192e3468205
Thom Shutt <thomshutt@users.noreply.github.com>
2022-05-24 20:06:34 +0100

    Split trusted/untrusted across all nodes, rather than within a region (#1083)

--- packages/api/src/controllers/region.js
+++ packages/api/src/controllers/region.js
@@ -7,9 +7,11 @@ const defaultScore = 1;
 const app = Router();
 
 function flatRegions(regions = [], halfRegionOrchestratorsUntrusted = false) {
+  let count = 0;
   return regions.flatMap((reg) =>
-    reg.orchestrators.map((orch, i) => ({
-      score: halfRegionOrchestratorsUntrusted && i % 2 == 0 ? 0 : defaultScore,
+    reg.orchestrators.map((orch) => ({
+      score:
+        halfRegionOrchestratorsUntrusted && count++ % 2 == 0 ? 0 : defaultScore,
       region: reg.region,
       ...orch,
     }))
