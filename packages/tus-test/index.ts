c4ac37da5e0e985b1872d9add6effaf306d1f8d3
Victor Elias <victorgelias@gmail.com>
2022-06-17 20:18:29 -0300

    [DEV-ONLY] Add tus-test project to test tus development
    Remember to delete this or make it a proper unit test once
    it's done.

---
+++ packages/tus-test/index.ts
@@ -0,0 +1,53 @@
+/* eslint-disable */
+
+import axios from "axios";
+import fs from "fs";
+import * as tus from "tus-js-client";
+
+if (!process.env.LP_API_KEY) {
+  throw new Error("Must set LP_API_KEY env var for calling /request-upload");
+}
+
+const base = `/Users/victorges/workspace/test-videos/`;
+const filename = `rtm.mp4`;
+const path = base + filename;
+const file = fs.createReadStream(path);
+const { size } = fs.statSync(path);
+
+async function doUpload() {
+  const res = await axios({
+    method: "POST",
+    url: "http://localhost:3004/api/asset/request-upload",
+    data: { name: "tus-test" },
+    headers: {
+      authorization: `Bearer ${process.env.LP_API_KEY}`,
+    },
+  });
+  if (res.status !== 200) {
+    throw new Error(
+      `Failed to request upload status=${res.status} body=${res.data}`
+    );
+  }
+
+  const upload = new tus.Upload(file, {
+    endpoint: res.data.tusEndpoint,
+    metadata: {
+      filename,
+      filetype: "video/mp4",
+    },
+    uploadSize: size,
+    onError(error) {
+      throw error;
+    },
+    onProgress(bytesUploaded, bytesTotal) {
+      const percentage = ((bytesUploaded / bytesTotal) * 100).toFixed(2);
+      console.log(bytesUploaded, bytesTotal, `${percentage}%`);
+    },
+    onSuccess() {
+      console.log("Upload finished:", upload.url);
+    },
+  });
+  upload.start();
+}
+
+doUpload().catch((err) => console.error("Error uploading:", err.message));
