b00b9a7b6bdc36704f00e05d3e86b562f13fe121
Victor Elias <victorgelias@gmail.com>
2022-07-12 18:26:58 -0300

    api/asset: Make CID properties queryable on API

--- packages/api/src/controllers/asset.ts
+++ packages/api/src/controllers/asset.ts
@@ -247,6 +247,8 @@ const fieldsMap: FieldsMap = {
   playbackId: `asset.data->>'playbackId'`,
   "user.email": { val: `users.data->>'email'`, type: "full-text" },
   meta: `asset.data->>'meta'`,
+  ipfsVideoFileCid: `asset.data->'status'->'storage'->'ipfs'->'data'->'videoFileCid'`,
+  ipfsNftMetadataCid: `asset.data->'status'->'storage'->'ipfs'->'data'->'nftMetadataCid'`,
 };
 
 app.get("/", authorizer({}), async (req, res) => {
