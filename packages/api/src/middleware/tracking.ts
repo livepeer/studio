169225bc7f17c391ff3ec5d723305cc7f6524c25
Victor Elias <victorgelias@gmail.com>
2022-07-30 03:00:38 -0300

    api/tracking: Buffer lastSeen updates for a minute

--- packages/api/src/middleware/tracking.ts
+++ packages/api/src/middleware/tracking.ts
@@ -1,25 +1,48 @@
+import sql from "sql-template-strings";
 import { ApiToken } from "../schema/types";
 import { db } from "../store";
+import Table from "../store/table";
+
+const flushDelay = 60 * 1000; // 60s
 
 class Tracker {
-  recordToken({ id }: ApiToken) {
-    Promise.resolve().then(async () => {
-      try {
-        await db.apiToken.update(id, { lastSeen: Date.now() });
-      } catch (err) {
-        console.log("token tracking record error: ", err);
-      }
-    });
-  }
+  updateLastSeenTasks: Map<string, number> = new Map();
 
   recordUser(userId: string) {
-    Promise.resolve().then(async () => {
+    this.recordLastSeen(db.user, userId);
+  }
+
+  recordToken({ id }: ApiToken) {
+    this.recordLastSeen(db.apiToken, id);
+  }
+
+  private recordLastSeen(
+    table: Table<{ id: string; lastSeen?: number }>,
+    id: string
+  ) {
+    const alreadyScheduled = this.updateLastSeenTasks.has(id);
+    this.updateLastSeenTasks.set(id, Date.now());
+    if (alreadyScheduled) return;
+
+    setTimeout(async () => {
       try {
-        await db.user.update(userId, { lastSeen: Date.now() });
+        const lastSeen = this.updateLastSeenTasks.get(id);
+        this.updateLastSeenTasks.delete(id);
+
+        await table.update(
+          [
+            sql`id = ${id}`,
+            sql`coalesce((data->'lastSeen'})::bigint, 0) < ${lastSeen}`,
+          ],
+          { lastSeen } as any
+        );
       } catch (err) {
-        console.log("user tracking record error: ", err);
+        console.log(
+          `error saving last seen: table=${table.name} id=${id} err=`,
+          err
+        );
       }
-    });
+    }, flushDelay);
   }
 }
 
