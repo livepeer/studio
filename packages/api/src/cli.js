e749fbc07d620ad1fadaa38b0e142986e3724165
Eli Mallon <eli@livepeer.org>
2022-08-03 10:31:50 -0700

    api: bundle stream-info-service

--- packages/api/src/cli.js
+++ packages/api/src/cli.js
@@ -5,5 +5,9 @@ import makeApp from "./index";
 import parseCli from "./parse-cli";
 
 if (!module.parent) {
-  makeApp(parseCli());
+  if (process.env.LP_API_STREAM_INFO_SERVICE === "true") {
+    require("./stream-info-service");
+  } else {
+    makeApp(parseCli());
+  }
 }
