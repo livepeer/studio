845e074c9f755e876bb23db83b374901d4c68842
Victor Elias <victorgelias@gmail.com>
2022-08-23 18:36:05 -0300

    api: Fix tests

--- packages/api/src/controllers/asset.test.ts
+++ packages/api/src/controllers/asset.test.ts
@@ -79,7 +79,7 @@ describe("controllers/asset", () => {
   beforeEach(async () => {
     await db.objectStore.create({
       id: "mock_vod_store",
-      url: "http://user:password@localhost:8080/us-east-1/vod",
+      url: "s3+http://user:password@localhost:8080/us-east-1/vod",
     });
     ({ client, adminUser, adminApiKey, nonAdminUser, nonAdminToken } =
       await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
