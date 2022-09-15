81695bd72cb9054e1e4599ecb7ea0a561e519713
Victor Elias <victorgelias@gmail.com>
2022-09-15 19:30:36 -0300

    api/auth: Move helper out of handler

--- packages/api/src/controllers/auth.ts
+++ packages/api/src/controllers/auth.ts
@@ -2,7 +2,7 @@
  * Special controller for forwarding all incoming requests to a geolocated API region
  */
 
-import { Router } from "express";
+import { Request, Router } from "express";
 import { authorizer } from "../middleware";
 import { db } from "../store";
 import { ForbiddenError, NotFoundError } from "../store/errors";
@@ -10,31 +10,33 @@ import Table from "../store/table";
 
 type UserOwnedObj = { id: string; deleted?: boolean; userId?: string };
 
+async function checkUserOwned(
+  req: Request,
+  headerName: string,
+  table: Table<UserOwnedObj>
+) {
+  if (!(headerName in req.headers)) {
+    return;
+  }
+  const id = req.headers[headerName]?.toString();
+  const obj = await table.get(id);
+  if (!obj || obj.deleted) {
+    throw new NotFoundError(`${table.name} not found`);
+  }
+  const hasAccess = obj.userId === req.user.id || req.user.admin;
+  if (!hasAccess) {
+    throw new ForbiddenError(`access forbidden`);
+  }
+}
+
 const app = Router();
 
 app.all(
   "/",
   authorizer({ originalUriHeader: "x-original-uri" }),
   async (req, res) => {
-    const checkUserOwned = async (
-      headerName: string,
-      table: Table<UserOwnedObj>
-    ) => {
-      if (!(headerName in req.headers)) {
-        return;
-      }
-      const id = req.headers[headerName]?.toString();
-      const obj = await table.get(id);
-      if (!obj || obj.deleted) {
-        throw new NotFoundError(`${table.name} not found`);
-      }
-      const hasAccess = obj.userId === req.user.id || req.user.admin;
-      if (!hasAccess) {
-        throw new ForbiddenError(`access forbidden`);
-      }
-    };
-    await checkUserOwned("x-livepeer-stream-id", db.stream);
-    await checkUserOwned("x-livepeer-asset-id", db.asset);
+    await checkUserOwned(req, "x-livepeer-stream-id", db.stream);
+    await checkUserOwned(req, "x-livepeer-asset-id", db.asset);
     res.status(204).end();
   }
 );
