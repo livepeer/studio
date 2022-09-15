8348381e606f9c0399ffe13aee2533ba3239d8e5
Victor Elias <victorgelias@gmail.com>
2022-09-15 19:20:44 -0300

    api/auth: Support authorizing asset requests

--- packages/api/src/controllers/auth.ts
+++ packages/api/src/controllers/auth.ts
@@ -12,14 +12,27 @@ app.all(
   "/",
   authorizer({ originalUriHeader: "x-original-uri" }),
   async (req, res) => {
-    const streamId = req.headers["x-livepeer-stream-id"];
-    const stream = await db.stream.get(streamId?.toString() ?? "");
-    if (!stream) {
-      return res.status(404).json({ errors: ["stream not found"] });
+    const streamId = req.headers["x-livepeer-stream-id"]?.toString();
+    if (streamId) {
+      const stream = await db.stream.get(streamId);
+      if (!stream || stream.deleted) {
+        return res.status(404).json({ errors: ["stream not found"] });
+      }
+      const hasAccess = stream.userId === req.user.id || req.user.admin;
+      if (!hasAccess) {
+        return res.status(403).json({ errors: ["access forbidden"] });
+      }
     }
-    const hasAccess = stream.userId === req.user.id || req.user.admin;
-    if (!hasAccess) {
-      return res.status(403).json({ errors: ["access forbidden"] });
+    const assetId = req.headers["x-livepeer-asset-id"]?.toString();
+    if (assetId) {
+      const asset = await db.asset.get(assetId);
+      if (!asset || asset.deleted) {
+        return res.status(404).json({ errors: ["asset not found"] });
+      }
+      const hasAccess = asset.userId === req.user.id || req.user.admin;
+      if (!hasAccess) {
+        return res.status(403).json({ errors: ["access forbidden"] });
+      }
     }
     res.status(204).end();
   }
