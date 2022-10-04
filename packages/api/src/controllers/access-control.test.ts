bc91dd7718f220d4fdf0ad85ee5711742769a7b9
gioelecerati <50955448+gioelecerati@users.noreply.github.com>
2022-10-04 02:34:12 +0200

    accessControl: Add /gate endpoint for Catalyst integration (#1317)
    * access-control: added /gate endpoint draft & tests
    
    * access-control: added public gate test
    
    * access-control: added unspecified gate policy test
    
    * access-control: check disabled key
    
    * access-control: empty response on 200
    
    * access-control: 200 to 204s
    
    * access-control: 204 also in tests
    
    * access-control: fix client tokens in tests - asset tests - added asset to gated contents
    
    * access-contro: 404 for content not found
    
    * access-control: fix tests & add deleted or disabled keys test
    
    * access-control: added unknown policy type as default case
    
    * access-control: jwt instead of signed as type in playback policy
    
    * access-control: cache only on allowed playback
    
    * access-control: check if keys are not multiple
    
    * access-control: disallow playback if user is suspended & tests
    
    * access-control: added lastSeen tracking
    
    * access-control: remove unused stuff

---
+++ packages/api/src/controllers/access-control.test.ts
@@ -0,0 +1,232 @@
+import serverPromise, { TestServer } from "../test-server";
+import { TestClient, clearDatabase, setupUsers } from "../test-helpers";
+import { SigningKey, SigningKeyResponsePayload, User } from "../schema/types";
+import { WithID } from "../store/types";
+import { Asset } from "../schema/types";
+import { v4 as uuid } from "uuid";
+import { db } from "../store";
+import { generateUniquePlaybackId } from "./generate-keys";
+
+// includes auth file tests
+
+let server: TestServer;
+let mockAdminUserInput: User;
+let mockNonAdminUserInput: User;
+
+// jest.setTimeout(70000)
+
+beforeAll(async () => {
+  server = await serverPromise;
+
+  mockAdminUserInput = {
+    email: "user_admin@gmail.com",
+    password: "x".repeat(64),
+  };
+
+  mockNonAdminUserInput = {
+    email: "user_non_admin@gmail.com",
+    password: "y".repeat(64),
+  };
+});
+
+afterEach(async () => {
+  await clearDatabase(server);
+});
+
+describe("controllers/signing-key", () => {
+  describe("basic CRUD with JWT authorization", () => {
+    let client: TestClient;
+    let adminUser: User;
+    let adminToken: string;
+    let nonAdminUser: User;
+    let nonAdminToken: string;
+    let signingKey: WithID<SigningKey>;
+    let gatedPlaybackId: string;
+    let publicPlaybackId: string;
+    let gatedAsset: WithID<Asset>;
+
+    beforeEach(async () => {
+      ({ client, adminUser, adminToken, nonAdminUser, nonAdminToken } =
+        await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
+      client.jwtAuth = nonAdminToken;
+      let created: SigningKeyResponsePayload = await client
+        .post("/access-control/signing-key")
+        .then((res) => res.json());
+      let res = await client.get(`/access-control/signing-key/${created.id}`);
+      signingKey = await res.json();
+      const res2 = await client.post("/stream", {
+        name: "test",
+        playbackPolicy: {
+          type: "jwt",
+        },
+      });
+      const stream = await res2.json();
+      gatedPlaybackId = stream.playbackId;
+      const res3 = await client.post("/stream", {
+        name: "test",
+        playbackPolicy: {
+          type: "public",
+        },
+      });
+      const publicStream = await res3.json();
+      publicPlaybackId = publicStream.playbackId;
+      let id = uuid();
+      gatedAsset = await db.asset.create({
+        id,
+        name: "test-storage",
+        createdAt: Date.now(),
+        playbackId: await generateUniquePlaybackId(id),
+        playbackPolicy: {
+          type: "jwt",
+        },
+        status: {
+          phase: "ready",
+          updatedAt: Date.now(),
+        },
+        userId: nonAdminUser.id,
+      });
+    });
+
+    it("should create a gate stream and allow playback with given public key", async () => {
+      client.jwtAuth = adminToken;
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${gatedPlaybackId}`,
+        pub: signingKey.publicKey,
+        type: "jwt",
+      });
+
+      expect(res2.status).toBe(204);
+    });
+
+    it("should not allow playback on not existing streams or public keys", async () => {
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+0000000000123`,
+        pub: signingKey.publicKey,
+        type: "jwt",
+      });
+      expect(res.status).toBe(404);
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${gatedPlaybackId}`,
+        pub: "00000000000000",
+        type: "jwt",
+      });
+      expect(res2.status).toBe(403);
+    });
+
+    it("should not allow playback if stream is gated an pub is missing", async () => {
+      client.jwtAuth = adminToken;
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${gatedPlaybackId}`,
+        type: "jwt",
+      });
+      expect(res2.status).toBe(403);
+    });
+
+    it("should not allow playback if stream and public key does not share the same owner", async () => {
+      client.jwtAuth = adminToken;
+      const otherStream = await client.post("/stream", {
+        name: "test",
+        playbackPolicy: {
+          type: "jwt",
+        },
+      });
+      const otherStreamPlaybackId = (await otherStream.json()).playbackId;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${otherStreamPlaybackId}`,
+        pub: signingKey.publicKey,
+        type: "jwt",
+      });
+      expect(res.status).toBe(403);
+    });
+
+    it("should allow playback on public playbackId with and without a public key provided", async () => {
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${publicPlaybackId}`,
+        type: "jwt",
+      });
+      expect(res.status).toBe(204);
+      client.jwtAuth = nonAdminToken;
+      const stream = await client.post("/stream", {
+        name: "test",
+      });
+      client.jwtAuth = adminToken;
+      const streamPlaybackId = (await stream.json()).playbackId;
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${streamPlaybackId}`,
+        type: "jwt",
+      });
+      expect(res2.status).toBe(204);
+    });
+
+    it("should allow playback if playbackPolicy is not specified", async () => {
+      client.jwtAuth = nonAdminToken;
+      const stream = await client.post("/stream", {
+        name: "test",
+      });
+      client.jwtAuth = adminToken;
+      const streamPlaybackId = (await stream.json()).playbackId;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${streamPlaybackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res.status).toBe(204);
+    });
+
+    it("should allow playback on gated asset", async () => {
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res.status).toBe(204);
+    });
+
+    it("should not allow playback if user is suspended", async () => {
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res.status).toBe(204);
+      await db.user.update(gatedAsset.userId, { suspended: true });
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res2.status).toBe(403);
+      await db.user.update(gatedAsset.userId, { suspended: false });
+    });
+
+    it("should not allow playback if signing key is deleted or disabled", async () => {
+      client.jwtAuth = nonAdminToken;
+      await client.patch(`/access-control/signing-key/${signingKey.id}`, {
+        disabled: true,
+      });
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedPlaybackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res.status).toBe(403);
+      client.jwtAuth = nonAdminToken;
+      await client.patch(`/access-control/signing-key/${signingKey.id}`, {
+        disabled: false,
+      });
+      await client.delete(`/access-control/signing-key/${signingKey.id}`);
+      client.jwtAuth = adminToken;
+      const res2 = await client.post("/access-control/gate", {
+        stream: `video+${gatedPlaybackId}`,
+        type: "jwt",
+        pub: signingKey.publicKey,
+      });
+      expect(res2.status).toBe(403);
+    });
+  });
+});
--- packages/api/src/controllers/access-control.ts
+++ packages/api/src/controllers/access-control.ts
@@ -1,5 +1,16 @@
-import { Router } from "express";
 import signingKeyApp from "./signing-key";
+import { authorizer } from "../middleware";
+import { validatePost } from "../middleware";
+import { Router } from "express";
+import _ from "lodash";
+import { db } from "../store";
+import sql from "sql-template-strings";
+import {
+  NotFoundError,
+  ForbiddenError,
+  BadRequestError,
+} from "../store/errors";
+import tracking from "../middleware/tracking";
 
 const accessControl = Router();
 
@@ -7,5 +18,78 @@ const app = Router();
 
 accessControl.use("/signing-key", signingKeyApp);
 app.use("/access-control", accessControl);
+
+accessControl.post(
+  "/gate",
+  authorizer({ anyAdmin: true }),
+  validatePost("access-control-gate-payload"),
+  async (req, res) => {
+    const playbackId = req.body.stream.replace("video+", "");
+    const content =
+      (await db.stream.getByPlaybackId(playbackId)) ||
+      (await db.asset.getByPlaybackId(playbackId));
+
+    if (!content || content.deleted) {
+      throw new NotFoundError("Content not found");
+    }
+
+    const playbackPolicyType = content.playbackPolicy?.type ?? "public";
+
+    if (playbackPolicyType === "public") {
+      res.set("Cache-Control", "max-age=120,stale-while-revalidate=600");
+      res.status(204);
+      return res.end();
+    } else if (playbackPolicyType === "jwt") {
+      if (!req.body.pub) {
+        throw new ForbiddenError("Stream is gated and requires a public key");
+      }
+
+      const query = [];
+      query.push(sql`signing_key.data->>'publicKey' = ${req.body.pub}`);
+      const [signingKeyOutput] = await db.signingKey.find(query, {
+        limit: 2,
+      });
+
+      if (signingKeyOutput.length == 0) {
+        throw new ForbiddenError(
+          "Stream is gated and corresponding public key not found"
+        );
+      }
+
+      if (signingKeyOutput.length > 1) {
+        throw new BadRequestError(
+          "Multiple signing keys found for the same public key. This shouldn't happen."
+        );
+      }
+
+      const signingKey = signingKeyOutput[0];
+
+      if (signingKey.userId !== content.userId) {
+        throw new ForbiddenError(
+          "The stream and the public key do not share the same owner"
+        );
+      }
+
+      if (signingKey.disabled || signingKey.deleted) {
+        throw new ForbiddenError("The public key is disabled or deleted");
+      }
+
+      const user = await db.user.get(content.userId);
+
+      if (user.suspended) {
+        throw new ForbiddenError("The owner of the content is suspended");
+      }
+
+      tracking.recordSigningKeyValidation(signingKey.id);
+      res.set("Cache-Control", "max-age=120,stale-while-revalidate=600");
+      res.status(204);
+      return res.end();
+    } else {
+      throw new BadRequestError(
+        `unknown playbackPolicy type: ${playbackPolicyType}`
+      );
+    }
+  }
+);
 
 export default accessControl;
