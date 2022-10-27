ec268220ceb1b1ea17bd9c07415c67262c962b17
Chase Adams <c@cadams.io>
2022-10-27 11:00:00 -0700

    fix: added import to CORS enabled routes

--- packages/api/src/middleware/auth.ts
+++ packages/api/src/middleware/auth.ts
@@ -208,6 +208,7 @@ export const corsApiKeyAccessRules: AuthRule[] = [
   {
     methods: ["post"],
     resources: [
+      "/asset/import",
       "/asset/request-upload",
       "/asset/:id/transcode",
       "/asset/transcode", // legacy, remove
