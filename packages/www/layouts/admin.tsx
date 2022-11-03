7aa50827e3e4847edd950edf6f05f1af76e6e345
Claudio Carnino <claudio@livepeer.org>
2022-11-03 17:17:58 +0000

    Revert "Revert "fix: remove footer from admin layout (#1438)""
    This reverts commit 78851f050997416250f33594e338cb4825512dcc.

--- packages/www/layouts/admin.tsx
+++ packages/www/layouts/admin.tsx
@@ -3,7 +3,6 @@ import { jsx } from "theme-ui";
 import { NextSeo } from "next-seo";
 import { withEmailVerifyMode } from "./withEmailVerifyMode";
 import { DefaultNav } from "@components/Site/Navigation";
-import Footer from "@components/Site/Footer";
 import { Flex, Box } from "@theme-ui/components";
 import { useEffect } from "react";
 import ReactGA from "react-ga";
@@ -128,7 +127,6 @@ const Layout = ({
               )}
               <Box css={{ position: "relative" }}>{children}</Box>
             </Flex>
-            <Footer />
           </Flex>
         </Box>
       </ThemeProvider>
