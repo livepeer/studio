ddd27296aa26941cd866c2d05bfde22e6cd2d67d
Claudio Carnino <claudio@livepeer.org>
2022-11-09 17:04:51 +0000

    Trying to fix the basic component test

--- packages/www/__tests__/index.test.tsx
+++ packages/www/__tests__/index.test.tsx
@@ -1,9 +1,12 @@
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import ActiveStreamsBadge from "../components/Dashboard/ActiveStreamsBadge";
 
 describe("CreateDialog component", () => {
   it("example", () => {
-    render(<main>Ciao</main>);
+    render(<ActiveStreamsBadge />);
+
+    // render(<main>Ciao</main>);
     const main = screen.getByRole("main");
     expect(main).toBeInTheDocument();
   });
--- packages/www/jest.config.js
+++ packages/www/jest.config.js
@@ -1,17 +1,7 @@
-// jest.config.js
+console.log("#### CONFIG ####");
+
 module.exports = {
-  collectCoverage: true,
-  // on node 14.x coverage provider v8 offers good speed and more or less good report
-  coverageProvider: "v8",
-  collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
-    "!<rootDir>/out/**",
-    "!<rootDir>/.next/**",
-    "!<rootDir>/*.config.js",
-    "!<rootDir>/coverage/**",
-  ],
+  collectCoverage: false,
   moduleNameMapper: {
     // Handle CSS imports (with CSS modules)
     // https://jestjs.io/docs/webpack#mocking-css-modules
@@ -25,11 +15,11 @@ module.exports = {
     "^.+\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$/i": `<rootDir>/__mocks__/fileMock.js`,
 
     // Handle module aliases
-    "^@/components/(.*)$": "<rootDir>/components/$1",
-    "^@/lib/(.*)$": "<rootDir>/lib/$1",
+    "^@components/(.*)$": "<rootDir>/components/$1",
+    "^@lib/(.*)$": "<rootDir>/lib/$1",
   },
   // Add more setup options before each test is run
-  // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
   testEnvironment: "jsdom",
   transform: {
