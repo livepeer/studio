49b7d9e1f7ed9693a50738d9d3ef6854695c8b6c
gioelecerati <gioele@cerati.org>
2022-11-18 15:01:36 +0100

    access-control: gate: error logs

--- packages/api/src/controllers/access-control.ts
+++ packages/api/src/controllers/access-control.ts
@@ -29,6 +29,9 @@ accessControl.post(
       (await db.asset.getByPlaybackId(playbackId));
 
     if (!content || content.deleted) {
+      console.log(`
+        access-control: gate: content not found for playbackId ${playbackId}, disallowing playback
+      `);
       throw new NotFoundError("Content not found");
     }
 
@@ -49,6 +52,9 @@ accessControl.post(
       return res.end();
     } else if (playbackPolicyType === "jwt") {
       if (!req.body.pub) {
+        console.log(`
+          access-control: gate: no pub provided for playbackId ${playbackId}, disallowing playback
+        `);
         throw new ForbiddenError("Content is gated and requires a public key");
       }
 
@@ -59,12 +65,18 @@ accessControl.post(
       });
 
       if (signingKeyOutput.length == 0) {
+        console.log(`
+          access-control: gate: content with playbackId ${playbackId} is gated but corresponding public key not found for key ${req.body.pub}, disallowing playback
+        `);
         throw new ForbiddenError(
           "Content is gated and corresponding public key not found"
         );
       }
 
       if (signingKeyOutput.length > 1) {
+        console.log(`
+          access-control: gate: content contentId ${content.id} is gated but multiple public keys found for key ${req.body.pub}, disallowing playback
+        `);
         throw new BadRequestError(
           "Multiple signing keys found for the same public key."
         );
@@ -80,6 +92,9 @@ accessControl.post(
       }
 
       if (signingKey.disabled || signingKey.deleted) {
+        console.log(`
+          access-control: disallowing playback for contentId=${content.id} the public key pub=${signingKey.id} is disabled or deleted
+        `);
         throw new ForbiddenError("The public key is disabled or deleted");
       }
 
@@ -88,6 +103,9 @@ accessControl.post(
       res.status(204);
       return res.end();
     } else {
+      console.log(`
+        access-control: gate: content with playbackId ${playbackId} is gated but playbackPolicyType ${playbackPolicyType} is not supported, disallowing playback
+      `);
       throw new BadRequestError(
         `unknown playbackPolicy type: ${playbackPolicyType}`
       );
