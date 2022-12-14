2161a38fe832e9e1b16906eb19a21ff7602cfa56
Chase Adams <c@cadams.io>
2022-12-14 10:29:13 -0700

    fix: renamed tracking for player

--- packages/www/components/Site/AppPlayer/index.tsx
+++ packages/www/components/Site/AppPlayer/index.tsx
@@ -5,15 +5,18 @@ import { memo, useCallback } from "react";
 interface AppPlayerProps {
   playbackUrl: string;
   autoPlay?: boolean;
+  type: "asset" | "stream";
 }
 
-const AppPlayer = ({ playbackUrl, autoPlay = true }: AppPlayerProps) => {
+const AppPlayer = ({ playbackUrl, autoPlay = true, type }: AppPlayerProps) => {
   const mediaElementRef = useCallback((element: HTMLMediaElement) => {
     mux.monitor(element, {
       debug: false,
       data: {
         env_key: "8oj27fenun6v4ffvrgn6ehc7m",
-        player_name: "Studio Dashboard Player",
+        player_name: `Studio Dashboard Player (${
+          type === "asset" ? "Asset" : "Stream"
+        })`,
         player_env: process.env.NEXT_PUBLIC_VERCEL_ENV ?? "development",
       },
     });
