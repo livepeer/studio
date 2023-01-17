93a5ac108e06e70646166647482853d654c230d3
Victor Elias <victorgelias@gmail.com>
2023-01-16 21:44:25 -0300

    api: Implement API to change experiment audience

--- packages/api/src/controllers/experiment.ts
+++ packages/api/src/controllers/experiment.ts
@@ -7,8 +7,24 @@ import {
   BadRequestError,
 } from "../store/errors";
 import { toStringValues } from "./helpers";
-import { User } from "../schema/types";
-import { authorizer } from "../middleware";
+import { ExperimentAudiencePayload, User } from "../schema/types";
+import { authorizer, validatePost } from "../middleware";
+
+async function toUserIds(emailOrIds: string[]) {
+  return Promise.all(
+    emailOrIds?.map(async (emailOrId) => {
+      let user = await db.user.get(emailOrId);
+      if (!user) {
+        const [users] = await db.user.find({ email: emailOrId });
+        if (users?.length === 0) {
+          throw new NotFoundError("user not found: " + emailOrId);
+        }
+        user = users[0];
+      }
+      return user.id;
+    })
+  );
+}
 
 const app = Router();
 
@@ -46,31 +62,29 @@ app.get("/check/:experiment", async (req, res) => {
   res.status(204).end();
 });
 
-app.post("/gatekeep", authorizer({ anyAdmin: true }), async (req, res) => {
-  let user: User;
-  if (req.user) {
-    user = req.user;
-  } else {
-    let { userId, playbackId } = toStringValues(req.query);
-    if (!userId) {
-      if (!playbackId) {
-        throw new BadRequestError(
-          "must be authenticated or provide userId or playbackId"
-        );
-      }
-      const content =
-        (await db.asset.getByPlaybackId(playbackId)) ||
-        (await db.stream.getByPlaybackId(playbackId));
-      if (!content) {
-        throw new NotFoundError("content not found");
-      }
-      userId = content.userId;
-    }
-    user = await db.user.get(userId);
-    if (!user || user.suspended) {
-      throw new NotFoundError("user not found or suspended");
-    }
+app.post(
+  "/:experiment/audience",
+  authorizer({ anyAdmin: true }),
+  validatePost("experiment-audience-payload"),
+  async (req, res) => {
+    const { experiment: experimentQuery } = req.params;
+    const { addUsers, removeUsers } = req.body as ExperimentAudiencePayload;
+
+    const [experiment, addUserIds, removeUserIds] = await Promise.all([
+      db.experiment.getByNameOrId(experimentQuery),
+      toUserIds(addUsers),
+      toUserIds(removeUsers),
+    ]);
+    const userIds = _(experiment.userIds ?? [])
+      .concat(addUserIds)
+      .difference(removeUserIds)
+      .uniq()
+      .value();
+
+    await db.experiment.update(experiment.id, { userIds });
+
+    res.status(204).end();
   }
-});
+);
 
 export default app;
