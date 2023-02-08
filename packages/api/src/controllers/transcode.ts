de7f56174c76b9121571f8403701b0f11ac53526
Max Holland <max@livepeer.org>
2023-02-08 13:53:24 +0000

    fix

--- packages/api/src/controllers/transcode.ts
+++ packages/api/src/controllers/transcode.ts
@@ -17,7 +17,7 @@ app.post(
   validatePost("transcode-payload"),
   async (req, res) => {
     const params = req.body as TranscodePayload;
-    const { catalystPipelineStrategy = external } = req.user.admin
+    const { catalystPipelineStrategy = "external" } = req.user.admin
       ? params
       : {};
 
