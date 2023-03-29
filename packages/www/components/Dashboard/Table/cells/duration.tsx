e80cf497e29da846c188a3bb8a33a266e5596764
Victor Elias <victorgelias@gmail.com>
2023-03-29 17:08:11 -0300

    www: Round session durations instead of ceil

--- packages/www/components/Dashboard/Table/cells/duration.tsx
+++ packages/www/components/Dashboard/Table/cells/duration.tsx
@@ -16,11 +16,13 @@ const DurationCell = <D extends TableData>({
     return "n/a";
   }
   try {
+    const durationMins = Math.round(cell.value.sourceSegmentsDuration / 60);
+    if (!durationMins) {
+      return "<1 minute";
+    }
     const dur = intervalToDuration({
       start: new Date(0),
-      end: new Date(
-        Math.ceil(cell.value.sourceSegmentsDuration / 60) * 60 * 1000 || 0
-      ),
+      end: new Date(durationMins * 60 * 1000 || 0),
     });
     return formatDuration(dur);
   } catch (error) {
