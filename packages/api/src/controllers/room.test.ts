01c76a33009eec492e607a0da547fd020f85e217
Max Holland <max@livepeer.org>
2023-04-21 13:21:24 +0100

    fix test

--- packages/api/src/controllers/room.test.ts
+++ packages/api/src/controllers/room.test.ts
@@ -1,10 +1,7 @@
 import serverPromise, { TestServer } from "../test-server";
 import { TestClient, clearDatabase, setupUsers } from "../test-helpers";
-import { v4 as uuid } from "uuid";
 import { User } from "../schema/types";
-import { db } from "../store";
 import { RoomServiceClient } from "livekit-server-sdk";
-import { CreateOptions } from "livekit-server-sdk/dist/RoomServiceClient";
 jest.mock("livekit-server-sdk");
 const MockedRoomServiceClient =
   RoomServiceClient as jest.Mock<RoomServiceClient>;
@@ -38,31 +35,27 @@ describe("controllers/room", () => {
   let nonAdminUser: User;
   let nonAdminToken: string;
 
-  const createStub = jest.fn((options: CreateOptions) => {
-    return Promise.resolve();
-  });
   beforeEach(async () => {
     ({ client, adminUser, adminApiKey, nonAdminUser, nonAdminToken } =
       await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
     client.jwtAuth = nonAdminToken;
-
-    MockedRoomServiceClient.mockImplementation(() => {
-      return {
-        createRoom: createStub,
-      };
-    });
   });
 
   describe("room creation", () => {
     it("should create a room", async () => {
+      let roomServiceClient = new RoomServiceClient("");
+      const mockCreateRoom = jest.spyOn(roomServiceClient, "createRoom");
+      MockedRoomServiceClient.mockReturnValueOnce(roomServiceClient);
+      mockCreateRoom.mockReturnValueOnce(Promise.resolve(undefined));
+
       let res = await client.post(`/room`);
 
       expect(res.status).toBe(201);
       const roomRes = await res.json();
       expect(roomRes.id).toBeDefined();
 
-      expect(createStub).toHaveBeenCalledTimes(1);
-      const createOpts = createStub.mock.calls[0][0];
+      expect(mockCreateRoom).toHaveBeenCalledTimes(1);
+      const createOpts = mockCreateRoom.mock.calls[0][0];
       expect(roomRes.id).toBe(createOpts.name);
     });
   });
