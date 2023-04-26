8b4976381c7faf8c17415e06b1fafe4e665798b8
Victor Elias <victor@livepeer.org>
2023-04-26 14:41:09 -0300

    api: Add `creatorId` field to assets (#1700)
    * api/schema: Add creatorId field to asset
    
    * api/schema: Create creatorId on new-asset-payload
    
    * api/asset: Set the creatorId on new assets
    
    * api: Add a test for the new field
    
    * api/test: Fix nested test
    
    * api/schema: Fix tests
    
    Just found out that the type field is obligatory
    and not inferred automatically once you specify
    properties lol

--- packages/api/src/controllers/asset.test.ts
+++ packages/api/src/controllers/asset.test.ts
@@ -170,6 +170,32 @@ describe("controllers/asset", () => {
     });
   });
 
+  it("should store the creator ID as an object", async () => {
+    const spec = {
+      name: "test",
+      url: "https://example.com/test.mp4",
+      creatorId: "i am creator",
+    };
+    let res = await client.post(`/asset/upload/url`, spec);
+    expect(res.status).toBe(201);
+    let {
+      asset: { id: assetId },
+      task: { id: taskId },
+    } = await res.json();
+    expect(assetId).toMatch(/^[a-f0-9-]{36}$/);
+    expect(taskId).toMatch(/^[a-f0-9-]{36}$/);
+
+    res = await client.get(`/asset/${assetId}`);
+    expect(res.status).toBe(200);
+    await expect(res.json()).resolves.toMatchObject({
+      id: assetId,
+      creatorId: {
+        type: "unverified",
+        value: "i am creator",
+      },
+    });
+  });
+
   describe("asset inline storage", () => {
     let asset: WithID<Asset>;
 
