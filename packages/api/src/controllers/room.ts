5ba89c849c4381c63612c5a3ab95cd522faae6dc
Max Holland <max@livepeer.org>
2023-04-26 14:50:06 +0100

    review comments

--- packages/api/src/controllers/room.ts
+++ packages/api/src/controllers/room.ts
@@ -3,7 +3,11 @@ import { RoomServiceClient, WebhookReceiver } from "livekit-server-sdk";
 import { v4 as uuid } from "uuid";
 import { authorizer } from "../middleware";
 import { db } from "../store";
-import { ForbiddenError } from "../store/errors";
+import {
+  BadRequestError,
+  ForbiddenError,
+  NotFoundError,
+} from "../store/errors";
 import { Room } from "../schema/types";
 import { WithID } from "../store/types";
 
@@ -26,7 +30,7 @@ app.post("/", authorizer({}), async (req, res) => {
     // timeout in seconds
     emptyTimeout: 15 * 60,
     maxParticipants: 10,
-    metadata: `{"userId": "${req.user.id}"}`,
+    metadata: JSON.stringify({ userId: req.user.id }),
   });
   console.log("livekit room created", room);
 
@@ -45,6 +49,9 @@ app.post("/", authorizer({}), async (req, res) => {
 
 app.get("/:roomId", authorizer({}), async (req, res) => {
   const room = await db.room.get(req.params.roomId);
+  if (!room) {
+    throw new NotFoundError(`room not found`);
+  }
 
   if (!req.user.admin && req.user.id !== room.userId) {
     throw new ForbiddenError(`users may only view their own rooms`);
@@ -66,23 +73,37 @@ function toExternalRoom(room: Room, isAdmin = false) {
 }
 
 // Implement a webhook handler to receive webhooks from Livekit to update our state with room and participant details.
-app.use("/webhook", express.raw({ type: "*/*" }));
-app.post("/webhook", async (req, res) => {
+app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
   const receiver = new WebhookReceiver(
     req.config.livekitApiKey,
     req.config.livekitSecret
   );
-  console.log("livekit webhook auth", req.get("Authorization"));
 
   // event is a WebhookEvent object
-  const event = receiver.receive(req.body, req.get("Authorization"));
+  let event;
+  try {
+    event = receiver.receive(req.body, req.get("Authorization"));
+  } catch (e) {
+    console.log(
+      "failed to receive room webhook. auth:",
+      req.get("Authorization")
+    );
+    throw e;
+  }
 
-  let room: WithID<Room>;
+  const roomId = event.room.name;
+  if (!roomId) {
+    throw new BadRequestError(`no room name on event`);
+  }
+  const room = await db.room.get(roomId);
+  if (!room) {
+    throw new NotFoundError(`room not found`);
+  }
+
   switch (event.event) {
     case "track_published":
     case "participant_joined":
     case "participant_left":
-      const roomId = event.room.name;
       const svc = new RoomServiceClient(
         req.config.livekitHost,
         req.config.livekitApiKey,
@@ -90,10 +111,7 @@ app.post("/webhook", async (req, res) => {
       );
       const participants = await svc.listParticipants(roomId);
 
-      room = await db.room.get(roomId);
       for (const participant of participants) {
-        console.log("tracks", participant.tracks);
-
         if (room.participants[participant.identity] === undefined) {
           room.participants[participant.identity] = {
             identity: participant.identity,
@@ -124,7 +142,6 @@ app.post("/webhook", async (req, res) => {
       break;
     case "room_started":
     case "room_finished":
-      room = await db.room.get(event.room.name);
       room.events.push({
         eventName: event.event,
         timestamp: new Date().getTime(),
