10bcdb62e85058e10c25541cfe94bda93e1907d5
Max Holland <max@hollandhome.co.uk>
2023-04-28 18:10:08 +0100

    Add remaining room APIs (#1705)
    * add more apis
    
    * add tests
    
    * add schemas
    
    * record egress events
    
    * address review comments

--- packages/api/src/controllers/room.test.ts
+++ packages/api/src/controllers/room.test.ts
@@ -1,10 +1,11 @@
 import serverPromise, { TestServer } from "../test-server";
 import { TestClient, clearDatabase, setupUsers } from "../test-helpers";
 import { User } from "../schema/types";
-import { RoomServiceClient } from "livekit-server-sdk";
+import { EgressClient, RoomServiceClient } from "livekit-server-sdk";
 jest.mock("livekit-server-sdk");
 const MockedRoomServiceClient =
   RoomServiceClient as jest.Mock<RoomServiceClient>;
+const MockedEgressClient = EgressClient as jest.Mock<EgressClient>;
 
 let server: TestServer;
 let mockAdminUserInput: User;
@@ -34,22 +35,27 @@ describe("controllers/room", () => {
   let adminApiKey: string;
   let nonAdminUser: User;
   let nonAdminToken: string;
+  let roomServiceClient;
+  let mockCreateRoom: jest.SpyInstance;
+  let egressClient;
 
   beforeEach(async () => {
     ({ client, adminUser, adminApiKey, nonAdminUser, nonAdminToken } =
       await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
     client.jwtAuth = nonAdminToken;
+
+    roomServiceClient = new RoomServiceClient("");
+    egressClient = new EgressClient("");
+
+    MockedRoomServiceClient.mockReturnValue(roomServiceClient);
+    mockCreateRoom = jest.spyOn(roomServiceClient, "createRoom");
+    mockCreateRoom.mockReturnValueOnce(Promise.resolve(undefined));
+    MockedEgressClient.mockReturnValue(egressClient);
   });
 
   describe("room creation", () => {
-    it("should create a room", async () => {
-      let roomServiceClient = new RoomServiceClient("");
-      const mockCreateRoom = jest.spyOn(roomServiceClient, "createRoom");
-      MockedRoomServiceClient.mockReturnValueOnce(roomServiceClient);
-      mockCreateRoom.mockReturnValueOnce(Promise.resolve(undefined));
-
+    it("should create and delete rooms", async () => {
       let res = await client.post(`/room`);
-
       expect(res.status).toBe(201);
       const roomRes = await res.json();
       expect(roomRes.id).toBeDefined();
@@ -64,6 +70,93 @@ describe("controllers/room", () => {
       const getRoomRes = await res.json();
       expect(getRoomRes.id).toBe(roomRes.id);
       expect(getRoomRes.userId).toBeUndefined();
+
+      const mockDeleteRoom = jest.spyOn(roomServiceClient, "deleteRoom");
+      res = await client.delete(`/room/${roomRes.id}`);
+      expect(res.status).toBe(204);
+      expect(mockDeleteRoom).toHaveBeenCalledTimes(1);
+      expect(mockDeleteRoom.mock.calls[0][0]).toBe(roomRes.id);
+
+      // should no longer exist
+      res = await client.get(`/room/${roomRes.id}`);
+      expect(res.status).toBe(404);
+    });
+
+    it("should add and remove participants", async () => {
+      let res = await client.post(`/room`);
+      expect(res.status).toBe(201);
+      const roomRes = await res.json();
+      expect(roomRes.id).toBeDefined();
+
+      res = await client.post(`/room/${roomRes.id}/user`, {
+        name: "display name",
+      });
+      expect(res.status).toBe(201);
+      const resp = await res.json();
+      expect(resp.id).toBeDefined();
+      expect(resp.joinUrl).toBeDefined();
+
+      const mockRemoveParticipant = jest.spyOn(
+        roomServiceClient,
+        "removeParticipant"
+      );
+      res = await client.delete(`/room/${roomRes.id}/user/${resp.id}`);
+      expect(res.status).toBe(204);
+      expect(mockRemoveParticipant).toHaveBeenCalledTimes(1);
+      expect(mockRemoveParticipant.mock.calls[0]).toEqual([
+        roomRes.id,
+        resp.id,
+      ]);
+    });
+
+    it("should start and stop egress", async () => {
+      let res = await client.post(`/room`);
+      expect(res.status).toBe(201);
+      const roomRes = await res.json();
+      expect(roomRes.id).toBeDefined();
+
+      let mockStartEgress = jest.spyOn(
+        egressClient,
+        "startRoomCompositeEgress"
+      );
+      mockStartEgress.mockReturnValueOnce(
+        Promise.resolve({
+          egressId: "egress-id",
+        })
+      );
+      let mockStopEgress = jest.spyOn(egressClient, "stopEgress");
+      mockStopEgress.mockReturnValueOnce(Promise.resolve(undefined));
+
+      res = await client.delete(`/room/${roomRes.id}/egress`);
+      expect(res.status).toBe(400);
+
+      res = await client.post(`/stream`, {
+        name: "stream",
+      });
+      expect(res.status).toBe(201);
+      const streamResp = await res.json();
+
+      res = await client.post(`/room/${roomRes.id}/egress`, {
+        streamId: streamResp.id,
+      });
+      expect(res.status).toBe(204);
+
+      // already started so should 400
+      res = await client.post(`/room/${roomRes.id}/egress`, {
+        streamId: streamResp.id,
+      });
+      expect(res.status).toBe(400);
+
+      res = await client.delete(`/room/${roomRes.id}/egress`);
+      expect(res.status).toBe(204);
+
+      // already stopped so should 400
+      res = await client.delete(`/room/${roomRes.id}/egress`);
+      expect(res.status).toBe(400);
+
+      expect(mockStartEgress).toHaveBeenCalledTimes(1);
+      expect(mockStopEgress).toHaveBeenCalledTimes(1);
+      expect(mockStopEgress.mock.calls[0][0]).toBe("egress-id");
     });
   });
 });
--- packages/api/src/controllers/room.ts
+++ packages/api/src/controllers/room.ts
@@ -1,11 +1,19 @@
 import express, { Router } from "express";
-import { RoomServiceClient, WebhookReceiver } from "livekit-server-sdk";
+import {
+  AccessToken,
+  EgressClient,
+  RoomServiceClient,
+  StreamOutput,
+  StreamProtocol,
+  WebhookReceiver,
+} from "livekit-server-sdk";
 import { v4 as uuid } from "uuid";
-import { authorizer } from "../middleware";
+import { authorizer, validatePost } from "../middleware";
 import { db } from "../store";
 import {
   BadRequestError,
   ForbiddenError,
+  InternalServerError,
   NotFoundError,
 } from "../store/errors";
 import { Room } from "../schema/types";
@@ -37,6 +45,7 @@ app.post("/", authorizer({}), async (req, res) => {
   await db.room.create({
     id: id,
     userId: req.user.id,
+    createdAt: Date.now(),
     participants: {},
     events: [],
   });
@@ -47,15 +56,20 @@ app.post("/", authorizer({}), async (req, res) => {
   });
 });
 
-app.get("/:roomId", authorizer({}), async (req, res) => {
+async function getRoom(req) {
   const room = await db.room.get(req.params.roomId);
-  if (!room) {
+  if (!room || room.deleted) {
     throw new NotFoundError(`room not found`);
   }
 
   if (!req.user.admin && req.user.id !== room.userId) {
-    throw new ForbiddenError(`users may only view their own rooms`);
+    throw new ForbiddenError(`invalid user`);
   }
+  return room;
+}
+
+app.get("/:roomId", authorizer({}), async (req, res) => {
+  const room = await getRoom(req);
 
   res.status(200).json(toExternalRoom(room, req.user.admin));
 });
@@ -64,13 +78,148 @@ function toExternalRoom(room: Room, isAdmin = false) {
   if (isAdmin) {
     return room;
   }
-  room.userId = undefined;
-  for (const key in room.participants) {
-    room.participants[key].tracksPublished = undefined;
+  const roomForResp = {
+    id: room.id,
+    updatedAt: room.updatedAt,
+    createdAt: room.createdAt,
+    participants: room.participants,
+  };
+
+  for (const key in roomForResp.participants) {
+    roomForResp.participants[key].tracksPublished = undefined;
   }
-  room.events = undefined;
-  return room;
+  return roomForResp;
 }
+
+app.delete("/:roomId", authorizer({}), async (req, res) => {
+  const room = await getRoom(req);
+
+  const svc = new RoomServiceClient(
+    req.config.livekitHost,
+    req.config.livekitApiKey,
+    req.config.livekitSecret
+  );
+  await svc.deleteRoom(req.params.roomId);
+
+  await db.room.update(room.id, { deleted: true, deletedAt: Date.now() });
+
+  res.status(204).end();
+});
+
+app.post(
+  "/:roomId/egress",
+  authorizer({}),
+  validatePost("room-egress-payload"),
+  async (req, res) => {
+    const room = await getRoom(req);
+
+    if (room.egressId !== undefined && room.egressId != "") {
+      throw new BadRequestError("egress already started");
+    }
+
+    const stream = await db.stream.get(req.body.streamId);
+    if (
+      !stream ||
+      stream.deleted ||
+      (!req.user.admin && req.user.id !== stream.userId)
+    ) {
+      throw new NotFoundError(`stream not found`);
+    }
+
+    const egressClient = new EgressClient(
+      req.config.livekitHost,
+      req.config.livekitApiKey,
+      req.config.livekitSecret
+    );
+    const output: StreamOutput = {
+      protocol: StreamProtocol.RTMP,
+      urls: [req.config.ingest[0].ingest + "/" + stream.streamKey],
+    };
+
+    const info = await egressClient.startRoomCompositeEgress(
+      req.params.roomId,
+      output,
+      {
+        layout: "speaker-dark",
+        encodingOptions: {
+          keyFrameInterval: 2,
+        },
+      }
+    );
+    console.log("egress started", info);
+    await db.room.update(room.id, {
+      egressId: info.egressId,
+      updatedAt: Date.now(),
+    });
+    res.status(204).end();
+  }
+);
+
+app.delete("/:roomId/egress", authorizer({}), async (req, res) => {
+  const room = await getRoom(req);
+
+  if (room.egressId === undefined || room.egressId == "") {
+    throw new BadRequestError("egress has not started");
+  }
+
+  const egressClient = new EgressClient(
+    req.config.livekitHost,
+    req.config.livekitApiKey,
+    req.config.livekitSecret
+  );
+
+  const info = await egressClient.stopEgress(room.egressId);
+  console.log("egress stopped", info);
+  await db.room.update(room.id, { egressId: "", updatedAt: Date.now() });
+  res.status(204).end();
+});
+
+app.post(
+  "/:roomId/user",
+  authorizer({}),
+  validatePost("room-user-payload"),
+  async (req, res) => {
+    await getRoom(req);
+
+    const id = uuid();
+    const at = new AccessToken(
+      req.config.livekitApiKey,
+      req.config.livekitSecret,
+      {
+        name: req.body.name,
+        identity: id,
+        ttl: 5 * 60,
+      }
+    );
+    at.addGrant({ roomJoin: true, room: req.params.roomId });
+    const token = at.toJwt();
+
+    res.status(201);
+    res.json({
+      id: id,
+      joinUrl:
+        req.config.livekitMeetUrl +
+        "?liveKitUrl=" +
+        req.config.livekitHost +
+        "&token=" +
+        token,
+      token: token,
+    });
+  }
+);
+
+app.delete("/:roomId/user/:participantId", authorizer({}), async (req, res) => {
+  await getRoom(req);
+
+  const svc = new RoomServiceClient(
+    req.config.livekitHost,
+    req.config.livekitApiKey,
+    req.config.livekitSecret
+  );
+  await svc.removeParticipant(req.params.roomId, req.params.participantId);
+
+  res.status(204).end();
+});
 
 // Implement a webhook handler to receive webhooks from Livekit to update our state with room and participant details.
 app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
@@ -97,7 +246,7 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
   }
   const room = await db.room.get(roomId);
   if (!room) {
-    throw new NotFoundError(`room not found`);
+    throw new InternalServerError(`room not found`);
   }
 
   switch (event.event) {
@@ -121,7 +270,7 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
           };
         }
         if (event.event == "participant_left") {
-          room.participants[participant.identity].leftAt = new Date().getTime();
+          room.participants[participant.identity].leftAt = Date.now();
         }
 
         let tracks = room.participants[participant.identity].tracksPublished;
@@ -134,18 +283,22 @@ app.post("/webhook", express.raw({ type: "*/*" }), async (req, res) => {
             height: track.height,
             width: track.width,
             mimeType: track.mimeType,
-            timestamp: new Date().getTime(),
+            timestamp: Date.now(),
           };
         }
       }
+      room.updatedAt = Date.now();
       await db.room.replace(room);
       break;
     case "room_started":
     case "room_finished":
+    case "egress_started":
+    case "egress_ended":
       room.events.push({
         eventName: event.event,
-        timestamp: new Date().getTime(),
+        timestamp: Date.now(),
       });
+      room.updatedAt = Date.now();
       await db.room.replace(room);
       break;
   }
