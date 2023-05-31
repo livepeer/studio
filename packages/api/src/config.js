5a8244c68db39361c0d1e5cec8a9383629f3d641
gioelecerati <gioele@cerati.org>
2023-05-31 20:47:34 +0200

    fix old products

--- packages/api/src/config.js
+++ packages/api/src/config.js
@@ -1,18 +1,30 @@
 exports.products = {
   prod_0: {
     order: 0,
-    name: "Free",
+    name: "Personal",
     lookupKeys: ["price_0"],
-    usage: [],
+    usage: [
+      {
+        name: "Transcoding",
+        description: "Transcoding (minutes)",
+        price: 0.0,
+      },
+    ],
   },
   prod_1: {
-    order: 0,
+    order: 1,
     name: "Pro",
     lookupKeys: ["price_1"],
-    usage: [],
+    usage: [
+      {
+        name: "Transcoding",
+        description: "Transcoding (minutes)",
+        price: 0.005,
+      },
+    ],
   },
   prod_2: {
-    order: 0,
+    order: 2,
     name: "Business",
     lookupKeys: ["price_2"],
     usage: [],
--- packages/www/pages/dashboard/billing/plans.tsx
+++ packages/www/pages/dashboard/billing/plans.tsx
@@ -48,7 +48,7 @@ const PlansPage = () => {
         <Plans
           dashboard={true}
           stripeProductId={
-            user?.stripeProductId ? user.stripeProductId : "hacker_1"
+            user?.stripeProductId ? user.stripeProductId : "prod_0"
           }
         />
       </Box>
