dacf72fee445681618642ef2df58553e2c7be297
Hunter Hillman <hunterhillman@Hunters-MacBook-Pro-109.local>
2023-06-23 14:07:43 -0700

    add tooltip

--- packages/www/components/AssetDetails/AssetPlayerBox/ProcessingProgress.tsx
+++ packages/www/components/AssetDetails/AssetPlayerBox/ProcessingProgress.tsx
@@ -1,13 +1,29 @@
 import { useMemo } from "react";
 import { Asset } from "livepeer";
 import Progress from "./Progress";
+import { Flex, Box, Tooltip } from "@livepeer/design-system";
+import { QuestionMarkCircledIcon as Help } from "@radix-ui/react-icons";
 
 const ProcessingProgress = ({ asset }: { asset?: Asset }) => {
   const percentage = useMemo(() => {
     const progress = asset?.status?.progress ?? 0;
     return Math.floor(progress * 100);
   }, [asset]);
-  return <Progress text="Processing" percentage={percentage} />;
+  return (
+    <Flex align="center">
+      <Tooltip
+        multiline
+        content={
+          <Box>
+            Your video can now be played. In the background, it is converted
+            into several quality levels so that it can be played smoothly by all
+            viewers.
+          </Box>
+        }>
+        <Help />
+      </Tooltip>
+    </Flex>
+  );
 };
 
 export default ProcessingProgress;
