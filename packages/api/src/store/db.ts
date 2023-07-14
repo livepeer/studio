a1b5e65d9e6a36c25da2fa129f5602e66ac5078b
Eli Mallon <eli@livepeer.org>
2023-07-14 14:40:03 -0700

    api: we don't use this table anymore, right?

--- packages/api/src/store/db.ts
+++ packages/api/src/store/db.ts
@@ -14,7 +14,6 @@ import {
   Region,
   WebhookResponse,
   Session,
-  CdnUsageLast,
   SigningKey,
   Room,
   Attestation,
@@ -29,7 +28,6 @@ import { kebabToCamel } from "../util";
 import { QueryOptions, WithID } from "./types";
 import MultistreamTargetTable from "./multistream-table";
 import WebhookTable from "./webhook-table";
-import { CdnUsageTable } from "./cdn-usage-table";
 import AssetTable from "./asset-table";
 import TaskTable from "./task-table";
 import ExperimentTable from "./experiment-table";
@@ -84,8 +82,6 @@ export class DB {
   passwordResetToken: Table<PasswordResetToken>;
   region: Table<Region>;
   session: Table<DBSession>;
-  cdnUsageLast: Table<CdnUsageLast>;
-  cdnUsageTable: CdnUsageTable;
   room: Table<Room>;
 
   postgresUrl: string;
@@ -192,10 +188,6 @@ export class DB {
       schema: schemas["webhook-response"],
     });
     this.session = makeTable<Session>({ db: this, schema: schemas["session"] });
-    this.cdnUsageLast = makeTable<CdnUsageLast>({
-      db: this,
-      schema: schemas["cdn-usage-last"],
-    });
     this.room = makeTable<Room>({ db: this, schema: schemas["room"] });
 
     const tables = Object.entries(schema.components.schemas).filter(
@@ -207,9 +199,6 @@ export class DB {
         return this[camelName].ensureTable();
       })
     );
-
-    this.cdnUsageTable = new CdnUsageTable(this);
-    await this.cdnUsageTable.makeTable();
   }
 
   queryWithOpts<T, I extends any[] = any[]>(
