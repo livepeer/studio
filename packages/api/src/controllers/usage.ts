196025289eabf4cabd2bc5a6a396730e9215f0c8
gioelecerati <50955448+gioelecerati@users.noreply.github.com>
2023-08-11 20:23:09 +0200

    usage: restore cron reporting (#1841)

--- packages/api/src/controllers/usage.ts
+++ packages/api/src/controllers/usage.ts
@@ -34,13 +34,53 @@ app.post(
   async (req, res) => {
     let { fromTime, toTime } = req.query;
 
+    /* New usage report for billing
     let token = req.token;
-
     // New automated billing usage report
     let result = await reportUsage(req, token);
 
     res.status(200);
     res.json(result);
+    let { fromTime, toTime } = req.query;
+    */
+
+    // if time range isn't specified return all usage
+    if (!fromTime) {
+      let rows = (
+        await db.usage.find(
+          {},
+          { limit: 1, order: "data->>'date' DESC", useReplica: true }
+        )
+      )[0];
+
+      if (rows.length) {
+        fromTime = rows[0].date; // get last updated date from cache
+      } else {
+        fromTime = +new Date(2020, 0); // start at beginning
+      }
+    }
+
+    if (!toTime) {
+      toTime = +new Date();
+    }
+
+    let usageHistory = await db.stream.usageHistory(fromTime, toTime, {
+      useReplica: true,
+    });
+
+    // store each day of usage
+    for (const row of usageHistory) {
+      const dbRow = await req.store.get(`usage/${row.id}`);
+      // if row already exists in cache, update it, otherwise create it
+      if (dbRow) {
+        await req.store.replace({ kind: "usage", ...row });
+      } else {
+        await req.store.create({ kind: "usage", ...row });
+      }
+    }
+
+    res.status(200);
+    res.json(usageHistory);
   }
 );
 
