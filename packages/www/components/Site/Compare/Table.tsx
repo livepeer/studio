610889f6d2f2241277f154ac607d7d1598167fdd
Adam Soffer <adam@livepeer.org>
2023-09-18 12:00:29 -0400

    Fix compare page (#1888)

--- packages/www/components/Site/Compare/Table.tsx
+++ packages/www/components/Site/Compare/Table.tsx
@@ -1,6 +1,17 @@
-import { Table } from "@radix-ui/themes";
 import { CheckIcon } from "@radix-ui/react-icons";
-import { Flex, Box, Text, Status, Badge } from "@livepeer/design-system";
+import {
+  Flex,
+  Box,
+  Text,
+  Status,
+  Badge,
+  Table,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@livepeer/design-system";
 
 const Check = () => {
   return (
@@ -23,50 +34,62 @@ const CompareTable = () => {
   return (
     <Box
       css={{
-        ".rt-TableCell": { verticalAlign: "middle" },
-        "[data-radix-scroll-area-viewport] > div": {
+        position: "relative",
+        overflow: "scroll",
+      }}>
+      <Table
+        css={{
           minWidth: "950px !important",
           width: "100%",
-        },
-      }}>
-      <Table.Root>
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell />
-            <Table.ColumnHeaderCell>
-              <Flex align="center" gap={2} css={{ fontSize: "$5" }}>
+        }}>
+        <Thead>
+          <Tr>
+            <Th />
+            <Th>
+              <Flex
+                align="center"
+                gap={2}
+                css={{ color: "black", fontSize: "$5", fontWeight: 600 }}>
                 <Status variant="green" />
                 Livepeer Studio
               </Flex>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>
-              <Flex align="center" gap={2} css={{ fontSize: "$5" }}>
+            </Th>
+            <Th>
+              <Flex
+                align="center"
+                gap={2}
+                css={{ color: "black", fontSize: "$5", fontWeight: 600 }}>
                 <Status variant="red" />
                 Mux
               </Flex>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell>
-              <Flex align="center" gap={2} css={{ fontSize: "$5" }}>
+            </Th>
+            <Th>
+              <Flex
+                align="center"
+                gap={2}
+                css={{ color: "black", fontSize: "$5", fontWeight: 600 }}>
                 <Status css={{ bc: "$orange9" }} />
                 Cloudflare Stream
               </Flex>
-            </Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
+            </Th>
+          </Tr>
+        </Thead>
         {/* Cost */}
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>
-              <Box css={{ fontSize: "$4" }}>Cost</Box>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>
+        <Thead>
+          <Tr>
+            <Th>
+              <Box css={{ fontWeight: 500, color: "black", fontSize: "$4" }}>
+                Cost
+              </Box>
+            </Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Th>
               <Flex gap={1} css={{ mb: "$1" }}>
                 <Text>Transcoding (10k mins)</Text>
               </Flex>
@@ -76,290 +99,296 @@ const CompareTable = () => {
               <Flex gap={1} css={{ mb: "$1" }}>
                 <Text>Delivery (500k mins)</Text>
               </Flex>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Text>$500</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text css={{ gap: "$2", display: "flex", alignItems: "center" }}>
                 $1212
                 <Badge variant="green">+140%</Badge>
               </Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text css={{ gap: "$2", display: "flex", alignItems: "center" }}>
                 $750 <Badge variant="green">+50%</Badge>
               </Text>
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
+            </Td>
+          </Tr>
+        </Tbody>
         {/* Performance */}
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>
+        <Thead>
+          <Tr>
+            <Th>
               <Flex direction="column" gap={2}>
-                <Box css={{ fontSize: "$4" }}>Performance</Box>
+                <Box css={{ fontWeight: 500, color: "black", fontSize: "$4" }}>
+                  Performance
+                </Box>
                 <Text size={2} css={{ fontStyle: "italic" }}>
                   Based on a controlled-environment test
                 </Text>
               </Flex>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Th>
               <Text>Expected Latency</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Text>0.5 - 3s</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text>4s</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text>0.5 - 3s</Text>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Video Startup Time (TTFF)</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Text>0.2s</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text>0.4s</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text>0.2s</Text>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Uptime SLA</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
+            </Td>
+          </Tr>
+        </Tbody>
         {/* Key features */}
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>
-              <Box css={{ fontSize: "$4" }}>Key features</Box>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>
+        <Thead>
+          <Tr>
+            <Th>
+              <Box css={{ fontWeight: 500, color: "black", fontSize: "$4" }}>
+                Key features
+              </Box>
+            </Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Th>
               <Text css={{ maxWidth: 220 }}>
                 Decentralized physical infrastructure network
               </Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
+            </Td>
+            <Td>—</Td>
+            <Td>—</Td>
+          </Tr>
 
-          <Table.Row>
-            <Table.RowHeaderCell>
+          <Tr>
+            <Th>
               <Text>Open source with self-host option</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+            <Td>—</Td>
+            <Td>—</Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Embeddable player</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>—</Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Analytics API & Visualization</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Text>Available at additional cost</Text>
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Transcoding API</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+            <Td>—</Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Developer SDKs</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>In-browser Broadcasting</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>—</Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Access control</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
+            </Td>
+            <Td>—</Td>
+          </Tr>
 
-          <Table.Row>
-            <Table.RowHeaderCell>
+          <Tr>
+            <Th>
               <Text>Multistreaming</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Video clipping</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>ETA Sept. 2023</Table.Cell>
-            <Table.Cell>
+            </Th>
+            <Td>ETA Sept. 2023</Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Thumbnails</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>ETA Sept. 2023</Table.Cell>
-            <Table.Cell>
+            </Th>
+            <Td>ETA Sept. 2023</Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Captions</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>ETA Sept. 2023</Table.Cell>
-            <Table.Cell>
+            </Th>
+            <Td>ETA Sept. 2023</Td>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>
+            </Td>
+            <Td>
               <Check />
-            </Table.Cell>
-          </Table.Row>
-        </Table.Body>
+            </Td>
+          </Tr>
+        </Tbody>
         {/* Onchain integrations */}
-        <Table.Header>
-          <Table.Row>
-            <Table.ColumnHeaderCell>
-              <Box css={{ fontSize: "$4" }}>Onchain features</Box>
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell></Table.ColumnHeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.RowHeaderCell>
+        <Thead>
+          <Tr>
+            <Th>
+              <Box css={{ fontWeight: 500, color: "black", fontSize: "$4" }}>
+                Onchain features
+              </Box>
+            </Th>
+            <Th></Th>
+            <Th></Th>
+            <Th></Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          <Tr>
+            <Th>
               <Text>Decentralized storage integrations</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+            <Td>—</Td>
+            <Td>—</Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Token-gated video</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
-          <Table.Row>
-            <Table.RowHeaderCell>
+            </Td>
+            <Td>—</Td>
+            <Td>—</Td>
+          </Tr>
+          <Tr>
+            <Th>
               <Text>Wallet-based engagement analytics</Text>
-            </Table.RowHeaderCell>
-            <Table.Cell>
+            </Th>
+            <Td>
               <Check />
-            </Table.Cell>
-            <Table.Cell>—</Table.Cell>
-            <Table.Cell>—</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table.Root>
+            </Td>
+            <Td>—</Td>
+            <Td>—</Td>
+          </Tr>
+        </Tbody>
+      </Table>
     </Box>
   );
 };
