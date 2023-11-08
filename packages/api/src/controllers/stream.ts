b98b95a67ee5c1ce0d51de17b0a6f76ca6f4b4e6
Rafał Leszko <rafal@livepeer.org>
2023-11-08 08:47:23 +0100

    Remove unused condition

--- packages/api/src/controllers/stream.ts
+++ packages/api/src/controllers/stream.ts
@@ -1363,11 +1363,7 @@ app.patch(
         errors: ["cannot change 'record' field while stream is active"],
       });
     }
-    if (
-      profiles != undefined &&
-      stream.isActive &&
-      stream.profiles != profiles
-    ) {
+    if (profiles != undefined && stream.isActive) {
       res.status(400);
       return res.json({
         errors: ["cannot change 'profiles' field while stream is active"],
