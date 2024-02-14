c4b16c99da363b2da4c980b0611e37a50f60e1c9
Suhail Kakar <suhail.zahed@gmail.com>
2024-02-14 19:55:15 +0530

    hot fix: multistream state (#2050)
    * fix: multistream state
    
    * remove logs

--- packages/www/components/StreamDetails/MultistreamTargetsTable/TargetStatusBadge.tsx
+++ packages/www/components/StreamDetails/MultistreamTargetsTable/TargetStatusBadge.tsx
@@ -1,14 +1,16 @@
 import { MultistreamTarget, Stream } from "@livepeer.studio/api";
 
-import { MultistreamStatus } from "hooks/use-analyzer";
+import { Metrics, MultistreamStatus } from "hooks/use-analyzer";
 import { useMemo } from "react";
 import StatusBadge, { Variant as Status } from "../../StatusBadge";
+import moment from "moment";
 
 const computeStatus = (
   stream: Stream,
   target: MultistreamTarget,
   status: MultistreamStatus,
-  streamActiveSince: number | undefined
+  streamActiveSince: number | undefined,
+  metrics: Metrics
 ): Status => {
   if (
     status?.connected.lastProbeTime < streamActiveSince ||
@@ -16,14 +18,26 @@ const computeStatus = (
   ) {
     status = null;
   }
-  const isConnected = status?.connected.status;
+
+  const currentTimestamp = moment().unix() * 1000;
+
+  const lastActive = metrics.MultistreamActiveSec.find(
+    (m) => m.dimensions.targetId === target.id
+  ).last[0];
+
+  const difference = (currentTimestamp - lastActive) / 1000;
+
+  const TIME_THRESHOLD = 30;
+
+  const isConnected = difference < TIME_THRESHOLD;
+
   if (
     !stream?.isActive ||
     target?.createdAt > streamActiveSince ||
     (target?.disabled && !isConnected)
   ) {
     return Status.Idle;
-  } else if (!status) {
+  } else if (difference > TIME_THRESHOLD) {
     return Status.Pending;
   }
   return isConnected ? Status.Online : Status.Offline;
@@ -33,15 +47,17 @@ const TargetStatusBadge = ({
   stream,
   target,
   status: msStatus,
+  metrics,
   streamActiveSince,
 }: {
   stream: Stream;
   target: MultistreamTarget;
   status: MultistreamStatus;
+  metrics: Metrics;
   streamActiveSince: number | undefined;
 }) => {
   const status = useMemo(
-    () => computeStatus(stream, target, msStatus, streamActiveSince),
+    () => computeStatus(stream, target, msStatus, streamActiveSince, metrics),
     [stream, target, msStatus, streamActiveSince]
   );
   const timestamp = msStatus?.connected.lastProbeTime;
