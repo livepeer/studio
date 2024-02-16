715d61d2b2ed7c31d4aa4db841031781b591c8fa
emranemran <emran.mah@gmail.com>
2024-02-15 18:58:16 -0800

    project: update get/post handlers

--- packages/api/src/controllers/project.ts
+++ packages/api/src/controllers/project.ts
@@ -5,13 +5,36 @@ import { v4 as uuid } from "uuid";
 
 const app = Router();
 
-app.get("/", authorizer({}), async (req, res) => {
+async function getProject(req) {
+  const project = await db.project.get(req.params.projectId);
+
+  if (!project || project.deleted) {
+    throw new NotFoundError(`project not found`);
+  }
+
+  if (!req.user.admin && req.user.id !== project.userId) {
+    throw new ForbiddenError(`invalid user`);
+  }
+
+  return project;
+}
+
+app.get("/:projectId", authorizer({}), async (req, res) => {
+  const project = await getProject(req);
+
+  if (!project) {
+    res.status(403);
+    return res.json({ errors: ["project not found"] });
+  }
+
   res.status(200);
-  res.json({});
+  res.json(project);
 });
 
 app.post("/", authorizer({}), async (req, res) => {
   const { name } = req.body;
+
+  console.log("XXX: req.user", req.user);
 
   const id = uuid();
   await db.project.create({
@@ -20,7 +43,17 @@ app.post("/", authorizer({}), async (req, res) => {
     userId: req.user.id,
     createdAt: Date.now(),
   });
-  res.status(201).end();
+  res.status(201);
+
+  const project = await db.project.get(id, { useReplica: false });
+
+  if (!project) {
+    res.status(403);
+    return res.json({ errors: ["project not created"] });
+  }
+
+  res.status(201);
+  res.json(id);
 });
 
 export default app;
