023fd2eb3d4ecc1df75d158a7290b0847d9bd1fb
gioelecerati <gioele@cerati.org>
2024-02-19 16:34:54 +0100

    api: flag streams as pull created for auth header in webhook

--- packages/api/src/controllers/stream.ts
+++ packages/api/src/controllers/stream.ts
@@ -1038,6 +1038,7 @@ app.put(
       profiles: req.config.defaultStreamProfiles,
       ...rawPayload,
       creatorId: mapInputCreatorId(rawPayload.creatorId),
+      isTrovoAuth: true,
     };
 
     const keyValue = _.get(payload, pullStreamKeyAccessors[key]);
