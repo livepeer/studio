a20d53340f4d2e12bdc121ac817e4f2c9cdfe759
Max Holland <max@livepeer.org>
2024-03-05 14:55:25 +0000

    unit tests

--- packages/api/src/controllers/access-control.test.ts
+++ packages/api/src/controllers/access-control.test.ts
@@ -1,11 +1,19 @@
 import serverPromise, { TestServer } from "../test-server";
-import { TestClient, clearDatabase, setupUsers } from "../test-helpers";
+import {
+  TestClient,
+  clearDatabase,
+  setupUsers,
+  AuxTestServer,
+  startAuxTestServer,
+} from "../test-helpers";
 import { SigningKey, SigningKeyResponsePayload, User } from "../schema/types";
 import { WithID } from "../store/types";
 import { Asset } from "../schema/types";
 import { v4 as uuid } from "uuid";
 import { db } from "../store";
 import { generateUniquePlaybackId } from "./generate-keys";
+import { json as bodyParserJson } from "body-parser";
+import { semaphore } from "../util";
 
 // includes auth file tests
 
@@ -33,22 +41,115 @@ afterEach(async () => {
   await clearDatabase(server);
 });
 
-describe("controllers/signing-key", () => {
+describe("controllers/access-control", () => {
+  let client: TestClient;
+  let adminUser: User;
+  let adminToken: string;
+  let nonAdminUser: User;
+  let nonAdminToken: string;
+  let gatedAsset: WithID<Asset>;
+
+  describe("webhook authorization", () => {
+    let webhookServer: AuxTestServer;
+    let hookSem: ReturnType<typeof semaphore>;
+    let webhookStatusCode: number;
+    let webhookResponseBody: string;
+
+    beforeAll(async () => {
+      ({ client, adminUser, adminToken, nonAdminUser, nonAdminToken } =
+        await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
+      client.jwtAuth = nonAdminToken;
+
+      webhookServer = await startAuxTestServer();
+      webhookServer.app.post("/hook", (req, res) => {
+        hookSem.release();
+        res.status(webhookStatusCode);
+        res.send(webhookResponseBody).end();
+      });
+      const webhook = await server.db.webhook.create({
+        id: uuid(),
+        userId: nonAdminUser.id,
+        name: "auth-webhook",
+        kind: "webhook",
+        createdAt: Date.now(),
+        events: [],
+        url: `http://localhost:${webhookServer.port}/hook`,
+      });
+
+      let id = uuid();
+      gatedAsset = await db.asset.create({
+        id,
+        name: "test-storage",
+        source: { type: "directUpload" },
+        createdAt: Date.now(),
+        playbackId: await generateUniquePlaybackId(id),
+        playbackPolicy: {
+          type: "webhook",
+          webhookId: webhook.id,
+        },
+        status: {
+          phase: "ready",
+          updatedAt: Date.now(),
+        },
+        userId: nonAdminUser.id,
+      });
+    });
+    afterAll(() => webhookServer.close());
+
+    beforeEach(async () => {
+      hookSem = semaphore();
+      webhookStatusCode = 204;
+      webhookResponseBody = "";
+    });
+
+    it("should allow playback on gated asset", async () => {
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "accessKey",
+        accessKey: "foo",
+      });
+      expect(res.status).toBe(200);
+      expect(await res.json()).toEqual({});
+      await hookSem.wait(3000);
+    });
+
+    it("should deny for non 2xx", async () => {
+      webhookStatusCode = 403;
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "accessKey",
+        accessKey: "foo",
+      });
+      expect(res.status).toBe(502);
+      await hookSem.wait(3000);
+    });
+
+    it("should deny for non 2xx status in body", async () => {
+      webhookStatusCode = 200;
+      webhookResponseBody = JSON.stringify({ ret: 403 });
+      client.jwtAuth = adminToken;
+      const res = await client.post("/access-control/gate", {
+        stream: `video+${gatedAsset.playbackId}`,
+        type: "accessKey",
+        accessKey: "foo",
+      });
+      expect(res.status).toBe(502);
+      await hookSem.wait(3000);
+    });
+  });
+
   describe("basic CRUD with JWT authorization", () => {
-    let client: TestClient;
-    let adminUser: User;
-    let adminToken: string;
-    let nonAdminUser: User;
-    let nonAdminToken: string;
     let signingKey: WithID<SigningKey>;
     let gatedPlaybackId: string;
     let publicPlaybackId: string;
-    let gatedAsset: WithID<Asset>;
 
     beforeEach(async () => {
       ({ client, adminUser, adminToken, nonAdminUser, nonAdminToken } =
         await setupUsers(server, mockAdminUserInput, mockNonAdminUserInput));
       client.jwtAuth = nonAdminToken;
+
       let created: SigningKeyResponsePayload = await client
         .post("/access-control/signing-key")
         .then((res) => res.json());
