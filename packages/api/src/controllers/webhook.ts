239193db19717ac700cebc5de84b82939e0de239
Suhail Kakar <suhail.zahed@gmail.com>
2024-05-01 08:51:20 +0530

    update webhook payload

--- packages/api/src/controllers/webhook.ts
+++ packages/api/src/controllers/webhook.ts
@@ -143,23 +143,18 @@ app.get("/subscribed/:event", authorizer({}), async (req, res) => {
   return res.json(data);
 });
 
-app.post(
-  "/",
-  authorizer({}),
-  validatePost("webhook-payload"),
-  async (req, res) => {
-    const id = uuid();
-    const doc = validateWebhookPayload(id, req.user.id, Date.now(), req.body);
-    try {
-      await req.store.create(doc);
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-    res.status(201);
-    res.json(doc);
+app.post("/", authorizer({}), validatePost("webhook"), async (req, res) => {
+  const id = uuid();
+  const doc = validateWebhookPayload(id, req.user.id, Date.now(), req.body);
+  try {
+    await req.store.create(doc);
+  } catch (e) {
+    console.error(e);
+    throw e;
   }
-);
+  res.status(201);
+  res.json(doc);
+});
 
 app.get("/:id", authorizer({}), async (req, res) => {
   // get a specific webhook
@@ -178,39 +173,31 @@ app.get("/:id", authorizer({}), async (req, res) => {
   res.json(webhook);
 });
 
-app.put(
-  "/:id",
-  authorizer({}),
-  validatePost("webhook-payload"),
-  async (req, res) => {
-    // modify a specific webhook
-    const webhook = await req.store.get(`webhook/${req.body.id}`);
-    if (
-      (webhook.userId !== req.user.id || webhook.deleted) &&
-      !req.user.admin
-    ) {
-      // do not reveal that webhooks exists
-      res.status(404);
-      return res.json({ errors: ["not found"] });
-    }
-
-    const { id, userId, createdAt } = webhook;
-    const doc = validateWebhookPayload(id, userId, createdAt, req.body);
-    try {
-      await req.store.replace(doc);
-    } catch (e) {
-      console.error(e);
-      throw e;
-    }
-    res.status(200);
-    res.json({ id: req.body.id });
+app.put("/:id", authorizer({}), validatePost("webhook"), async (req, res) => {
+  // modify a specific webhook
+  const webhook = await req.store.get(`webhook/${req.body.id}`);
+  if ((webhook.userId !== req.user.id || webhook.deleted) && !req.user.admin) {
+    // do not reveal that webhooks exists
+    res.status(404);
+    return res.json({ errors: ["not found"] });
   }
-);
+
+  const { id, userId, createdAt } = webhook;
+  const doc = validateWebhookPayload(id, userId, createdAt, req.body);
+  try {
+    await req.store.replace(doc);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+  res.status(200);
+  res.json({ id: req.body.id });
+});
 
 app.patch(
   "/:id",
   authorizer({}),
-  validatePost("webhook-payload"),
+  validatePost("webhook-patch-payload"),
   async (req, res) => {
     const webhook = await db.webhook.get(req.params.id);
 
