d103fb2c0401084430c59f421565e9dcef65c86d
Suhail Kakar <suhail.zahed@gmail.com>
2024-06-23 17:23:54 +0530

    refactor: update the home page

--- packages/www/components/Project/ProjectTile.tsx
+++ packages/www/components/Project/ProjectTile.tsx
@@ -13,90 +13,10 @@ import { HiDotsHorizontal } from "react-icons/hi";
 import { NavLink, generalSidebarItems } from "components/Sidebar";
 import { useProjectContext } from "context/ProjectContext";
 import Link from "next/link";
+import { getEmojiIcon } from "lib/get-emoji";
 
 function ProjectTile({ name, id }) {
   const { setProjectId } = useProjectContext();
-
-  const getEmojiIcon = (name = "") => {
-    const stagingKeywords = [
-      "staging",
-      "dev",
-      "qa",
-      "demo",
-      "preview",
-      "sandbox",
-      "playground",
-      "beta",
-    ];
-
-    const productionKeywords = [
-      "prod",
-      "production",
-      "live",
-      "master",
-      "main",
-      "release",
-      "deploy",
-      "ship",
-      "go",
-    ];
-
-    const otherDevKeywords = [
-      "build",
-      "compile",
-      "run",
-      "execute",
-      "debug",
-      "test",
-    ];
-
-    const randomEmojis = [
-      "✨",
-      "🌟",
-      "🎯",
-      "🏅",
-      "💡",
-      "📚",
-      "🛠️",
-      "🔒",
-      "🔍",
-      "💼",
-      "🧩",
-      "📝",
-      "🌐",
-      "🏆",
-      "🎉",
-    ];
-
-    const containsKeyword = (keywords) => {
-      return keywords.some((keyword) => name.toLowerCase().includes(keyword));
-    };
-
-    const hashString = (str) => {
-      let hash = 0;
-      for (let i = 0; i < str.length; i++) {
-        hash = str.charCodeAt(i) + ((hash << 5) - hash);
-      }
-      return hash;
-    };
-
-    const getRandomEmoji = (str) => {
-      const hash = hashString(str);
-      return randomEmojis[Math.abs(hash) % randomEmojis.length];
-    };
-
-    if (name.toLowerCase().includes("default")) {
-      return "⭐";
-    } else if (containsKeyword(stagingKeywords)) {
-      return "🔧";
-    } else if (containsKeyword(productionKeywords)) {
-      return "🚀";
-    } else if (containsKeyword(otherDevKeywords)) {
-      return "🧪";
-    } else {
-      return getRandomEmoji(name);
-    }
-  };
 
   return (
     <Box
