7000fa437918418c1835edfe75ce6ce81c1224d3
Suhail Kakar <suhail.zahed@gmail.com>
2024-07-12 03:30:55 +0530

    ui: add basic color definations

--- packages/www/tailwind.config.js
+++ packages/www/tailwind.config.js
@@ -1,12 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class",
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        border: {
+          DEFAULT: "hsl(var(--color-border))",
+        },
+        background: {
+          DEFAULT: "hsl(var(--color-background))",
+        },
+        foreground: {
+          DEFAULT: "hsl(var(--color-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--color-muted))",
+        },
+        surface: {
+          DEFAULT: "hsl(var(--color-surface))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--color-accent))",
+        },
+      },
+    },
   },
   plugins: [],
 };
