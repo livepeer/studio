c87b3a1176de9ffca972c6ff6750149ec70a0659
Victor Elias <victor@livepeer.org>
2024-07-29 22:06:26 +0100

    api: Log AI generate requests in DB (#2273)
    * api/schema: Create ai-generate-log schemas
    
    * api: Refactor generate handler to give space for log
    
    * api/generate: Implement basic request logging to DB
    
    * api/schema: Add a few boundaries to AI params
    
    * api: Add metric for AI generate performance
    
    We can tell that  from the DB too but will be useful
    to have on prometheus one more detailed than just API latency.
    
    * api/generate: Make sure to catch exceptions for log
    
    Also some improvements to the funcitons.
    
    * api: Fix build!
    
    * api: Implement tests with a fixes/improvements to impl
    
    * Fix typo
    
    * api/schema: Move log to db schema
    
    * api: Replace "complexity" with a simple status_code label
    
    ---------
    
    Co-authored-by: Rafał Leszko <rafal@livepeer.org>

--- packages/api/src/controllers/generate.ts
+++ packages/api/src/controllers/generate.ts
@@ -1,33 +1,152 @@
-import { RequestHandler, Router } from "express";
+import crypto from "crypto";
+import { Request, RequestHandler, Router } from "express";
 import FormData from "form-data";
 import multer from "multer";
-import { BodyInit } from "node-fetch";
+import { BodyInit, Response } from "node-fetch";
+import promclient from "prom-client";
+import { v4 as uuid } from "uuid";
 import logger from "../logger";
 import { authorizer, validateFormData, validatePost } from "../middleware";
+import { AiGenerateLog } from "../schema/types";
+import { db } from "../store";
+import { BadRequestError } from "../store/errors";
 import { fetchWithTimeout } from "../util";
 import { experimentSubjectsOnly } from "./experiment";
 import { pathJoin2 } from "./helpers";
 
 const AI_GATEWAY_TIMEOUT = 10 * 60 * 1000; // 10 minutes
 
+type AiGenerateType = AiGenerateLog["type"];
+
 const multipart = multer({
   storage: multer.memoryStorage(),
   limits: { fileSize: 10485760 }, // 10MiB
+});
+
+const aiGenerateDurationMetric = new promclient.Histogram({
+  name: "livepeer_api_ai_generate_duration",
+  buckets: [100, 500, 1000, 2500, 5000, 10000, 30000, 60000],
+  help: "Duration in milliseconds of the AI generation request",
+  labelNames: ["type", "status_code"],
 });
 
 const app = Router();
 
 app.use(experimentSubjectsOnly("ai-generate"));
 
-function registerGenerateHandler(
-  name: string,
+function createPayload(
+  req: Request,
+  isJSONReq: boolean,
   defaultModel: string,
-  isJSONReq = false,
+): [BodyInit, AiGenerateLog["request"]] {
+  const payload = {
+    model_id: defaultModel,
+    ...req.body,
+  };
+  if (isJSONReq) {
+    return [JSON.stringify(payload), payload];
+  }
+  if (!Array.isArray(req.files)) {
+    throw new BadRequestError("Expected an array of files");
+  }
+
+  const form = new FormData();
+  if (!("model_id" in req.body)) {
+    form.append("model_id", defaultModel);
+  }
+  for (const [key, value] of Object.entries(req.body)) {
+    form.append(key, value);
+  }
+  for (const file of req.files) {
+    form.append(file.fieldname, file.buffer, {
+      filename: file.originalname,
+      contentType: file.mimetype,
+      knownLength: file.size,
+    });
+    // We save only the hash of the file on the `payload` since that will be saved in the DB.
+    // TODO: Make this save the contents of the file in some OS and only reference them here.
+    const fileHash = crypto
+      .createHash("sha256")
+      .update(file.buffer)
+      .digest("hex");
+    payload[file.fieldname] = fileHash;
+  }
+  return [form, payload];
+}
+
+function parseOptionalJson(buf: Buffer): any {
+  const str = buf?.toString();
+  if (!str) {
+    return str;
+  }
+  try {
+    return JSON.parse(str);
+  } catch (err) {
+    return str;
+  }
+}
+
+function logAiGenerateRequest(
+  userId: string,
+  projectId: string,
+  type: AiGenerateType,
+  startedAt: number,
+  request: AiGenerateLog["request"],
+  statusCode?: number,
+  responseBuf?: Buffer,
+  error?: any,
+): void {
+  const durationMs = Date.now() - startedAt;
+  const success = !error && statusCode && statusCode >= 200 && statusCode < 300;
+  if (!success) {
+    logger.error(
+      `Error from generate API type=${type} status=${statusCode} body=${responseBuf} error=${error}`,
+    );
+  }
+
+  setImmediate(async () => {
+    let log: AiGenerateLog;
+    try {
+      const response = parseOptionalJson(responseBuf);
+      const errorStr = error ? String(error) : response?.details?.msg;
+      log = {
+        id: uuid(),
+        userId,
+        projectId,
+        type,
+        startedAt,
+        durationMs,
+        success,
+        request,
+        statusCode,
+        response,
+        error: success ? undefined : errorStr,
+      };
+      log = await db.aiGenerateLog.create(log);
+
+      aiGenerateDurationMetric.observe(
+        { type, status_code: statusCode },
+        durationMs,
+      );
+    } catch (err) {
+      logger.error(
+        `Failed to save AI generation log type=${type} log=${JSON.stringify(
+          log,
+        )} err=${err}`,
+      );
+    }
+  });
+}
+
+function registerGenerateHandler(
+  type: AiGenerateType,
+  defaultModel: string,
+  isJSONReq = false, // multipart by default
 ): RequestHandler {
-  const path = `/${name}`;
+  const path = `/${type}`;
   const middlewares = isJSONReq
-    ? [validatePost(`${name}-payload`)]
-    : [multipart.any(), validateFormData(`${name}-payload`)];
+    ? [validatePost(`${type}-payload`)]
+    : [multipart.any(), validateFormData(`${type}-payload`)];
   return app.post(
     path,
     authorizer({}),
@@ -39,58 +158,49 @@ function registerGenerateHandler(
         return;
       }
 
+      const startedAt = Date.now();
       const apiUrl = pathJoin2(aiGatewayUrl, path);
-
-      let payload: BodyInit;
-      if (isJSONReq) {
-        payload = JSON.stringify({
-          model_id: defaultModel,
-          ...req.body,
+      const [body, request] = createPayload(req, isJSONReq, defaultModel);
+      let gatewayRes: Response, response: Buffer, error: any;
+      try {
+        gatewayRes = await fetchWithTimeout(apiUrl, {
+          method: "POST",
+          body,
+          timeout: AI_GATEWAY_TIMEOUT,
+          headers: isJSONReq ? { "content-type": "application/json" } : {},
         });
-      } else {
-        const form = new FormData();
-        if (!("model_id" in req.body)) {
-          form.append("model_id", defaultModel);
-        }
-        for (const [key, value] of Object.entries(req.body)) {
-          form.append(key, value);
-        }
 
-        if (!Array.isArray(req.files)) {
-          return res.status(400).json({
-            errors: ["Expected an array of files"],
-          });
-        }
-        for (const file of req.files) {
-          form.append(file.fieldname, file.buffer, {
-            filename: file.originalname,
-            contentType: file.mimetype,
-            knownLength: file.size,
-          });
-        }
-        payload = form;
-      }
-
-      const response = await fetchWithTimeout(apiUrl, {
-        method: "POST",
-        body: payload,
-        timeout: AI_GATEWAY_TIMEOUT,
-        headers: isJSONReq ? { "content-type": "application/json" } : {},
-      });
-
-      const body = await response.json();
-      if (!response.ok) {
-        logger.error(
-          `Error from generate API ${path} status=${
-            response.status
-          } body=${JSON.stringify(body)}`,
+        response = await gatewayRes.buffer();
+      } catch (err) {
+        error = err;
+      } finally {
+        logAiGenerateRequest(
+          req.user.id,
+          req.project?.id,
+          type,
+          startedAt,
+          request,
+          gatewayRes?.status,
+          response,
+          error,
         );
-      }
-      if (response.status >= 500) {
-        return res.status(500).json({ errors: [`Failed to generate ${name}`] });
+        if (error) {
+          throw error;
+        }
       }
 
-      res.status(response.status).json(body);
+      if (gatewayRes.status >= 500) {
+        // We hide internal server error details from the user.
+        return res.status(500).json({ errors: [`Failed to generate ${type}`] });
+      }
+
+      // forward content metadata headers
+      for (const [key, value] of gatewayRes.headers.entries()) {
+        if (key.toLowerCase().startsWith("content-")) {
+          res.set(key, value);
+        }
+      }
+      res.status(gatewayRes.status).send(response);
     },
   );
 }
