9e698b29475c5b08d6986166aac74874eaecfd4b
Suhail Kakar <suhail.zahed@gmail.com>
2024-10-09 13:21:49 +0530

    feat: add ftux model

--- packages/www/content/ftux-model.tsx
+++ packages/www/content/ftux-model.tsx
@@ -4,14 +4,25 @@
 
 export const featuresList = [
   {
-    id: "projects",
+    id: "ai-gallery-playground",
     isNewFeature: true,
+    title: "Introducing AI Gallery & Playground",
+    description:
+      "Projects in Livepeer Studio are a way for your organization to create dedicated and siloed environments for streams, assets, data, and developer tools like API keys and webhooks. \n\n This can be helpful for separating staging and production environments or managing multiple applications within a single account. To help you get started, we've created a default project that contains all of your existing data.",
+    learnMoreUrl:
+      "https://docs.livepeer.org/developers/guides/managing-projects",
+    imageUrl: "/dashboard/ai/SDXL-Lightning.jpg",
+    isActive: true,
+  },
+  {
+    id: "projects",
+    isNewFeature: false,
     title: "Introducing Projects",
     description:
       "Projects in Livepeer Studio are a way for your organization to create dedicated and siloed environments for streams, assets, data, and developer tools like API keys and webhooks. \n\n This can be helpful for separating staging and production environments or managing multiple applications within a single account. To help you get started, we've created a default project that contains all of your existing data.",
     learnMoreUrl:
       "https://docs.livepeer.org/developers/guides/managing-projects",
     imageUrl: "/dashboard/img/features/projects.png",
-    isActive: true,
+    isActive: false,
   },
 ];
